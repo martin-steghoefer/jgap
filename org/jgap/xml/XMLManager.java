21057a7b60fd5016bac07b7ef04e4d83bdae929b
Neil Rotstan <neil@fitorbit.com>
2002-06-25 23:44:55 +0000

    Added javadocs to XMLManager class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@35 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- org/jgap/xml/XMLManager.java
+++ org/jgap/xml/XMLManager.java
@@ -5,7 +5,12 @@ import org.w3c.dom.*;
 import java.util.BitSet;
 import org.jgap.*;
 
-
+/**
+ * The XMLManager performs conversions from instances of genetic
+ * entities (such as Chromosome and Genotype) to XML representations
+ * of those entities, and vice versa. All of the methods in this
+ * class are static, so no construction is required (or allowed).
+ */
 public class XMLManager 
 {
   private static final DocumentBuilder documentCreator;
@@ -27,8 +32,20 @@ public class XMLManager
 
   private static final Object lock = new Object();
 
+  /**
+   * Constructor. All methods in this class are static, so no
+   * construction is allowed.
+   */
   private XMLManager() {}
 
+
+  /**
+   * Retrieve an XML Document representation of a Chromosome instance.
+   *
+   * @param subject   The chromosome to represent as an XML document.
+   *
+   * @return a document representing the given Chromosome.
+   */
   public static Document getChromosomeAsDocument(Chromosome subject) 
   {
     // DocumentBuilders do not have to be thread safe.
@@ -46,6 +63,14 @@ public class XMLManager
   }
 
 
+  /**
+   * Retrieve an XML Document representation of a Genotype instance,
+   * including its population of Chromosome instances.
+   *
+   * @param subject   The genotype to represent as an XML document.
+   *
+   * @return a Document representing the given Genotype.
+   */
   public static Document getGenotypeAsDocument(Genotype subject)
   {
     // DocumentBuilders do not have to be thread safe.
@@ -63,6 +88,20 @@ public class XMLManager
   }
 
 
+  /**
+   * Retrieve an XML Element representation of a Chromosome instance.
+   * This may be useful in scenarios where representation as an
+   * entire Document is undesirable, such as when the representation
+   * of this Chromosome is to be combined with other elements in a
+   * single Document.
+   *
+   * @param subject   The chromosome to represent as an XML element.
+   * @param xmlDocument A Document instance that will be used to
+   *                    create the Element instance. Note that the
+   *                    element will NOT be added to the document.
+   *
+   * @return an Element representing the given Chromosome.
+   */
   public static Element getChromosomeAsElement(Chromosome subject, 
                                                Document xmlDocument)
   {
@@ -98,6 +137,21 @@ public class XMLManager
   }
 
 
+  /**
+   * Retrieve an XML Element representation of a Genotype instance,
+   * including its population of Chromosome instances as sub-elements.
+   * This may be useful in scenarios where representation as an
+   * entire Document is undesirable, such as when the representation
+   * of this Genotype is to be combined with other elements in a
+   * single Document.
+   *
+   * @param subject     The chromosome to represent as an XML element.
+   * @param xmlDocument A Document instance that will be used to
+   *                    create the Element instance. Note that the
+   *                    element will NOT be added to the document.
+   *
+   * @return an Element representing the given Genotype.
+   */
   public static Element getGenotypeAsElement(Genotype subject,
                                              Document xmlDocument)
   {
@@ -118,6 +172,19 @@ public class XMLManager
   }
 
 
+  /**
+   * Retrieve a Chromosome instance constructed from a given
+   * XML Element representation.
+   *
+   * @param gaConf   The Configuration object that will be passed
+   *                 to the Chromosome during construction.
+   *
+   * @param xmlElement The XML Element representation of the
+   *                   Chromosome.
+   *
+   * @return A new Chromosome instance setup with the data
+   *         from the XML Element representation.
+   */
   public static Chromosome getChromosomeFromElement(Configuration gaConf,
                                                     Element xmlElement)
                            throws InvalidConfigurationException
@@ -161,6 +228,21 @@ public class XMLManager
   }
 
 
+  /**
+   * Retrieve a Genotype instance constructed from a given
+   * XML Element representation. Its population of Chromosomes
+   * will be constructed from the Chromosome sub-elements.
+   *
+   * @param gaConf   The Configuration object that will be passed
+   *                 to the Genotype and Chromosomes during construction.
+   *
+   * @param xmlElement The XML Element representation of the
+   *                   Genotype.
+   *
+   * @return A new Genotype instance, complete with a population
+   *         of Chromosomes, setup with the data from the XML
+   *         Element representation.
+   */
   public static Genotype getGenotypeFromElement(Configuration gaConf,
                                                 Element xmlElement)
                          throws InvalidConfigurationException
@@ -180,6 +262,21 @@ public class XMLManager
   }
 
 
+  /**
+   * Retrieve a Genotype instance constructed from a given
+   * XML Document representation. Its population of Chromosomes
+   * will be constructed from the Chromosome sub-elements.
+   *
+   * @param gaConf   The Configuration object that will be passed
+   *                 to the Genotype and Chromosomes during construction.
+   *
+   * @param xmlDocument The XML Document representation of the
+   *                    Genotype.
+   *
+   * @return A new Genotype instance, complete with a population
+   *         of Chromosomes, setup with the data from the XML
+   *         Document representation.
+   */
   public static Genotype getGenotypeFromDocument(Configuration gaConf,
                                                  Document xmlDocument)
                          throws InvalidConfigurationException
@@ -188,6 +285,19 @@ public class XMLManager
   }
 
 
+  /**
+   * Retrieve a Chromosome instance constructed from a given
+   * XML Document representation.
+   *
+   * @param gaConf   The Configuration object that will be passed
+   *                 to the Chromosome during construction.
+   *
+   * @param xmlDocument The XML Document representation of the
+   *                    Chromosome.
+   *
+   * @return A new Chromosome instance setup with the data
+   *         from the XML Document representation.
+   */
   public static Chromosome getChromosomeFromDocument(Configuration gaConf,
                                                      Document xmlDocument)
                            throws InvalidConfigurationException
