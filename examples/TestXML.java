ef551fe63abe5529a3d411a5eb6808d54439acc0
Neil Rotstan <neil@fitorbit.com>
2002-06-26 16:27:10 +0000

    Added chromosome-specific tests to TestXML class.
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@42 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/TestXML.java
+++ examples/TestXML.java
@@ -14,27 +14,39 @@ public class TestXML
       gaConf.setChromosomeSize(8);
       gaConf.setPopulationSize(10);
       gaConf.setFitnessFunction(new MaxFunction());
-    
+
+      // Test Chromsome manipulation methods
+      Chromosome chromosome = Chromosome.randomInitialChromosome(gaConf);
+      Document chromosomeDoc = XMLManager.getChromosomeAsDocument(chromosome);
+      Chromosome chromosomeFromXML =
+        XMLManager.getChromosomeFromDocument(gaConf, chromosomeDoc);
+
+      if (!(chromosomeFromXML.equals(chromosome)))
+      {
+        System.out.println("Chromosome test failed.");
+        System.exit(-1);
+      }
+
+      // Test Genotype manipulation methods
       Genotype genotype = Genotype.randomInitialGenotype(gaConf);
     
       Document genotypeDoc = XMLManager.getGenotypeAsDocument(genotype);
       Genotype genotypeFromXML = 
         XMLManager.getGenotypeFromDocument(gaConf, genotypeDoc);
 
-      if (genotypeFromXML.equals(genotype))
-      {
-        System.out.println("Test successful.");
-      }
-      else
+      if (!(genotypeFromXML.equals(genotype)))
       {
         System.out.println("Test failed.");
-      }  
+        System.exit(-1);
+      }
     }
     catch (Exception e)
     { 
       System.out.println("Test failed due to error:");
       e.printStackTrace();
     }
+
+    System.out.println("Tests successful.");
   }
 }
 
