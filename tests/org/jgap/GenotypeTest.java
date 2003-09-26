761faadb5eb843e3b7d5fcb746b315b0f579d8ad
Klaus Meffert <kontakt@doktor-meffert.de>
2003-09-26 15:20:12 +0000

    Made conform with coding guidelines
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@107 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/GenotypeTest.java
+++ tests/org/jgap/GenotypeTest.java
@@ -1,190 +1,262 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.jgap;
 
-import junit.framework.*;
-import org.jgap.impl.*;
+import org.jgap.impl.BooleanGene;
+import org.jgap.impl.DefaultConfiguration;
+import org.jgap.impl.IntegerGene;
+import org.jgap.impl.StaticFitnessFunction;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
- * <p>Title: Tests for Genotype class</p>
- * <p>Description: </p>
- * <p>Copyright: Copyright (c) 2003</p>
- * @author Klaus Meffert
+ * Tests for Genotype class
  */
 
-public class GenotypeTest extends TestCase {
-  public GenotypeTest() {
-  }
+public class GenotypeTest
+    extends TestCase
+{
 
-  public static Test suite() {
-    TestSuite suite = new TestSuite(GenotypeTest.class);
-    return suite;
-  }
+    public GenotypeTest ()
+    {
 
-  public void testConstruct_0() {
-    try {
-      Genotype genotype = new Genotype(null, null);
-      fail();
     }
-    catch (InvalidConfigurationException invex) {
-      ; //this is OK
-    }
-    catch (IllegalArgumentException invex) {
-      ; //this is OK
-    }
-  }
 
-  public void testConstruct_2() throws Exception {
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    chroms[ 0 ] = new Chromosome(new Gene[] {new IntegerGene(1, 5)});
-    try {
-      Genotype genotype = new Genotype(null, chroms);
-      fail();
-    }
-    catch (InvalidConfigurationException invex) {
-      ; //this is OK
-    }
-    catch (IllegalArgumentException invex) {
-      ; //this is OK
-    }
-  }
+    public static Test suite ()
+    {
 
-  public void testConstruct_3() throws Exception {
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    chroms[ 0 ] = new Chromosome(new Gene[] {new IntegerGene(1, 5)});
-    try {
-      Genotype genotype = new Genotype(new DefaultConfiguration(), chroms);
-      fail();
+        TestSuite suite = new TestSuite (GenotypeTest.class);
+
+        return suite;
+
     }
-    catch (InvalidConfigurationException invex) {
-      ; //this is OK
+
+    public void testConstruct_0 ()
+    {
+        try
+        {
+            Genotype genotype = new Genotype (null, null);
+            fail ();
+        }
+
+        catch (InvalidConfigurationException invex)
+        {
+            ; //this is OK
+        }
+        catch (IllegalArgumentException invex)
+        {
+            ; //this is OK
+        }
     }
-  }
 
-  public void testConstruct_4() throws Exception {
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    chroms[ 0 ] = new Chromosome(new Gene[] {new IntegerGene(1, 5)});
-    Configuration conf = new DefaultConfiguration();
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    try {
-      Genotype genotype = new Genotype(conf, chroms);
-      fail();
+    public void testConstruct_2 () throws Exception
+    {
+        Chromosome[] chroms = new Chromosome[1];
+        chroms[0] = new Chromosome (new Gene[]
+                                    {new IntegerGene (1, 5)});
+        try
+        {
+            Genotype genotype = new Genotype (null, chroms);
+            fail ();
+        }
+        catch (InvalidConfigurationException invex)
+        {
+            ; //this is OK
+        }
+        catch (IllegalArgumentException invex)
+        {
+            ; //this is OK
+        }
     }
-    catch (InvalidConfigurationException invex) {
-      ; //this is OK
+
+    public void testConstruct_3 () throws Exception
+    {
+        Chromosome[] chroms = new Chromosome[1];
+        chroms[0] = new Chromosome (new Gene[]
+                                    {new IntegerGene (1, 5)});
+        try
+        {
+            Genotype genotype = new Genotype (new DefaultConfiguration (),
+                                              chroms);
+            fail ();
+        }
+        catch (InvalidConfigurationException invex)
+        {
+            ; //this is OK
+        }
     }
-  }
 
-  public void testConstruct_5() throws Exception {
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    chroms[ 0 ] = new Chromosome(new Gene[] {new IntegerGene(1, 5)});
-    Configuration conf = new DefaultConfiguration();
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    conf.setSampleChromosome(new Chromosome(new BooleanGene(), 9));
-    try {
-      Genotype genotype = new Genotype(conf, chroms);
-      fail();
+    public void testConstruct_4 () throws Exception
+    {
+        Chromosome[] chroms = new Chromosome[1];
+        chroms[0] = new Chromosome (new Gene[]
+                                    {new IntegerGene (1, 5)});
+        Configuration conf = new DefaultConfiguration ();
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        try
+        {
+            Genotype genotype = new Genotype (conf, chroms);
+            fail ();
+        }
+        catch (InvalidConfigurationException invex)
+        {
+            ; //this is OK
+        }
     }
-    catch (InvalidConfigurationException invex) {
-      ; //this is OK
+
+    public void testConstruct_5 () throws Exception
+    {
+        Chromosome[] chroms = new Chromosome[1];
+        chroms[0] = new Chromosome (new Gene[]
+                                    {new IntegerGene (1, 5)});
+        Configuration conf = new DefaultConfiguration ();
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        conf.setSampleChromosome (new Chromosome (new BooleanGene (), 9));
+        try
+        {
+            Genotype genotype = new Genotype (conf, chroms);
+            fail ();
+        }
+        catch (InvalidConfigurationException invex)
+        {
+            ; //this is OK
+        }
     }
-  }
 
-  public void testConstruct_6() throws Exception {
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    chroms[ 0 ] = new Chromosome(new Gene[] {new IntegerGene(1, 5)});
-    Configuration conf = new DefaultConfiguration();
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    conf.setSampleChromosome(new Chromosome(new BooleanGene(), 9));
-    conf.setPopulationSize(7);
-    Genotype genotype = new Genotype(conf, chroms);
-  }
-
-  public void testConstruct_7() throws Exception {
-    Configuration conf = new DefaultConfiguration();
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    conf.setSampleChromosome(new Chromosome(new BooleanGene(), 9));
-    conf.setPopulationSize(7);
-    try {
-      Genotype genotype = new Genotype(conf, null);
-      fail();
+    public void testConstruct_6 () throws Exception
+    {
+        Chromosome[] chroms = new Chromosome[1];
+        chroms[0] = new Chromosome (new Gene[]
+                                    {new IntegerGene (1, 5)});
+        Configuration conf = new DefaultConfiguration ();
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        conf.setSampleChromosome (new Chromosome (new BooleanGene (), 9));
+        conf.setPopulationSize (7);
+        Genotype genotype = new Genotype (conf, chroms);
     }
-    catch (IllegalArgumentException illex) {
-      ; //this is OK
+
+    public void testConstruct_7 () throws Exception
+    {
+        Configuration conf = new DefaultConfiguration ();
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        conf.setSampleChromosome (new Chromosome (new BooleanGene (), 9));
+        conf.setPopulationSize (7);
+        try
+        {
+            Genotype genotype = new Genotype (conf, null);
+            fail ();
+        }
+        catch (IllegalArgumentException illex)
+        {
+            ; //this is OK
+        }
     }
-  }
 
-  public void testGetChromosomes_0() throws Exception {
-    Configuration conf = new DefaultConfiguration();
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    Chromosome chrom = new Chromosome(new Gene[] {new IntegerGene(1, 5)});
-    chroms[ 0 ] = chrom;
-    conf.setSampleChromosome(chrom);
-    conf.setPopulationSize(7);
-    Genotype genotype = new Genotype(conf, chroms);
-    assertEquals(1, genotype.getChromosomes().length);
-    assertEquals(chrom, genotype.getChromosomes()[ 0 ]);
-  }
-
-  public void testGetFittestChromosome_0() {
-    /**@todo implement*/
-  }
-
-  public void testEvolve_0() {
-    /**@todo implement*/
-  }
-
-  /**
-   * This test fails intentionally.
-   * Reason: Chromosome.m_activeConfiguration is final but there is an easy way to
-   * construct a Chromosome without giving a configuration (see below)!
-   * @throws Exception
-   */
-  public void testToString_0() throws Exception {
-    Configuration conf = new DefaultConfiguration();
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    Chromosome chrom = new Chromosome(new Gene[] {new IntegerGene(1, 55)});
-    chroms[ 0 ] = chrom;
-    conf.setSampleChromosome(chrom);
-    conf.setPopulationSize(7);
-    chrom.setActiveConfiguration(conf);
-    Genotype genotype = new Genotype(conf, chroms);
-    assertTrue(genotype.toString() != null);
-    assertTrue(genotype.toString().length() > 0);
-  }
-
-  public void testRandomInitialGenotype_0() throws Exception {
-    try {
-      Genotype genotype = Genotype.randomInitialGenotype(null);
-      fail();
+    public void testGetChromosomes_0 () throws Exception
+    {
+        Configuration conf = new DefaultConfiguration ();
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        Chromosome[] chroms = new Chromosome[1];
+        Chromosome chrom = new Chromosome (new Gene[]
+                                           {new IntegerGene (1, 5)});
+        chroms[0] = chrom;
+        conf.setSampleChromosome (chrom);
+        conf.setPopulationSize (7);
+        Genotype genotype = new Genotype (conf, chroms);
+        assertEquals (1, genotype.getChromosomes ().length);
+        assertEquals (chrom, genotype.getChromosomes ()[0]);
     }
-    catch (IllegalArgumentException illex) {
-      ; //this is OK
+
+    public void testGetFittestChromosome_0 ()
+    {
+        /**@todo implement*/
     }
-  }
 
-  public void testRandomInitialGenotype_1() throws Exception {
-    Configuration conf = new DefaultConfiguration();
-    Chromosome chrom = new Chromosome(new Gene[] {new IntegerGene(1, 9999)});
-    conf.setPopulationSize(7777);
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    conf.setSampleChromosome(chrom);
-    Genotype genotype = Genotype.randomInitialGenotype(conf);
-    assertEquals(7777, genotype.getChromosomes().length);
-  }
+    public void testEvolve_0 ()
+    {
+        /**@todo implement*/
+    }
 
-  public void testEquals_0() throws Exception {
-    Chromosome[] chroms = new Chromosome[ 1 ];
-    chroms[ 0 ] = new Chromosome(new Gene[] {new IntegerGene(1, 5)});
-    Configuration conf = new DefaultConfiguration();
-    conf.setFitnessFunction(new StaticFitnessFunction(5));
-    conf.setSampleChromosome(new Chromosome(new BooleanGene(), 9));
-    conf.setPopulationSize(99999);
-    Genotype genotype = new Genotype(conf, chroms);
-    assertEquals(false, genotype.equals(null));
-    Genotype genotype2 = new Genotype(conf, chroms);
-    assertTrue(genotype.equals(genotype2));
-    /**@todo following still fails, also see testToString_0()*/
+    /**
+     * This test fails intentionally.
+     * Reason: Chromosome.m_activeConfiguration is final but there is an easy way to
+     * construct a Chromosome without giving a configuration (see below)!
+     * @throws Exception
+     */
+    public void testToString_0 () throws Exception
+    {
+        Configuration conf = new DefaultConfiguration ();
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        Chromosome[] chroms = new Chromosome[1];
+        Chromosome chrom = new Chromosome (new Gene[]
+                                           {new IntegerGene (1, 55)});
+        chroms[0] = chrom;
+        conf.setSampleChromosome (chrom);
+        conf.setPopulationSize (7);
+        chrom.setActiveConfiguration (conf);
+        Genotype genotype = new Genotype (conf, chroms);
+        assertTrue (genotype.toString () != null);
+        assertTrue (genotype.toString ().length () > 0);
+    }
+
+    public void testRandomInitialGenotype_0 () throws Exception
+    {
+        try
+        {
+            Genotype genotype = Genotype.randomInitialGenotype (null);
+            fail ();
+        }
+        catch (IllegalArgumentException illex)
+        {
+            ; //this is OK
+        }
+    }
+
+    public void testRandomInitialGenotype_1 () throws Exception
+    {
+        Configuration conf = new DefaultConfiguration ();
+        Chromosome chrom = new Chromosome (new Gene[]
+                                           {new IntegerGene (1, 9999)});
+        conf.setPopulationSize (7777);
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        conf.setSampleChromosome (chrom);
+        Genotype genotype = Genotype.randomInitialGenotype (conf);
+        assertEquals (7777, genotype.getChromosomes ().length);
+    }
+
+    public void testEquals_0 () throws Exception
+    {
+        Chromosome[] chroms = new Chromosome[1];
+        chroms[0] = new Chromosome (new Gene[]
+                                    {new IntegerGene (1, 5)});
+        Configuration conf = new DefaultConfiguration ();
+        conf.setFitnessFunction (new StaticFitnessFunction (5));
+        conf.setSampleChromosome (new Chromosome (new BooleanGene (), 9));
+        conf.setPopulationSize (99999);
+        Genotype genotype = new Genotype (conf, chroms);
+        assertEquals (false, genotype.equals (null));
+        Genotype genotype2 = new Genotype (conf, chroms);
+        assertTrue (genotype.equals (genotype2));
+        /**@todo following still fails, also see testToString_0()*/
 //    assertEquals(genotype.toString(), genotype2.toString());
-  }
+    }
+
 }
