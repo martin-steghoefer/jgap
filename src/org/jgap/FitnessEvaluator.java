4e4c13a54347e5eafe45b773a178c7667d2e9d59
Klaus Meffert <kontakt@doktor-meffert.de>
2003-09-27 22:21:39 +0000

    Added FitnessEvaluator interface class (req.783992)
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@131 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/FitnessEvaluator.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jgap;
+
+/**
+ * Interface for a fitness evaluator used in a Genotype to determine how to
+ * interpret the fitness value. The fitness value can either be interpreted
+ * straight forward as a fitness indicator (the higher the better). Or it could
+ * be seen as a defect rate (the lower the better).
+ *
+ * @author Klaus Meffert
+ * @since 1.1
+ */
+public interface FitnessEvaluator
+{
+    /**
+     * Compares the first given fitness value with the second and returns true
+     * if the first one is greater than the second one. Otherwise returns false
+     * @param a_fitness_value1 first fitness value
+     * @param a_fitness_value2 second fitness value
+     * @return true: first fitness value greater than second
+     * @since 1.1
+     */
+    boolean isFitter(int a_fitness_value1, int a_fitness_value2);
+}
