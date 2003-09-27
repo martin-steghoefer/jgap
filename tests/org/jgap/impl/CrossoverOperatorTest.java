4d5e650baf8919b736de1f4f6d12478abb000524
Klaus Meffert <kontakt@doktor-meffert.de>
2003-09-27 15:37:12 +0000

    Changed according to coding guidelines
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@118 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/CrossoverOperatorTest.java
+++ tests/org/jgap/impl/CrossoverOperatorTest.java
@@ -1,19 +1,40 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.jgap.impl;
 
-import junit.framework.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
 
 /**
- * <p>Title: Test class for CrossoverOperator class</p>
- * <p>Copyright: Copyright (c) 2003</p>
- * @author Klaus Meffert
+ * Test class for CrossoverOperator class
  */
 
 public class CrossoverOperatorTest extends TestCase {
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.1 $";
+  private static final String CVS_REVISION = "$Revision: 1.2 $";
 
   public CrossoverOperatorTest() {
+
   }
 
   public static Test suite() {
@@ -24,4 +45,5 @@ public class CrossoverOperatorTest extends TestCase {
   public void testOperate_0() {
     /**@todo implement*/
   }
+
 }
--- tests/org/jgap/impl/DefaultConfigurationTest.java
+++ tests/org/jgap/impl/DefaultConfigurationTest.java
@@ -1,24 +1,41 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.jgap.impl;
 
-import java.util.*;
-import junit.framework.*;
-import junitx.util.PrivateAccessor;
-import org.jgap.impl.*;
-import org.jgap.event.*;
-import org.jgap.*;
+import org.jgap.Configuration;
+import org.jgap.event.EventManager;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
- * <p>Title: Tests for DefaultConfiguration class</p>
- * <p>Copyright: Copyright (c) 2003</p>
- * @author Klaus Meffert
+ * Tests for DefaultConfiguration class
  */
 
 public class DefaultConfigurationTest extends TestCase {
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   public DefaultConfigurationTest() {
+
   }
 
   public static Test suite() {
