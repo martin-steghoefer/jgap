6ca73cf6d65e15ec0b9c06a899f5dfa19189fda1
Klaus Meffert <kontakt@doktor-meffert.de>
2003-09-28 12:03:03 +0000

    Test cases for CompositeGene
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@141 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/impl/CompositeGeneTest.java
@@ -0,0 +1,289 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jgap.impl;
+
+import java.util.Vector;
+
+import org.jgap.Gene;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junitx.util.PrivateAccessor;
+
+/**
+ * Tests for CompositeGene class
+ *
+ * @author Klaus Meffert
+ * @since 1.1
+ */
+public class CompositeGeneTest
+    extends TestCase
+{
+    private static final double DELTA = 0.0001d;
+
+    public CompositeGeneTest ()
+    {
+    }
+
+    public static Test suite ()
+    {
+        TestSuite suite = new TestSuite (CompositeGeneTest.class);
+        return suite;
+    }
+
+    public void testConstruct_0 ()
+    {
+        //following should be possible without exception
+        Gene gene = new CompositeGene ();
+    }
+
+    public void testAddGene_0 ()
+    {
+        CompositeGene gene = new CompositeGene ();
+        try
+        {
+            gene.addGene (new CompositeGene (), false);
+            fail ();
+        }
+        catch (IllegalArgumentException iex)
+        {
+            ; //this is OK
+        }
+    }
+
+    public void testAddGene_1 ()
+    {
+        CompositeGene gene = new CompositeGene ();
+        Gene newGene = new DoubleGene ();
+        gene.addGene (newGene, false);
+        assertEquals (1, gene.size ());
+        gene.addGene (new DoubleGene (1.2d, 3.4d), false);
+        assertEquals (2, gene.size ());
+        //try to remove a non existent gene
+        gene.removeGeneByIdentity (new DoubleGene ());
+        assertEquals (2, gene.size ());
+        //try to add an already existent gene
+        try
+        {
+            gene.addGene (newGene, false);
+            fail ();
+        }
+        catch (IllegalArgumentException iex)
+        {
+            ; //this is OK
+        }
+    }
+
+    public void testToString_0 ()
+    {
+        CompositeGene gene = new CompositeGene ();
+        Gene newGene1 = new DoubleGene();
+        newGene1.setAllele(new Double(47.123d));
+        gene.addGene(newGene1, false);
+        assertEquals (newGene1.toString(), gene.toString ());
+        Gene newGene2 = new IntegerGene();
+        newGene2.setAllele(new Integer(23456));
+        gene.addGene(newGene2, false);
+        assertEquals (newGene1.toString () + gene.GENE_DELIMITER +
+            newGene2.toString (), gene.toString ());
+    }
+
+    public void testGetAllele_0 ()
+    {
+        Gene gene = new CompositeGene ();
+        //this should be possible without exception
+        gene.setAllele (new Vector ());
+    }
+
+    public void testGetAllele_1 ()
+    {
+        CompositeGene gene = new CompositeGene ();
+        Gene newGene1 = new IntegerGene(1,5);
+        gene.addGene(newGene1, false);
+        Vector v = new Vector();
+        v.add(new Integer(4));
+        gene.setAllele (v);
+        v = (Vector)gene.getAllele ();
+        assertEquals (newGene1.getAllele(), v.elementAt(0));
+        assertEquals (1, v.size());
+    }
+
+    public void testGetAllele_2 ()
+    {
+        CompositeGene gene = new CompositeGene ();
+        Gene newGene1 = new IntegerGene(1,5);
+        gene.addGene(newGene1, false);
+        Gene newGene2 = new DoubleGene(77.2d,999.0d);
+        gene.addGene(newGene2, false);
+        Vector v = new Vector();
+        v.add(new Integer(4));
+        v.add(new Double(333.5d));
+        gene.setAllele (v);
+        v = (Vector)gene.getAllele ();
+        assertEquals (newGene1.getAllele(), v.elementAt(0));
+        assertEquals (newGene2.getAllele(), v.elementAt(1));
+        assertEquals (2, v.size());
+    }
+
+    public void testEquals_0 ()
+    {
+        Gene gene1 = new CompositeGene ();
+        Gene gene2 = new CompositeGene ();
+        assertTrue (gene1.equals (gene2));
+        assertTrue (gene2.equals (gene1));
+    }
+
+    public void testEquals_1 ()
+    {
+        Gene gene1 = new CompositeGene ();
+        assertFalse (gene1.equals (null));
+    }
+
+    public void testEquals_2 ()
+    {
+        Gene gene1 = new CompositeGene ();
+        assertFalse (gene1.equals (new BooleanGene ()));
+    }
+
+    public void testEquals_3 ()
+    {
+        Gene gene1 = new CompositeGene ();
+        assertFalse (gene1.equals (new Vector ()));
+    }
+
+    public void testEquals_4 ()
+    {
+        CompositeGene gene1 = new CompositeGene ();
+        gene1.addGene(null, false);
+        CompositeGene gene2 = new CompositeGene ();
+        gene2.addGene(null, false);
+        assertTrue (gene1.equals (gene2));
+        assertTrue (gene2.equals (gene1));
+    }
+
+    public void testEquals_5 ()
+    {
+        CompositeGene gene1 = new CompositeGene ();
+        Gene newGene1 = new IntegerGene(3,5);
+        gene1.addGene(newGene1, false);
+        CompositeGene gene2 = new CompositeGene ();
+        gene2.addGene(newGene1, false);
+        assertTrue (gene1.equals (gene2));
+        assertTrue (gene2.equals (gene1));
+    }
+
+    public void testEquals_6 ()
+    {
+        CompositeGene gene1 = new CompositeGene ();
+        Gene newGene1 = new IntegerGene(3,5);
+        gene1.addGene(newGene1, false);
+        Gene newGene2 = new IntegerGene(7,9);
+        CompositeGene gene2 = new CompositeGene ();
+        gene2.addGene(newGene2, false);
+        assertEquals (newGene1.equals(newGene2), gene1.equals (gene2));
+        assertEquals (newGene2.equals(newGene1), gene2.equals (gene1));
+    }
+
+    public void testEquals_7 ()
+    {
+        CompositeGene gene1 = new CompositeGene ();
+        Gene newGene1 = new IntegerGene(3,5);
+        gene1.addGene(newGene1, false);
+        Gene newGene2 = new DoubleGene(7.2d,59.4d);
+        CompositeGene gene2 = new CompositeGene ();
+        gene2.addGene(newGene2, false);
+        assertEquals (newGene1.equals(newGene2), gene1.equals (gene2));
+        assertEquals (newGene2.equals(newGene1), gene2.equals (gene1));
+    }
+
+    public void testSetAllele_0 ()
+    {
+        Gene gene = new CompositeGene ();
+        try
+        {
+            gene.setAllele (null);
+            fail ();
+        }
+        catch (IllegalArgumentException iex)
+        {
+            ; //this is OK
+        }
+    }
+
+    public void testSetAllele_1 ()
+    {
+        Gene gene = new CompositeGene ();
+        try
+        {
+            gene.setAllele (new Double (2.3d));
+            fail ();
+        }
+        catch (IllegalArgumentException iex)
+        {
+            ; //this is OK
+        }
+    }
+
+    /**
+     * Set Allele to empty Vector, no exception should occur
+     */
+    public void testSetAllele_2 ()
+    {
+        Gene gene1 = new CompositeGene ();
+        gene1.setAllele (new Vector());
+    }
+
+    public void testSetAllele_3 ()
+    {
+        Gene gene1 = new CompositeGene ();
+        try
+        {
+            gene1.setAllele ("22");
+            fail ();
+        }
+        catch (IllegalArgumentException classex)
+        {
+            ; //this is OK
+        }
+    }
+
+    public void testNewGene_0 () throws Exception
+    {
+        CompositeGene gene1 = new CompositeGene ();
+        gene1.addGene(new DoubleGene(2.05d, 7.53d), false);
+        gene1.addGene(new DoubleGene(128.35d, 155.90d), false);
+        gene1.addGene(new IntegerGene(3, 8), false);
+        gene1.addGene(new BooleanGene(), false);
+        gene1.addGene(new StringGene(), false);
+        gene1.addGene(new StringGene(2,5), false);
+        gene1.addGene(new StringGene(6,11,"ABC"), false);
+        CompositeGene gene2 = (CompositeGene)gene1.newGene(new DefaultConfiguration ());
+        assertTrue(gene1.equals(gene2));
+
+        //remove all genes from gene2 that are contained in gene1.
+        //Because they should be equal, gene2 should then be empty.
+        //---------------------------------------------------------
+        for (int i=0;i<gene1.size();i++) {
+            gene2.removeGene(gene1.geneAt(i));
+        }
+        assertEquals(0, gene2.size());
+    }
+
+}
