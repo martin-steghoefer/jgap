7071cf188b7cd7980c9ac51072f43ac5bdad8c21
Klaus Meffert <kontakt@doktor-meffert.de>
2003-09-28 10:48:55 +0000

    Default impl. of MutationRateCalculator
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@138 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/impl/DefaultMutationRateCalculator.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jgap.impl;
+
+import org.jgap.MutationRateCalculator;
+import org.jgap.Configuration;
+
+public class DefaultMutationRateCalculator
+    implements MutationRateCalculator
+{
+    /** String containing the CVS revision. Read out via reflection!*/
+    private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+    public DefaultMutationRateCalculator ()
+    {
+
+    }
+
+    public int calculateCurrentRate (Configuration a_activeConfiguration)
+    {
+        return a_activeConfiguration.getChromosomeSize() * 10;
+    }
+
+}
