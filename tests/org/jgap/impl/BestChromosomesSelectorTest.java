807302ed675d86aeb51056e3bb97bfc98f232b6d
Klaus Meffert <kontakt@doktor-meffert.de>
2003-09-29 17:08:49 +0000

    Test cases for BestChromosomeSelector
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@162 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/impl/BestChromosomesSelectorTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jgap.impl;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Tests for BestChromosomesSelector class
+ *
+ * @since 1.1
+ * @author Klaus Meffert
+ */
+public class BestChromosomesSelectorTest
+    extends TestCase
+{
+
+    /** String containing the CVS revision. Read out via reflection!*/
+    private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+    public BestChromosomesSelectorTest ()
+    {
+    }
+
+    public static Test suite ()
+    {
+        TestSuite suite = new TestSuite (BestChromosomesSelectorTest.class);
+        return suite;
+    }
+
+    public void testConstruct_0 ()
+    {
+        /**todo implement*/
+    }
+}
