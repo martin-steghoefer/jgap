c3636351cb705259e64312630335eae5eef4495f
Klaus Meffert <kontakt@doktor-meffert.de>
2003-10-04 10:39:33 +0000

    Added method to private test class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@199 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/NumberGeneTest.java
+++ tests/org/jgap/impl/NumberGeneTest.java
@@ -40,7 +40,7 @@ public class NumberGeneTest
     extends TestCase
 {
     /** String containing the CVS revision. Read out via reflection!*/
-    private final static String CVS_REVISION = "$Revision: 1.1 $";
+    private final static String CVS_REVISION = "$Revision: 1.2 $";
 
     public NumberGeneTest ()
     {
@@ -183,7 +183,6 @@ public class NumberGeneTest
         protected final static long INTEGER_RANGE = (long) Integer.MAX_VALUE -
             (long) Integer.MIN_VALUE;
 
-
         protected int m_upperBounds;
 
         protected int m_lowerBounds;
@@ -197,74 +196,82 @@ public class NumberGeneTest
             calculateBoundsUnitsToIntegerUnitsRatio ();
         }
 
-            public Gene newGene (Configuration a_activeConfiguration)
-            {
-                return new NumberGeneImpl (m_lowerBounds, m_upperBounds);
-            }
+        public Gene newGene (Configuration a_activeConfiguration)
+        {
+            return new NumberGeneImpl (m_lowerBounds, m_upperBounds);
+        }
 
-            public String getPersistentRepresentation ()
-                throws UnsupportedOperationException
-            {
-                return toString () + PERSISTENT_FIELD_DELIMITER + m_lowerBounds +
-                    PERSISTENT_FIELD_DELIMITER + m_upperBounds;
-            }
+        public String getPersistentRepresentation ()
+            throws UnsupportedOperationException
+        {
+            return toString () + PERSISTENT_FIELD_DELIMITER + m_lowerBounds +
+                PERSISTENT_FIELD_DELIMITER + m_upperBounds;
+        }
 
-            public void setValueFromPersistentRepresentation (String
-                a_representation)
-                throws UnsupportedRepresentationException
-            {
-                // not implemented here!
-                // ---------------------
-            }
+        public void setValueFromPersistentRepresentation (String
+            a_representation)
+            throws UnsupportedRepresentationException
+        {
+            // not implemented here!
+            // ---------------------
+        }
 
-            public void setToRandomValue (RandomGenerator a_numberGenerator)
-            {
-                // not implemented here!
-                // ---------------------
-            }
+        public void setToRandomValue (RandomGenerator a_numberGenerator)
+        {
+            // not implemented here!
+            // ---------------------
+        }
 
-            protected void calculateBoundsUnitsToIntegerUnitsRatio ()
-            {
-                int divisor = m_upperBounds - m_lowerBounds + 1;
-
-                if (divisor == 0)
-                {
-                    m_boundsUnitsToIntegerUnits = INTEGER_RANGE;
-                }
-                else
-                {
-                    m_boundsUnitsToIntegerUnits = INTEGER_RANGE / divisor;
-                }
-            }
-            protected void mapValueToWithinBounds ()
-            {
-                if (m_value != null)
-                {
-                    Integer i_value = ((Integer)m_value);
-                    // If the value exceeds either the upper or lower bounds, then
-                    // map the value to within the legal range. To do this, we basically
-                    // calculate the distance between the value and the integer min,
-                    // determine how many bounds units that represents, and then add
-                    // that number of units to the upper bound.
-                    // -----------------------------------------------------------------
-                    if (i_value.intValue () > m_upperBounds ||
-                        i_value.intValue () < m_lowerBounds)
-                    {
-                        long differenceFromIntMin = (long) Integer.MIN_VALUE +
-                            (long) i_value.intValue ();
-                        int differenceFromBoundsMin =
-                            (int) (differenceFromIntMin / m_boundsUnitsToIntegerUnits);
-
-                        m_value =
-                            new Integer (m_upperBounds + differenceFromBoundsMin);
-                    }
-                }
-            }
-
-            protected int compareToNative(Object o1, Object o2) {
-                return ((Integer)o1).compareTo (o2);
-            }
-
+        public void applyMutation (int index, double a_percentage)
+        {
+            // not implemented here!
+            // ---------------------
 
         }
+
+        protected void calculateBoundsUnitsToIntegerUnitsRatio ()
+        {
+            int divisor = m_upperBounds - m_lowerBounds + 1;
+
+            if (divisor == 0)
+            {
+                m_boundsUnitsToIntegerUnits = INTEGER_RANGE;
+            }
+            else
+            {
+                m_boundsUnitsToIntegerUnits = INTEGER_RANGE / divisor;
+            }
+        }
+
+        protected void mapValueToWithinBounds ()
+        {
+            if (m_value != null)
+            {
+                Integer i_value = ( (Integer) m_value);
+                // If the value exceeds either the upper or lower bounds, then
+                // map the value to within the legal range. To do this, we basically
+                // calculate the distance between the value and the integer min,
+                // determine how many bounds units that represents, and then add
+                // that number of units to the upper bound.
+                // -----------------------------------------------------------------
+                if (i_value.intValue () > m_upperBounds ||
+                    i_value.intValue () < m_lowerBounds)
+                {
+                    long differenceFromIntMin = (long) Integer.MIN_VALUE +
+                        (long) i_value.intValue ();
+                    int differenceFromBoundsMin =
+                        (int) (differenceFromIntMin /
+                        m_boundsUnitsToIntegerUnits);
+
+                    m_value =
+                        new Integer (m_upperBounds + differenceFromBoundsMin);
+                }
+            }
+        }
+
+        protected int compareToNative (Object o1, Object o2)
+        {
+            return ( (Integer) o1).compareTo (o2);
+        }
+    }
 }
