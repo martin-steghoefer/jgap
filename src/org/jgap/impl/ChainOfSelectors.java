177df00be08660160128779e2ace78eb5484f205
Klaus Meffert <kontakt@doktor-meffert.de>
2003-10-04 12:17:53 +0000

    Added class (req. 814057)
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@207 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/impl/ChainOfSelectors.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2003 Klaus Meffert
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jgap.impl;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
+
+import org.jgap.InvalidConfigurationException;
+import org.jgap.NaturalSelector;
+
+/**
+ * Ordered chain of NaturalSelectors. With this container you can plugin
+ * NaturalSelector implementations which will be performed either before (pre-)
+ * or after (post-selectors) registered genetic operations have been applied.
+ * @see Genotype.evolve
+ * @see Configuration.addNaturalSelector
+ *
+ * @author Klaus Meffert
+ * @since 1.1
+ */
+public class ChainOfSelectors
+{
+    /** String containing the CVS revision. Read out via reflection!*/
+    private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+    /**
+     * Ordered list holding the NaturalSelector's.
+     * Intentionally used as a decorator and not via inheritance!
+     */
+    private List selectors;
+
+    public ChainOfSelectors ()
+    {
+        selectors = new Vector ();
+    }
+
+    /**
+     * Adds a natural selector to the chain
+     * @param a_selector the selector to be added
+     * @throws InvalidConfigurationException
+     *
+     * @since 1.1 (previously part of class Configuration)
+     */
+    public void addNaturalSelector (NaturalSelector a_selector)
+        throws InvalidConfigurationException
+    {
+        if (a_selector == null)
+        {
+            throw new InvalidConfigurationException (
+                "This Configuration object is locked. Settings may not be " +
+                "altered.");
+        }
+        selectors.add (a_selector);
+    }
+
+    public void addAll (Collection c)
+        throws InvalidConfigurationException
+    {
+        Iterator it = c.iterator ();
+        while (it.hasNext ())
+        {
+            NaturalSelector selector = (NaturalSelector) it.next ();
+            addNaturalSelector (selector);
+        }
+    }
+
+    public int size ()
+    {
+        return selectors.size ();
+    }
+
+    public boolean isEmpty ()
+    {
+        return size () == 0;
+    }
+
+    public int hashCode ()
+    {
+        return selectors.hashCode ();
+    }
+
+    public boolean equals (Object o)
+    {
+        return selectors.equals (o);
+    }
+
+    public NaturalSelector get (int index)
+    {
+        return (NaturalSelector) selectors.get (index);
+    }
+}
