d265f412907c50d757f3cd9c24b27046df643666
Klaus Meffert <kontakt@doktor-meffert.de>
2003-10-19 14:09:11 +0000

    only beatifications
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@218 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/event/GeneticEventTest.java
+++ tests/org/jgap/event/GeneticEventTest.java
@@ -1,47 +1,61 @@
 package org.jgap.event;
 
-import junit.framework.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 /**
- * <p>Title: Tests for GeneticEvent class</p>
- * <p>Copyright: Copyright (c) 2003</p>
+ * Tests for GeneticEvent class
  * @author Klaus Meffert
+ * @since 1.1
  */
 
-public class GeneticEventTest extends TestCase {
+public class GeneticEventTest
+    extends TestCase
+{
 
-  /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+    /** String containing the CVS revision. Read out via reflection!*/
 
-  public GeneticEventTest() {
-  }
+    private final static String CVS_REVISION = "$Revision: 1.2 $";
 
-  public static Test suite() {
-    TestSuite suite = new TestSuite(GeneticEventTest.class);
-    return suite;
-  }
+    public GeneticEventTest ()
+    {
 
-  public void testConstruct_0() {
-    try {
-      GeneticEvent event = new GeneticEvent("testEventName", null);
-      fail();
     }
-    catch (IllegalArgumentException illex) {
-      ; //this is OK
+
+    public static Test suite ()
+    {
+        TestSuite suite = new TestSuite (GeneticEventTest.class);
+        return suite;
     }
-  }
 
-  public void testConstruct_1() {
-    GeneticEvent event = new GeneticEvent(null, this);
-  }
+    public void testConstruct_0 ()
+    {
+        try
+        {
+            GeneticEvent event = new GeneticEvent ("testEventName", null);
+            fail ();
+        }
+        catch (IllegalArgumentException illex)
+        {
+            ; //this is OK
+        }
+    }
 
-  public void testGetEventName_0() {
-    GeneticEvent event = new GeneticEvent("testEventName", this);
-    assertEquals("testEventName", event.getEventName());
-  }
+    public void testConstruct_1 ()
+    {
+        GeneticEvent event = new GeneticEvent (null, this);
+    }
 
-  public void testGENOTYPE_EVOLVED_EVENT_0() {
-    assertTrue(GeneticEvent.GENOTYPE_EVOLVED_EVENT != null);
-    assertTrue(GeneticEvent.GENOTYPE_EVOLVED_EVENT.length() > 0);
-  }
+    public void testGetEventName_0 ()
+    {
+        GeneticEvent event = new GeneticEvent ("testEventName", this);
+        assertEquals ("testEventName", event.getEventName ());
+    }
+
+    public void testGENOTYPE_EVOLVED_EVENT_0 ()
+    {
+        assertTrue (GeneticEvent.GENOTYPE_EVOLVED_EVENT != null);
+        assertTrue (GeneticEvent.GENOTYPE_EVOLVED_EVENT.length () > 0);
+    }
 }
