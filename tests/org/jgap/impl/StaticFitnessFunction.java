344a4ad48f9c50985637874035e15491d4d485b0
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-13 21:06:05 +0000

    only formatting
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@242 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/StaticFitnessFunction.java
+++ tests/org/jgap/impl/StaticFitnessFunction.java
@@ -1,21 +1,34 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package org.jgap.impl;
 
 import org.jgap.*;
-import java.util.Random;
 
 /**
- * <p>Title: Fitness function always returning the same value</p>
- * <p>Description: Only for testing purpose</p>
- * <p>Copyright: Copyright (c) 2003</p>
- * <p>Company: </p>
+ * Fitness function always returning the same value
+ * Only for testing purpose
  * @author Klaus Meffert
+ * @since 1.1
  */
-
-public class StaticFitnessFunction extends FitnessFunction {
-
+public class StaticFitnessFunction
+    extends FitnessFunction {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
-
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
   private int staticFitnessValue;
   public StaticFitnessFunction(int staticFitnessValue) {
     this.staticFitnessValue = staticFitnessValue;
@@ -25,9 +38,11 @@ public class StaticFitnessFunction extends FitnessFunction {
     int result = staticFitnessValue;
     return result;
   }
+
   public int getStaticFitnessValue() {
     return staticFitnessValue;
   }
+
   public void setStaticFitnessValue(int staticFitnessValue) {
     this.staticFitnessValue = staticFitnessValue;
   }
