efbbfa412a2113a681431ba7a0a8911eae5aee6e
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-23 19:17:06 +0000

    See request 1069011
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@346 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/Population.java
@@ -0,0 +1,83 @@
+package org.jgap;
+
+import java.util.*;
+
+/**
+ * List of chromosomes held in the Genotype (or possibly later in the
+ * Configuration object
+ *
+ * @author Klaus Meffert
+ * @since 2.0
+ */
+public class Population {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private static final String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * The array of Chromosomes that makeup the Genotype's population.
+   */
+  private List m_chromosomes;
+
+  public Population(Chromosome[] chromosomes) {
+    this();
+    for (int i = 0; i < chromosomes.length; i++) {
+      m_chromosomes.add(chromosomes[i]);
+    }
+  }
+
+  public Population(int size) {
+    m_chromosomes = new ArrayList(size);
+  }
+
+  public Population() {
+    m_chromosomes = new ArrayList(100);
+  }
+
+  /**
+   * Adds a <code>Cell</code> to this <code>Population</code>. Does nothing
+   * when given <code>null</code>.
+   *
+   * @param toAdd	the <code>Cell</code> to add
+   */
+  public void addChromosome(Chromosome toAdd) {
+    if (toAdd != null) {
+      m_chromosomes.add(toAdd);
+    }
+  }
+
+  /**
+   * Adds all the <code>Cell</code>s in the given <code>Population</code>.
+   * Does nothing on <code>null</code> or an empty <code>Population</code>.
+   *
+   * @param p	the <code>Population</code> to add
+   */
+  public void addChromosomes(Population toAdd) {
+    if (toAdd != null) {
+      m_chromosomes.addAll(toAdd.getChromosomes());
+    }
+  }
+
+  public List getChromosomes() {
+    return m_chromosomes;
+  }
+
+  public Chromosome getChromosome(int index) {
+    return (Chromosome) m_chromosomes.get(index);
+  }
+
+  public int size() {
+    return m_chromosomes.size();
+  }
+
+  public Iterator iterator() {
+    return m_chromosomes.iterator();
+  }
+
+  public Chromosome[] toChromosomes() {
+    Chromosome[] result = new Chromosome[m_chromosomes.size()];
+    for (int i = 0; i < m_chromosomes.size(); i++) {
+      result[i] = (Chromosome) m_chromosomes.get(i);
+    }
+    return result;
+  }
+}
