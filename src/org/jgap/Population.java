a7867032ddaea85300022d8f3971e2b0d839c8a1
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-24 17:51:53 +0000

    Added heading comment
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@374 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/Population.java
+++ src/org/jgap/Population.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package org.jgap;
 
 import java.util.*;
@@ -11,7 +29,7 @@ import java.util.*;
  */
 public class Population {
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.6 $";
+  private static final String CVS_REVISION = "$Revision: 1.7 $";
 
   /**
    * The array of Chromosomes that makeup the Genotype's population.
