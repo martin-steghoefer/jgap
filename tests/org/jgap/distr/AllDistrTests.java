eebe76b4f2aca844f55e736e75c35e10ef32b69c
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-25 21:45:11 +0000

    Test suite for package org.jgap.distr
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@390 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/distr/AllDistrTests.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.jgap.distr;
+
+import junit.framework.*;
+
+/**
+ * Test suite for all tests of package org.jgap.distr
+ *
+ * @author Klaus Meffert
+ * @since 1.0
+ */
+public class AllDistrTests
+    extends TestSuite {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public AllDistrTests() {
+  }
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite();
+//    suite.addTest(.suite());
+    return suite;
+  }
+}
