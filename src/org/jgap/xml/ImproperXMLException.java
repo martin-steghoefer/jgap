503b7a04d5721c533992e134104ac1c0d0467dce
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-27 17:25:12 +0000

    no message
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@403 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/xml/ImproperXMLException.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2001-2003 Neil Rotstan
+ *
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.jgap.xml;
+
+/**
+ * An ImproperXMLException will be thrown when an XML document or element is
+ * parsed but is found to be structured improperly or missing required data.
+ * The error message should be consulted for the exact reason the exception
+ * is being thrown.
+ *
+ * @author Neil Rotstan
+ * @since 1.0
+ */
+public class ImproperXMLException
+    extends Exception {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
+
+  /**
+   * Constructs a new ImproperXMLException instance with the given error
+   * message.
+   *
+   * @param a_message An error message describing the reason this exception
+   *                  is being thrown.
+   */
+  public ImproperXMLException(String a_message) {
+    super(a_message);
+  }
+}
