5826bf9545a0780d6e36a9d5b02401b5d81e00bb
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-29 15:31:57 +0000

    changed A comment
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@425 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/eval/AllEvalTests.java
+++ tests/org/jgap/eval/AllEvalTests.java
@@ -24,12 +24,12 @@ import junit.framework.*;
  * Test suite for all tests of package org.jgap.eval
  *
  * @author Klaus Meffert
- * @since 1.0
+ * @since 2.0
  */
 public class AllEvalTests
     extends TestSuite {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   public AllEvalTests() {
   }
