983f2b851583d3040f1c0e798f60b1222aad3489
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-29 11:06:08 +0000

    See request 1075163
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@416 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/eval/PopulationHistoryTest.java
@@ -0,0 +1,28 @@
+package org.jgap.eval;
+
+import junit.framework.*;
+
+/**
+ * Tests for PopulationHistory class
+ * @author Klaus Meffert
+ * @since 1.1
+ */
+public class PopulationHistoryTest
+    extends TestCase {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public PopulationHistoryTest() {
+
+  }
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite(PopulationHistoryTest.class);
+    return suite;
+  }
+
+  public void testConstruct_0() {
+    /**@todo implement*/
+  }
+  /**@todo implement further tests*/
+}
