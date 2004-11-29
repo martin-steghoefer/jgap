dcf0edd8183580bfaed2e9962f3e6b18aaf01c23
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-29 16:18:22 +0000

    Added CVS_REVISION
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@428 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/impl/ReproductionOperator.java
+++ src/org/jgap/impl/ReproductionOperator.java
@@ -18,9 +18,7 @@
 package org.jgap.impl;
 
 import java.util.*;
-
 import org.jgap.*;
-
 
 /**
  * The reproduction operator loops through each of the Chromosomes in the
@@ -35,47 +33,48 @@ import org.jgap.*;
  * @author Klaus Meffert
  * @since 1.0
  */
-public class ReproductionOperator implements GeneticOperator
-{
-    /**
-     * The operate method will be invoked on each of the genetic operators
-     * referenced by the current Configuration object during the evolution
-     * phase. Operators are given an opportunity to run in the order that
-     * they are added to the Configuration. Implementations of this method
-     * may reference the population of Chromosomes as it was at the beginning
-     * of the evolutionary phase and/or they may instead reference the
-     * candidate Chromosomes, which are the results of prior genetic operators.
-     * In either case, only Chromosomes added to the list of candidate
-     * chromosomes will be considered for natural selection. Implementations
-     * should never modify the original population, but should first make copies
-     * of the Chromosomes selected for modification and operate upon the copies.
-     *
-     * @param a_activeConfiguration The current active genetic configuration.
-     * @param a_population The population of chromosomes from the current
-     *                     evolution prior to exposure to any genetic operators.
-     *                     Chromosomes in this array should never be modified.
-     * @param a_candidateChromosomes The pool of chromosomes that are candidates
-     *                               for the next evolved population. Only these
-     *                               chromosomes will go to the natural
-     *                               phase, so it's important to add any
-     *                               modified copies of Chromosomes to this
-     *                               list if it's desired for them to be
-     *                               considered for natural selection.
-     * @author Neil Rotstan
-     * @author Klaus Meffert
-     * @since 1.0
-     */
-    public void operate( final Configuration a_activeConfiguration,
-                         final Population a_population,
-                         final List a_candidateChromosomes )
-    {
-        // Loop over the chromosomes in the population and add each one to
-        // the candidate chromosomes pool so that they'll be considered for
-        // the next phase of evolution. This is technically a no-no, but
-        // refraining from making copies here of every Chromosome will save
-        // significant time and memory.
-        // ----------------------------------------------------------------
-        a_candidateChromosomes.addAll( a_population.getChromosomes());
-    }
-}
+public class ReproductionOperator
+    implements GeneticOperator {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private static final String CVS_REVISION = "$Revision: 1.4 $";
 
+  /**
+   * The operate method will be invoked on each of the genetic operators
+   * referenced by the current Configuration object during the evolution
+   * phase. Operators are given an opportunity to run in the order that
+   * they are added to the Configuration. Implementations of this method
+   * may reference the population of Chromosomes as it was at the beginning
+   * of the evolutionary phase and/or they may instead reference the
+   * candidate Chromosomes, which are the results of prior genetic operators.
+   * In either case, only Chromosomes added to the list of candidate
+   * chromosomes will be considered for natural selection. Implementations
+   * should never modify the original population, but should first make copies
+   * of the Chromosomes selected for modification and operate upon the copies.
+   *
+   * @param a_activeConfiguration The current active genetic configuration.
+   * @param a_population The population of chromosomes from the current
+   *                     evolution prior to exposure to any genetic operators.
+   *                     Chromosomes in this array should never be modified.
+   * @param a_candidateChromosomes The pool of chromosomes that are candidates
+   *                               for the next evolved population. Only these
+   *                               chromosomes will go to the natural
+   *                               phase, so it's important to add any
+   *                               modified copies of Chromosomes to this
+   *                               list if it's desired for them to be
+   *                               considered for natural selection.
+   * @author Neil Rotstan
+   * @author Klaus Meffert
+   * @since 1.0
+   */
+  public void operate(final Configuration a_activeConfiguration,
+                      final Population a_population,
+                      final List a_candidateChromosomes) {
+    // Loop over the chromosomes in the population and add each one to
+    // the candidate chromosomes pool so that they'll be considered for
+    // the next phase of evolution. This is technically a no-no, but
+    // refraining from making copies here of every Chromosome will save
+    // significant time and memory.
+    // ----------------------------------------------------------------
+    a_candidateChromosomes.addAll(a_population.getChromosomes());
+  }
+}
