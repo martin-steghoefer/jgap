aa9c2063f9ca5062bc19ef8dbd2454ae6bbe581e
Klaus Meffert <kontakt@doktor-meffert.de>
2004-11-29 11:08:10 +0000

    Added comments and CVS_REVISION
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@418 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/IPopulationMerger.java
+++ src/org/jgap/distr/IPopulationMerger.java
@@ -1,3 +1,21 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package org.jgap.distr;
 
 import org.jgap.*;
@@ -10,6 +28,8 @@ import org.jgap.*;
  * @since 2.0
  */
 public interface IPopulationMerger {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.2 $";
 
   /**
    * Merges two Population's into one that has the given size.
