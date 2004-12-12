6af53ebc568029ec5fae4719e63808accd9d4ced
Klaus Meffert <kontakt@doktor-meffert.de>
2004-12-12 14:47:35 +0000

    Adapted to chanegd logic in FixedBinaryGene
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@551 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/FixedBinaryGeneTest.java
+++ tests/org/jgap/impl/FixedBinaryGeneTest.java
@@ -19,22 +19,19 @@
 package org.jgap.impl;
 
 import org.jgap.*;
-
 import junit.framework.*;
 
 /**
  * Test class for FixedBinaryGene class
  *
  * @author Klaus Meffert
+ * @author vamsi
  * @since 2.0
  */
 public class FixedBinaryGeneTest
     extends TestCase {
-
-/*@todo implement testcases by modifying the below tests copied from BooleanGene*/
-
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   public FixedBinaryGeneTest() {
   }
@@ -50,7 +47,7 @@ public class FixedBinaryGeneTest
 
   public void testConstruct_0() {
     //following should be possible without exception
-    Gene gene = new FixedBinaryGene(1);
+    FixedBinaryGene gene = new FixedBinaryGene(1);
     gene = new FixedBinaryGene(10);
     gene = new FixedBinaryGene(1000);
     gene = new FixedBinaryGene(100000);
@@ -76,26 +73,77 @@ public class FixedBinaryGeneTest
     }
   }
 
+  /**
+   * @author vamsi
+   */
+  public void testConstruct_3() {
+    //following should be possible without exception
+    int i = 0;
+    FixedBinaryGene gene = new FixedBinaryGene(5);
+    for (i = 0; i < 4; i++) {
+      //assert that we have
+      assertFalse(gene.getBit(i));
+    }
+    assertEquals("[0,0,0,0,0]", gene.toString());
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testConstruct_4() {
+    FixedBinaryGene gene = new FixedBinaryGene(6);
+    assertEquals(1, gene.size());
+    assertEquals(1, (gene.getValue()).length);
+    assertEquals("[0,0,0,0,0,0]", gene.toString());
+  }
+
+  /**
+   * Buffer allocation test case.
+   * @author vamsi
+   */
+  public void testConstruct_5() {
+    FixedBinaryGene gene;
+    gene = new FixedBinaryGene(32);
+    assertEquals(1, gene.size());
+    gene = new FixedBinaryGene(81);
+    assertEquals(3, gene.size());
+  }
+
+  /**
+   * @author vamsi
+   */
   public void testToString_0() {
     Gene gene = new FixedBinaryGene(1);
     gene.setAllele(new int[] {1});
     assertEquals("[1]", gene.toString());
   }
 
+  /**
+   * @author vamsi
+   */
   public void testToString_1() {
     Gene gene = new FixedBinaryGene(3);
     gene.setAllele(new int[] {1, 0, 1});
     assertEquals("[1,0,1]", gene.toString());
   }
 
+  /**
+   * @author vamsi
+   */
   public void testGetAllele_0() {
     Gene gene = new FixedBinaryGene(1);
     int[] value = new int[] {
         0};
     gene.setAllele(value);
-    assertEquals(value, gene.getAllele());
+    assertEquals(value.length,((int[])gene.getAllele()).length);
+    for (int i=0;i<value.length;i++) {
+      assertEquals(value[i], ((int[])gene.getAllele())[i]);
+    }
   }
 
+  /**
+   * @author vamsi
+   */
   public void testGetAllele_1() {
     Gene gene = new FixedBinaryGene(2);
     try {
@@ -107,71 +155,92 @@ public class FixedBinaryGeneTest
     }
   }
 
+  /**
+   * @author vamsi
+   */
   public void testEquals_0() {
-    Gene gene1 = new BooleanGene();
-    Gene gene2 = new BooleanGene();
+    Gene gene1 = new FixedBinaryGene(1);
+    Gene gene2 = new FixedBinaryGene(1);
     assertTrue(gene1.equals(gene2));
   }
 
+  /**
+   * @author vamsi
+   */
   public void testEquals_1() {
-    Gene gene1 = new BooleanGene();
+    Gene gene1 = new FixedBinaryGene(1);
     assertFalse(gene1.equals(null));
   }
 
+  /**
+   * @author vamsi
+   */
   public void testEquals_2() {
-    Gene gene1 = new BooleanGene();
-    gene1.setAllele(new Boolean(true));
-    Gene gene2 = new BooleanGene();
-    gene2.setAllele(new Boolean(false));
+    Gene gene1 = new FixedBinaryGene(2);
+    gene1.setAllele(new int[] {1, 0});
+    Gene gene2 = new FixedBinaryGene(2);
+    gene2.setAllele(new int[] {0, 1});
     assertFalse(gene1.equals(gene2));
     assertFalse(gene2.equals(gene1));
   }
 
+  /**
+   * @author vamsi
+   */
   public void testEquals_3() {
-    Gene gene1 = new BooleanGene();
+    Gene gene1 = new FixedBinaryGene(5);
     assertFalse(gene1.equals(new IntegerGene()));
   }
 
+  /**
+   * @author vamsi
+   */
   public void testIntValues_0() {
-    /**@todo implement*/
-
-    BooleanGene gene1 = new BooleanGene();
-    gene1.setAllele(new Boolean(true));
-    assertEquals(true, gene1.booleanValue());
+    FixedBinaryGene gene1 = new FixedBinaryGene(4);
+    assertFalse(gene1.getIntValues() == null);
   }
 
+  /**
+   * @author vamsi
+   */
   public void testIntValues_1() {
-    /**@todo implement*/
-
-    BooleanGene gene1 = new BooleanGene();
-    gene1.setAllele(new Boolean(false));
-    assertEquals(false, gene1.booleanValue());
+    FixedBinaryGene gene1 = new FixedBinaryGene(2);
+    int[] values = gene1.getIntValues();
+    int i;
+    for (i = 0; i < values.length; i++) {
+      assertEquals(0, values[i]);
+    }
   }
 
+  /**
+   * @author vamsi
+   */
   public void testIntValues_2() {
-    /**@todo implement*/
-
-    BooleanGene gene1 = new BooleanGene();
-    gene1.setAllele(null);
-    try {
-      assertEquals(true, gene1.booleanValue());
-      fail();
-    }
-    catch (NullPointerException nullex) {
-      ; //this is OK
-    }
+    FixedBinaryGene gene1 = new FixedBinaryGene(3);
+    gene1.setAllele(new int[] {0, 1, 0});
+    assertEquals(false, gene1.getBit(0));
+    assertEquals(true, gene1.getBit(1));
+    assertEquals(false, gene1.getBit(2));
+    assertEquals(3, gene1.getLength());
   }
 
+  /**
+   * @author vamsi
+   */
   public void testSetAllele_0() {
     Gene gene1 = new FixedBinaryGene(1);
     try {
       gene1.setAllele(null);
       fail();
-    } catch (IllegalArgumentException iex) {
-      ;//this is OK
+    }
+    catch (IllegalArgumentException iex) {
+      ; //this is OK
     }
   }
 
+  /**
+   * @author vamsi
+   */
   public void testSetAllele_1() {
     Gene gene1 = new FixedBinaryGene(1);
     try {
@@ -185,133 +254,210 @@ public class FixedBinaryGeneTest
 
   /**
    * Set Allele to int values, no exception should occur
+   * @author vamsi
    */
   public void testSetAllele_2() {
     Gene gene1 = new FixedBinaryGene(3);
     gene1.setAllele(new int[] {0, 0, 1});
   }
 
-  public void testCompareTo_0() {
-    /**@todo implement*/
-  }
+  /***
+   * The implementation should throw an exception if the alle size is more
+   * than the size of the created gene.
+   * @author vamsi
+   */
+  public void testSetAllele_3() {
+    FixedBinaryGene gene1 = new FixedBinaryGene(3);
+    try {
+      gene1.setAllele(new int[] {0, 1, 1, 1, 1, 1});
+      fail();
+    }
+    catch (Exception e) {
+      /*Some Exception should be thrown*/
 
-  public void testCompareTo_1() {
-    /**@todo implement*/
-           Gene gene1 = new BooleanGene();
-        Gene gene2 = new BooleanGene();
-        assertEquals(0, gene1.compareTo(gene2));
-        assertEquals(0, gene2.compareTo(gene1));
-  }
-
-  public void testCompareTo_2() {
-    /**@todo implement*/
-        Gene gene1 = new BooleanGene();
-        gene1.setAllele(new Boolean(true));
-        Gene gene2 = new BooleanGene();
-        gene2.setAllele(new Boolean(true));
-        assertEquals(0, gene1.compareTo(gene2));
-        assertEquals(0, gene2.compareTo(gene1));
-        gene1.setAllele(new Boolean(false));
-        gene2.setAllele(new Boolean(false));
-        assertEquals(0, gene1.compareTo(gene2));
-        assertEquals(0, gene2.compareTo(gene1));
-  }
-
-  public void testCompareTo_3() {
-    /**@todo implement*/
-    Gene gene1 = new BooleanGene();
-    gene1.setAllele(new Boolean(true));
-    Gene gene2 = new BooleanGene();
-    gene2.setAllele(new Boolean(false));
-    assertEquals(1, gene1.compareTo(gene2));
-    assertEquals( -1, gene2.compareTo(gene1));
-  }
-
-  public void testApplyMutation_0() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(true));
-    gene.applyMutation(0, 0.0d);
-    assertEquals(true, gene.booleanValue());
-  }
-
-  public void testApplyMutation_1() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(true));
-    gene.applyMutation(1, 0.000001d); //index 1 should be ignored
-    assertEquals(true, gene.booleanValue());
-  }
-
-  public void testApplyMutation_2() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(true));
-    gene.applyMutation(333, -0.000001d); //index 333 should be ignored
-    assertEquals(false, gene.booleanValue());
-  }
-
-  public void testApplyMutation_3() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(true));
-    gene.applyMutation(0, -1.0d);
-    assertEquals(false, gene.booleanValue());
-  }
-
-  public void testApplyMutation_4() {
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(true));
-    gene.applyMutation(0, -2.0d);
-    assertEquals(false, gene.booleanValue());
-  }
-
-  public void testApplyMutation_5() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(true));
-    gene.applyMutation(0, 2.0d);
-    assertEquals(true, gene.booleanValue());
-  }
-
-  public void testApplyMutation_6() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(false));
-    gene.applyMutation(0, 2.0d);
-    assertEquals(true, gene.booleanValue());
-  }
-
-  public void testApplyMutation_7() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(false));
-    gene.applyMutation(0, -1.0d);
-    assertEquals(false, gene.booleanValue());
-  }
-
-  public void testApplyMutation_8() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(false));
-    gene.applyMutation(22, -0.5d); //22 should be ignored
-    assertEquals(false, gene.booleanValue());
-  }
-
-  public void testApplyMutation_9() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(false));
-    gene.applyMutation(22, 0.5d); //22 should be ignored
-    assertEquals(true, gene.booleanValue());
+    }
   }
 
   /**
+   * @author vamsi
+   */
+  public void testSetAllele_4() {
+    FixedBinaryGene gene1 = new FixedBinaryGene(4);
+    try {
+      gene1.setAllele(new int[] {0, 3, 1, 4});
+      fail();
+    }
+    catch (Exception e) {
+      /**
+       *This is ok
+       */
+    }
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testSetAllele_5() {
+    FixedBinaryGene gene1 = new FixedBinaryGene(4);
+    try {
+      gene1.setAllele(new int[] {0, 0});
+      fail();
+    }
+    catch (Exception e) {
+      /*This is ok.*/
+    }
+
+  }
+
+  /**
+   *Comparision should return 0 if same, -1 if less 1 if more
+   * @author vamsi
+   */
+  public void testCompareTo_0() {
+    FixedBinaryGene gene1 = new FixedBinaryGene(4);
+    FixedBinaryGene gene2 = new FixedBinaryGene(4);
+
+    gene1.setAllele(new int[] {1, 0, 1, 0});
+    gene2.setAllele(new int[] {1, 1, 0, 1});
+
+    assertEquals(1, gene1.compareTo(null));
+    assertEquals( -1, gene1.compareTo(gene2));
+    assertEquals(1, gene2.compareTo(gene1));
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testCompareTo_1() {
+    FixedBinaryGene gene1 = new FixedBinaryGene(3);
+    FixedBinaryGene gene2 = new FixedBinaryGene(3);
+
+    assertEquals(0, gene1.compareTo(gene2));
+    assertEquals(0, gene2.compareTo(gene1));
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testCompareTo_2() {
+
+    FixedBinaryGene gene1 = new FixedBinaryGene(3);
+    FixedBinaryGene gene2 = new FixedBinaryGene(3);
+    gene1.setAllele(new int[] {1, 1, 1});
+    gene2.setAllele(new int[] {1, 1, 1});
+
+    assertEquals(0, gene1.compareTo(gene2));
+    assertEquals(0, gene2.compareTo(gene1));
+
+    gene1.setAllele(new int[] {0, 0, 0});
+    gene2.setAllele(new int[] {0, 0, 0});
+
+    assertEquals(0, gene1.compareTo(gene2));
+    assertEquals(0, gene2.compareTo(gene1));
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testCompareTo_3() {
+    FixedBinaryGene gene1 = new FixedBinaryGene(3);
+    BooleanGene gene2 = new BooleanGene();
+    try {
+      gene1.compareTo(gene2);
+      fail();
+    }
+    catch (Exception e) {
+      /*Should compare only Fixed Binary Genes*/
+    }
+
+    try {
+      gene1.compareTo(new Integer(3));
+
+    }
+    catch (Exception e) {
+      /*Should compare only FixedBinary Gene's*/
+    }
+  }
+
+  /***
+   * Apply Mutation (index,percentage). if >0 make 1(0) if <0 make 0(1)
+   * @author vamsi
+   */
+  public void testApplyMutation_0() {
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    gene.setAllele(new int[] {0, 0, 1, 1});
+    gene.applyMutation(0, 0.0d);
+    assertEquals("[0,0,1,1]", gene.toString());
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testApplyMutation_1() {
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    gene.setAllele(new int[] {0, 0, 1, 0});
+    gene.applyMutation(1, 0.000001d);
+    assertEquals("[0,1,1,0]", gene.toString());
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testApplyMutation_2() {
+    FixedBinaryGene gene = new FixedBinaryGene(5);
+    gene.setAllele(new int[] {1, 0, 1, 0, 1});
+    try {
+      //index size is greater
+      gene.applyMutation(333, -0.000001d);
+      fail();
+    }
+    catch (Exception E) {
+      /*Fine*/
+    }
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testApplyMutation_3() {
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    gene.setAllele(new int[] {1, 1, 0, 1});
+    gene.applyMutation(0, -1.0d);
+    assertEquals("[0,1,0,1]", gene.toString());
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testApplyMutation_4() {
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    gene.setAllele(new int[] {0, 1, 0, 1});
+    gene.applyMutation(0, -2.0d);
+    gene.applyMutation(3, 2.0d);
+    gene.applyMutation(1, -4.0d);
+    assertEquals("[0,0,0,1]", gene.toString());
+  }
+
+  /**
+   * @author vamsi
+   */
+  public void testApplyMutation_5() {
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    gene.setAllele(new int[] {1, 1, 1, 1});
+    gene.applyMutation(0, 2.0d);
+    gene.applyMutation(1, 2.0d);
+    gene.applyMutation(2, 2.0d);
+    gene.applyMutation(3, 2.0d);
+    assertEquals("[1,1,1,1]", gene.toString());
+  }
+
+  /**
+   * @author vamsi
    * @since 2.0
    */
   public void testSetValueFromPersistentRepresentation_0() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
+
+    FixedBinaryGene gene = new FixedBinaryGene(4);
     try {
       gene.setValueFromPersistentRepresentation(null);
       fail();
@@ -323,73 +469,58 @@ public class FixedBinaryGeneTest
 
   /**
    * @since 2.0
+   * @throws Exception
    */
   public void testSetValueFromPersistentRepresentation_1()
       throws Exception {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setValueFromPersistentRepresentation("null");
-    assertEquals(null, gene.getAllele());
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    try {
+      gene.setValueFromPersistentRepresentation("null");
+      fail();
+    }catch (UnsupportedRepresentationException uex) {
+      //this is OK
+    }
   }
 
   /**
+   * @author vamsi
    * @since 2.0
+   * @throws Exception
    */
   public void testSetValueFromPersistentRepresentation_2()
       throws Exception {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setValueFromPersistentRepresentation("true");
-    assertEquals(Boolean.TRUE, gene.getAllele());
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    try {
+      gene.setValueFromPersistentRepresentation("[1,1,1,1]");
+    }
+    catch (UnsupportedRepresentationException uex) {
+      /*Unsupported Exception*/
+    }
   }
 
   /**
+   * @author vamsi
    * @since 2.0
+   * @throws Exception
    */
   public void testSetValueFromPersistentRepresentation_3()
       throws Exception {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setValueFromPersistentRepresentation("false");
-    assertEquals(Boolean.FALSE, gene.getAllele());
+    FixedBinaryGene gene = new FixedBinaryGene(4);
+    try {
+      gene.setValueFromPersistentRepresentation("[0,0,0,0]");
+    }
+    catch (UnsupportedRepresentationException uex) {
+      /*Unsupported Exception*/
+    }
   }
 
   /**
+   * @author vamsi
    * @since 2.0
    */
   public void testSetValueFromPersistentRepresentation_4() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    try {
-      gene.setValueFromPersistentRepresentation("True");
-      fail();
-    }
-    catch (UnsupportedRepresentationException uex) {
-      ; //this is OK
-    }
-  }
 
-  /**
-   * @since 2.0
-   */
-  public void testSetValueFromPersistentRepresentation_5() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    try {
-      gene.setValueFromPersistentRepresentation("False");
-      fail();
-    }
-    catch (UnsupportedRepresentationException uex) {
-      ; //this is OK
-    }
-  }
-
-  /**
-   * @since 2.0
-   */
-  public void testSetValueFromPersistentRepresentation_6() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
+    FixedBinaryGene gene = new FixedBinaryGene(1);
     try {
       gene.setValueFromPersistentRepresentation("X");
       fail();
@@ -399,26 +530,42 @@ public class FixedBinaryGeneTest
     }
   }
 
+  /**
+   * @author vamsi
+   */
   public void testGetPersistentRepresentation_0() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(true));
+    FixedBinaryGene gene = new FixedBinaryGene(3);
+    gene.setAllele(new int[] {1, 0, 1});
     String s = gene.getPersistentRepresentation();
-    assertEquals("true", s);
+    assertEquals("[1,0,1]", s);
   }
 
-  public void testGetPersistentRepresentation_1() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
-    gene.setAllele(new Boolean(false));
-    String s = gene.getPersistentRepresentation();
-    assertEquals("false", s);
+  /**
+   * @throws Exception
+   * @author vamsi
+   * @since 2.0
+   */
+  public void testGetPersistentRepresentation_1()
+      throws Exception {
+    FixedBinaryGene gene = new FixedBinaryGene(3);
+    try {
+      gene.setValueFromPersistentRepresentation(null);
+      fail();
+    }catch (UnsupportedRepresentationException uex) {
+      ;//this is OK
+    }
   }
 
-  public void testGetPersistentRepresentation_2() {
-    /**@todo implement*/
-    BooleanGene gene = new BooleanGene();
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   * @since 2.0
+   */
+  public void testGetPersistentRepresentation_2()
+      throws Exception {
+    FixedBinaryGene gene = new FixedBinaryGene(3);
     String s = gene.getPersistentRepresentation();
-    assertEquals("null", s);
+    assertEquals("[0,0,0]", s);
   }
+
 }
