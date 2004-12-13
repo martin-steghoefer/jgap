a41d908d02e408025840c71a51257483d8a421ac
Klaus Meffert <kontakt@doktor-meffert.de>
2004-12-13 14:25:12 +0000

    New test cases
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@578 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/impl/DefaultCrossoverRateCalculatorTest.java
@@ -0,0 +1,77 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.jgap.impl;
+
+import org.jgap.*;
+import junit.framework.*;
+
+/**
+ * Tests for DefaultCrossoverRateCalculator class
+ *
+ * @author Klaus Meffert
+ * @since 2.0
+ */
+public class DefaultCrossoverRateCalculatorTest
+    extends TestCase {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private static final String CVS_REVISION = "$Revision: 1.1 $";
+
+  public DefaultCrossoverRateCalculatorTest() {
+  }
+
+  public void setUp() {
+    Genotype.setConfiguration(null);
+  }
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite(DefaultCrossoverRateCalculatorTest.class);
+    return suite;
+  }
+
+  public void testCalculateCurrentRate_0()
+      throws Exception {
+    Configuration conf = new DefaultConfiguration();
+    Genotype.setConfiguration(conf);
+    IUniversalRateCalculator calc = new DefaultCrossoverRateCalculator();
+    Gene gene = new IntegerGene(1, 5);
+    Chromosome chrom = new Chromosome(gene, 50);
+    conf.setSampleChromosome(chrom);
+    int rate = calc.calculateCurrentRate();
+    assertEquals(conf.getChromosomeSize(), rate);
+    chrom = new Chromosome(gene, 30);
+    conf.setSampleChromosome(chrom);
+    rate = calc.calculateCurrentRate();
+    assertEquals(conf.getChromosomeSize(), rate);
+  }
+
+  /**
+   * If there are zero chromosomes in the config., the crossover rate
+   * nevertheless should be 1, because Random needs positive integers as input
+   * (see CrossoverOperator.operate for calling Random class)
+   * @throws Exception
+   */
+  public void testCalculateCurrentRate_1()
+      throws Exception {
+    Configuration conf = new DefaultConfiguration();
+    Genotype.setConfiguration(conf);
+    IUniversalRateCalculator calc = new DefaultCrossoverRateCalculator();
+    int rate = calc.calculateCurrentRate();
+    assertEquals(1, rate);
+  }
+}
---
+++ tests/org/jgap/impl/FittestPopulationMergerTest.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * JGAP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with JGAP; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.jgap.impl;
+
+import java.util.*;
+
+import org.jgap.*;
+
+import junit.framework.*;
+
+/**
+ * Tests for FittestPopulationMerger class
+ *
+ * @since 2.0
+ * @author Klaus Meffert
+ */
+public class FittestPopulationMergerTest
+    extends TestCase {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public FittestPopulationMergerTest() {
+  }
+
+  public void setUp() {
+    Genotype.setConfiguration(null);
+  }
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite(FittestPopulationMergerTest.class);
+    return suite;
+  }
+
+  public void testConstruct_0() {
+    /**@todo implement*/
+  }
+
+  public void testMergePopulations_0() {
+    /**@todo implement*/
+  }
+
+}
