65f72e542ee8b31be498953261518191ce6ed9c7
Klaus Meffert <kontakt@doktor-meffert.de>
2004-12-14 17:07:19 +0000

    Added a test case
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@594 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/CrossoverOperatorTest.java
+++ tests/org/jgap/impl/CrossoverOperatorTest.java
@@ -19,9 +19,7 @@
 package org.jgap.impl;
 
 import java.util.*;
-
 import org.jgap.*;
-
 import junit.framework.*;
 
 /**
@@ -32,9 +30,8 @@ import junit.framework.*;
  */
 public class CrossoverOperatorTest
     extends TestCase {
-
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.9 $";
+  private static final String CVS_REVISION = "$Revision: 1.10 $";
 
   public CrossoverOperatorTest() {
   }
@@ -44,7 +41,14 @@ public class CrossoverOperatorTest
     return suite;
   }
 
-  public void testOperate_0() throws Exception {
+  /**
+   * @throws Exception
+   *
+   * @author Klaus Meffert
+   * @since 1.1
+   */
+  public void testOperate_0()
+      throws Exception {
     DefaultConfiguration conf = new DefaultConfiguration();
     Genotype.setConfiguration(conf);
     RandomGeneratorForTest rand = new RandomGeneratorForTest();
@@ -86,5 +90,84 @@ public class CrossoverOperatorTest
     target = (Chromosome) chroms.get(3);
     assertEquals(8, ( (Integer) target.getGene(0).getAllele()).intValue());
   }
-}
 
+  /**
+   * Tests if crossing over produces same results for two operate-runs
+   * @throws Exception
+   *
+   * @author Klaus Meffert
+   * @since 2.0
+   */
+  public void testOperate_1()
+      throws Exception {
+    DefaultConfiguration conf = new DefaultConfiguration();
+    GeneticOperator op = new CrossoverOperator();
+    Genotype.setConfiguration(conf);
+    conf.addGeneticOperator(op);
+    RandomGeneratorForTest rand = new RandomGeneratorForTest();
+    rand.setNextIntSequence(new int[] {
+                            0, 1, 0, 1, 2});
+    conf.setRandomGenerator(rand);
+    conf.setFitnessFunction(new TestFitnessFunction());
+    Gene sampleGene = new IntegerGene(1, 10);
+    Chromosome chrom = new Chromosome(sampleGene, 3);
+    conf.setSampleChromosome(chrom);
+    conf.setPopulationSize(6);
+    Gene cgene1 = new IntegerGene(1, 10);
+    cgene1.setAllele(new Integer(6));
+    Gene[] genes1 = new Gene[] {
+        cgene1};
+    Chromosome chrom1 = new Chromosome(genes1);
+    Gene cgene2 = new IntegerGene(1, 10);
+    cgene2.setAllele(new Integer(8));
+    Gene[] genes2 = new Gene[] {
+        cgene2};
+    Chromosome chrom2 = new Chromosome(genes2);
+    Chromosome[] population = new Chromosome[] {
+        chrom1, chrom2};
+    List chroms = new Vector();
+    Gene gene1 = new IntegerGene(1, 10);
+    gene1.setAllele(new Integer(5));
+    chroms.add(gene1);
+    Gene gene2 = new IntegerGene(1, 10);
+    gene2.setAllele(new Integer(7));
+    chroms.add(gene2);
+    Gene gene3 = new IntegerGene(1, 10);
+    gene3.setAllele(new Integer(4));
+    chroms.add(gene3);
+    Chromosome[] population2 = (Chromosome[]) population.clone();
+    op.operate(new Population(population), chroms);
+    op.operate(new Population(population2), chroms);
+    assertTrue(isChromosomesEqual(population, population2));
+  }
+
+  public static boolean isChromosomesEqual(Chromosome[] list1,
+                                           Chromosome[] list2) {
+    if (list1 == null) {
+      if (list2 == null) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    else if (list2 == null) {
+      return false;
+    }
+    else {
+      if (list1.length != list2.length) {
+        return false;
+      }
+      else {
+        for (int i = 0; i < list1.length; i++) {
+          Chromosome c1 = (Chromosome) list1[i];
+          Chromosome c2 = (Chromosome) list2[i];
+          if (!c1.equals(c2)) {
+            return false;
+          }
+        }
+        return true;
+      }
+    }
+  }
+}
