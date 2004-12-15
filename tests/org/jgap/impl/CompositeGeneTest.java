459f2e7a4efdc881ab36b8ce98ecd6bf17bbd541
Audrius Meskauskas <audrius.meskauskas@meska.info>
2004-12-15 20:40:16 +0000

    I see you removed spaces before and after ( ) in CompositeGene.toString(), but they stayed in the test, so the test fails. I removed spaces in the test also.
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@610 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/CompositeGeneTest.java
+++ tests/org/jgap/impl/CompositeGeneTest.java
@@ -33,7 +33,7 @@ public class CompositeGeneTest
     extends TestCase {
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.12 $";
+  private final static String CVS_REVISION = "$Revision: 1.13 $";
 
   //delta for distinguishing whether a value is to be interpreted as zero
   private static final double DELTA = 0.000001d;
@@ -93,14 +93,12 @@ public class CompositeGeneTest
     Gene newGene1 = new DoubleGene();
     newGene1.setAllele(new Double(47.123d));
     gene.addGene(newGene1, false);
-    // assertEquals(newGene1.toString(), gene.toString());
-    // after my Audrius modification the following is correct instead:
-    assertEquals(" ( "+newGene1.toString()+" ) ", gene.toString());
+    assertEquals("("+newGene1.toString()+")", gene.toString());
     Gene newGene2 = new IntegerGene();
     newGene2.setAllele(new Integer(23456));
     gene.addGene(newGene2, false);
-    assertEquals(" ( "+newGene1.toString() + gene.GENE_DELIMITER +
-                 newGene2.toString()+" ) ", gene.toString());
+    assertEquals("("+newGene1.toString() + gene.GENE_DELIMITER +
+                 newGene2.toString()+")", gene.toString());
   }
 
   public void testGetAllele_0() {
