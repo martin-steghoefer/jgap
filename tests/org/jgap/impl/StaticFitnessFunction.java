58d8792801951276c85471bb3279b0fe8f3c4bc3
Klaus Meffert <kontakt@doktor-meffert.de>
2004-12-30 12:27:36 +0000

    comments
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@678 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/StaticFitnessFunction.java
+++ tests/org/jgap/impl/StaticFitnessFunction.java
@@ -1,19 +1,11 @@
 /*
  * This file is part of JGAP.
  *
- * JGAP is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
- * JGAP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- *
- * You should have received a copy of the GNU Lesser Public License
- * along with JGAP; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
 package org.jgap.impl;
 
@@ -31,7 +23,7 @@ public class StaticFitnessFunction
     extends FitnessFunction {
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.4 $";
+  private final static String CVS_REVISION = "$Revision: 1.5 $";
 
   /**
    * @since 2.0 (until 1.1: type int)
@@ -40,6 +32,8 @@ public class StaticFitnessFunction
 
   /**
    * @param staticFitnessValue double
+   *
+   * @author Klaus Meffert
    * @since 2.0 (until 1.1: input type int)
    */
   public StaticFitnessFunction(double staticFitnessValue) {
@@ -49,6 +43,8 @@ public class StaticFitnessFunction
   /**
    * @param chrom Chromosome
    * @return double
+   *
+   * @author Klaus  Meffert
    * @since 2.0 (until 1.1: return type int)
    */
   public double evaluate(Chromosome chrom) {
@@ -57,7 +53,9 @@ public class StaticFitnessFunction
   }
 
   /**
-   * @return double
+   * @return double typed fitness value
+   *
+   * @author Klaus Meffert
    * @since 2.0 (until 1.1: return type int)
    */
   public double getStaticFitnessValue() {
@@ -66,6 +64,8 @@ public class StaticFitnessFunction
 
   /**
    * @param staticFitnessValue double
+   *
+   * @author Klaus Meffert
    * @since 2.0 (until 1.1: type int)
    */
   public void setStaticFitnessValue(double staticFitnessValue) {
