530e82a14dcae3bbb75abaf32604d0bd3accd429
Klaus Meffert <kontakt@doktor-meffert.de>
2004-12-30 12:18:58 +0000

    Added tests
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@672 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/AllTests.java
+++ tests/org/jgap/AllTests.java
@@ -1,21 +1,12 @@
 /*
  * This file is part of JGAP.
  *
- * JGAP is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
- * JGAP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- *
- * You should have received a copy of the GNU Lesser Public License
- * along with JGAP; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
-
 package org.jgap;
 
 import org.jgap.data.*;
@@ -25,6 +16,7 @@ import org.jgap.event.*;
 import org.jgap.ext.*;
 import org.jgap.gp.*;
 import org.jgap.impl.*;
+import org.jgap.supergenes.*;
 import org.jgap.xml.*;
 
 import junit.framework.*;
@@ -43,7 +35,7 @@ public class AllTests
     extends TestSuite {
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.8 $";
+  private final static String CVS_REVISION = "$Revision: 1.9 $";
 
   public AllTests() {
   }
@@ -59,6 +51,7 @@ public class AllTests
     suite.addTest(AllGPTests.suite());
     suite.addTest(AllImplTests.suite());
     suite.addTest(AllXMLTests.suite());
+    suite.addTest(AllSupergenesTests.suite());
     return suite;
   }
 }
