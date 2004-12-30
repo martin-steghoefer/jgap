2ee7860bd47739f14b7d160ee07ad0b68e2cb588
Klaus Meffert <kontakt@doktor-meffert.de>
2004-12-30 12:25:07 +0000

    lincense information
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@676 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/xml/XMLDocumentBuilder.java
+++ src/org/jgap/xml/XMLDocumentBuilder.java
@@ -1,21 +1,12 @@
 /*
  * This file is part of JGAP.
  *
- * JGAP is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
- * JGAP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- *
- * You should have received a copy of the GNU Lesser Public License
- * along with JGAP; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
-
 package org.jgap.xml;
 
 import org.jgap.data.*;
@@ -32,7 +23,7 @@ import javax.xml.parsers.*;
 public class XMLDocumentBuilder
     extends DocumentBuilderBase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.4 $";
+  private final static String CVS_REVISION = "$Revision: 1.5 $";
 
   protected void setAttribute(Object xmlElement, String key, String value) {
     ( (Element) xmlElement).setAttribute(key, value);
