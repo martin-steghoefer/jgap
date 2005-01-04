53eee3bc41c861f8483879664788e43bc0f52909
Klaus Meffert <kontakt@doktor-meffert.de>
2005-01-04 22:31:08 +0000

    Re-added due to problems with CVS
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@705 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/perf/TestOverallPerformance.java
@@ -0,0 +1,174 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.perf;
+
+import java.util.*;
+
+import org.jgap.*;
+import org.jgap.impl.*;
+
+/**
+ * This class provides an implementation of an overall performance test.
+ *  To obtain this, the provided example has been modified slightly, regarding
+ * the random number generator. We use a static number generator here which does
+ * not deserve the name "random generator". With that we have a determined
+ * calculation path that results in reproducable results.
+ * By executing the example several times we get a performance measurement.
+ * The measured time has to be compared to other results manually as with
+ * different hardware equipment the numbers vary a lot.
+ *
+ * @author Klaus Meffert
+ * @since 2.0
+ */
+public class TestOverallPerformance {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.7 $";
+
+  /**
+   * The total number of times we'll let the population evolve.
+   */
+  private static final int MAX_ALLOWED_EVOLUTIONS = 1000;
+
+  /**
+   * Executes the genetic algorithm to determine the minimum number of
+   * coins necessary to make up the given target amount of change. The
+   * solution will then be written to System.out.
+   *
+   * @param a_targetChangeAmount The target amount of change for which this
+   *                             method is attempting to produce the minimum
+   *                             number of coins.
+   *
+   * @throws Exception
+   */
+  public void makeChangeForAmount(int a_targetChangeAmount)
+      throws
+      Exception {
+    // Start with a DefaultConfiguration, which comes setup with the
+    // most common settings.
+    // -------------------------------------------------------------
+    Configuration conf = new DefaultConfiguration();
+    RandomGeneratorForTest gen = new RandomGeneratorForTest();
+    gen.setNextDouble(0.5d);
+    gen.setNextBoolean(true);
+    gen.setNextInt(3);
+    gen.setNextFloat(0.7f);
+    gen.setNextLong(6);
+    conf.setRandomGenerator(gen);
+    // Set the fitness function we want to use, which is our
+    // MinimizingMakeChangeFitnessFunction. We construct it with
+    // the target amount of change passed in to this method.
+    // ---------------------------------------------------------
+    FitnessFunction myFunc =
+        new TestOverallPerformanceFitnessFunc(a_targetChangeAmount);
+    conf.setFitnessFunction(myFunc);
+    // Now we need to tell the Configuration object how we want our
+    // Chromosomes to be setup. We do that by actually creating a
+    // sample Chromosome and then setting it on the Configuration
+    // object. As mentioned earlier, we want our Chromosomes to each
+    // have four genes, one for each of the coin types. We want the
+    // values (alleles) of those genes to be integers, which represent
+    // how many coins of that type we have. We therefore use the
+    // IntegerGene class to represent each of the genes. That class
+    // also lets us specify a lower and upper bound, which we set
+    // to sensible values for each coin type.
+    // Here we use "fantasy" coins just to have more genes and bloat the time
+    // consumed for test performance test!
+    // --------------------------------------------------------------
+    Gene[] sampleGenes = new Gene[10];
+    sampleGenes[0] = new IntegerGene(0, 3); // Quarters
+    sampleGenes[1] = new IntegerGene(0, 2); // Dimes
+    sampleGenes[2] = new IntegerGene(0, 1); // Nickels
+    sampleGenes[3] = new IntegerGene(0, 4); // Pennies
+    sampleGenes[4] = new IntegerGene(0, 3); // A
+    sampleGenes[5] = new IntegerGene(0, 1); // B
+    sampleGenes[6] = new IntegerGene(0, 1); // C
+    sampleGenes[7] = new IntegerGene(0, 2); // D
+    sampleGenes[8] = new IntegerGene(0, 3); // E
+    sampleGenes[9] = new IntegerGene(0, 1); // F
+    Chromosome sampleChromosome = new Chromosome(sampleGenes);
+    conf.setSampleChromosome(sampleChromosome);
+    // Finally, we need to tell the Configuration object how many
+    // Chromosomes we want in our population. The more Chromosomes,
+    // the larger number of potential solutions (which is good for
+    // finding the answer), but the longer it will take to evolve
+    // the population (which could be seen as bad). We'll just set
+    // the population size to 10000 here. It is that big because of performance
+    // test issues!
+    // ------------------------------------------------------------
+    conf.setPopulationSize(10000);
+    // Create random initial population of Chromosomes.
+    // ------------------------------------------------
+    Genotype population = Genotype.randomInitialGenotype(conf);
+    // Evolve the population. Since we don't know what the best answer
+    // is going to be, we just evolve the max number of times.
+    // ---------------------------------------------------------------
+    for (int i = 0; i < MAX_ALLOWED_EVOLUTIONS; i++) {
+      population.evolve();
+    }
+    // Display the best solution we found.
+    // -----------------------------------
+    Chromosome bestSolutionSoFar = population.getFittestChromosome();
+    /*
+        System.out.println("The best solution contained the following: ");
+        System.out.println("\t" +
+                           TestOverallPerformanceFitnessFunc.
+                           getNumberOfCoinsAtGene(
+            bestSolutionSoFar, 0) + " quarters.");
+        System.out.println("\t" +
+                           TestOverallPerformanceFitnessFunc.
+                           getNumberOfCoinsAtGene(
+            bestSolutionSoFar, 1) + " dimes.");
+        System.out.println("\t" +
+                           TestOverallPerformanceFitnessFunc.
+                           getNumberOfCoinsAtGene(
+            bestSolutionSoFar, 2) + " nickels.");
+        System.out.println("\t" +
+                           TestOverallPerformanceFitnessFunc.
+                           getNumberOfCoinsAtGene(
+            bestSolutionSoFar, 3) + " pennies.");
+        System.out.println("For a total of " +
+                           TestOverallPerformanceFitnessFunc.amountOfChange(
+            bestSolutionSoFar) + " cents in " +
+                           TestOverallPerformanceFitnessFunc.
+                           getTotalNumberOfCoins(
+            bestSolutionSoFar) + " coins.");
+     */
+  }
+
+  /**
+   * Execute the performance test
+   *
+   * @author Klaus Meffert
+   * @since 2.0
+   */
+  public static void main(String[] args)
+      throws Exception {
+    final int amount = 287;
+    final int numRuns = 20;
+    long starttime, timeMillis;
+    System.out.println("Test started.");
+    // get current time
+    starttime = getCurrentMilliseconds();
+    for (int i = 0; i < numRuns; i++) {
+      TestOverallPerformance runner = new TestOverallPerformance();
+      runner.makeChangeForAmount(amount);
+    }
+    // calculate time of run
+    timeMillis = getCurrentMilliseconds() - starttime;
+    System.out.println("Overall time needed for executing performance test: "
+                       + timeMillis + " [millisecs]");
+  }
+
+  private static long getCurrentMilliseconds() {
+    Calendar cal = Calendar.getInstance(TimeZone.getDefault());
+    return cal.getTimeInMillis();
+  }
+}
---
+++ src/perf/TestOverallPerformanceFitnessFunc.java
@@ -0,0 +1,139 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.perf;
+
+import org.jgap.*;
+
+/**
+ * Sample fitness function for the MakeChange example.
+ *
+ * @author Klaus Meffert
+ * @since 2.0
+ */
+public class TestOverallPerformanceFitnessFunc
+    extends FitnessFunction {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.7 $";
+
+  private final int m_targetAmount;
+
+  public TestOverallPerformanceFitnessFunc(int a_targetAmount) {
+    if (a_targetAmount < 1 || a_targetAmount > 999) {
+      throw new IllegalArgumentException(
+          "Change amount must be between 1 and 999 cents.");
+    }
+    m_targetAmount = a_targetAmount;
+  }
+
+  /**
+   * Determine the fitness of the given Chromosome instance. The higher the
+   * return value, the more fit the instance. This method should always
+   * return the same fitness value for two equivalent Chromosome instances.
+   *
+   * @param a_subject The Chromosome instance to evaluate.
+   *
+   * @return A positive integer reflecting the fitness rating of the given
+   *         Chromosome.
+   */
+  public double evaluate(Chromosome a_subject) {
+    // The fitness value measures both how close the value is to the
+    // target amount supplied by the user and the total number of coins
+    // represented by the solution. We do this in two steps: first,
+    // we consider only the represented amount of change vs. the target
+    // amount of change and return higher fitness values for amounts
+    // closer to the target, and lower fitness values for amounts further
+    // away from the target. If the amount equals the target, then we go
+    // to step 2, which returns a higher fitness value for solutions
+    // representing fewer total coins, and lower fitness values for
+    // solutions representing more total coins.
+    // ------------------------------------------------------------------
+    int changeAmount = amountOfChange(a_subject);
+    int totalCoins = getTotalNumberOfCoins(a_subject);
+    int changeDifference = Math.abs(m_targetAmount - changeAmount);
+    // Step 1: Determine distance of amount represented by solution from
+    // the target amount. Since we know  the maximum amount of change is
+    // 99 cents, we'll subtract the difference in change between the
+    // solution amount and the target amount from 99. That will give
+    // the desired effect of returning higher values for amounts
+    // closer to the target amount and lower values for amounts
+    // further away from the target amount.
+    // -----------------------------------------------------------------
+    int fitness = (99 - changeDifference);
+    // Step 2: If the solution amount equals the target amount, then
+    // we add additional fitness points for solutions representing fewer
+    // total coins.
+    // -----------------------------------------------------------------
+    if (changeAmount == m_targetAmount) {
+      fitness += 100 - (10 * totalCoins);
+    }
+    // Make sure fitness value is always positive.
+    // -------------------------------------------
+    return Math.max(1, fitness);
+  }
+
+  /**
+   * Calculates the total amount of change (in cents) represented by
+   * the given potential solution and returns that amount.
+   * Here we use "fantasy" coins just to have more genes and bloat the time
+   * consumed for test performance test
+   *
+   * @param a_potentialSolution The pontential solution to evaluate.
+   * @return The total amount of change (in cents) represented by the
+   *         given solution.
+   */
+  public static int amountOfChange(Chromosome a_potentialSolution) {
+    int numQuarters = getNumberOfCoinsAtGene(a_potentialSolution, 0);
+    int numDimes = getNumberOfCoinsAtGene(a_potentialSolution, 1);
+    int numNickels = getNumberOfCoinsAtGene(a_potentialSolution, 2);
+    int numPennies = getNumberOfCoinsAtGene(a_potentialSolution, 3);
+    int A = getNumberOfCoinsAtGene(a_potentialSolution, 4);
+    int B = getNumberOfCoinsAtGene(a_potentialSolution, 5);
+    int C = getNumberOfCoinsAtGene(a_potentialSolution, 6);
+    int D = getNumberOfCoinsAtGene(a_potentialSolution, 7);
+    int E = getNumberOfCoinsAtGene(a_potentialSolution, 8);
+    int F = getNumberOfCoinsAtGene(a_potentialSolution, 9);
+    return (numQuarters * 25) + (numDimes * 10) + (numNickels * 5) +
+        numPennies + (A * 29) + (B * 31) + (C * 37) + (D * 41) + (E * 43)
+        + (F * 47);
+  }
+
+  /**
+   * Retrieves the number of coins represented by the given potential
+   * solution at the given gene position.
+   *
+   * @param a_potentialSolution The potential solution to evaluate.
+   * @param a_position The gene position to evaluate.
+   * @return the number of coins represented by the potential solution
+   *         at the given gene position.
+   */
+  public static int getNumberOfCoinsAtGene(Chromosome a_potentialSolution,
+                                           int a_position) {
+    Integer numCoins =
+        (Integer) a_potentialSolution.getGene(a_position).getAllele();
+    return numCoins.intValue();
+  }
+
+  /**
+   * Returns the total number of coins represented by all of the genes in
+   * the given potential solution.
+   *
+   * @param a_potentialsolution The potential solution to evaluate.
+   * @return The total number of coins represented by the given Chromosome.
+   */
+  public static int getTotalNumberOfCoins(Chromosome a_potentialsolution) {
+    int totalCoins = 0;
+    int numberOfGenes = a_potentialsolution.size();
+    for (int i = 0; i < numberOfGenes; i++) {
+      totalCoins += getNumberOfCoinsAtGene(a_potentialsolution, i);
+    }
+    return totalCoins;
+  }
+}
