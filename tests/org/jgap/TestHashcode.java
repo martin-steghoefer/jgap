eb0e7dfbf6e2a7e29cee08b1bc5510f341253cbc
Klaus Meffert <kontakt@doktor-meffert.de>
2005-01-04 21:13:32 +0000

    Comments
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@700 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/TestHashcode.java
+++ tests/org/jgap/TestHashcode.java
@@ -1,12 +1,17 @@
 /*
- * Created on Dec 13, 2004
+ * This file is part of JGAP.
  *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
+
 package org.jgap;
 
-import java.math.BigDecimal;
+import java.math.*;
 import java.util.*;
-import java.lang.IllegalArgumentException;
 
 /**
  * @author jserri
