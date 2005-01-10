25cfeab75fcfb859e3ff1d9bb7ce371368ec26cc
Klaus Meffert <kontakt@doktor-meffert.de>
2005-01-10 11:03:18 +0000

    Added tests (see request 1084425)
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@725 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/FittestPopulationMergerTest.java
+++ tests/org/jgap/impl/FittestPopulationMergerTest.java
@@ -10,20 +10,23 @@
 package org.jgap.impl;
 
 import org.jgap.*;
-
+import org.jgap.impl.*;
+import org.jgap.distr.*;
 import junit.framework.*;
 
 /**
  * Tests for FittestPopulationMerger class
  *
- * @since 2.0
  * @author Klaus Meffert
+ * @since 2.0
  */
 public class FittestPopulationMergerTest
     extends TestCase {
-
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.3 $";
+  private final static String CVS_REVISION = "$Revision: 1.4 $";
+
+  //delta for distinguishing whether a value is to be interpreted as zero
+  private static final double DELTA = 0.0000001;
 
   public FittestPopulationMergerTest() {
   }
@@ -37,12 +40,55 @@ public class FittestPopulationMergerTest
     return suite;
   }
 
+  /**
+   * Test if construction possible
+   *
+   * @author Klaus Meffert
+   * @since 2.1
+   */
   public void testConstruct_0() {
-    /**@todo implement*/
+    new FittestPopulationMerger();
   }
 
+  /**
+   * @author Klaus Meffert
+   * @since 2.1
+   */
   public void testMergePopulations_0() {
-    /**@todo implement*/
+    Configuration conf = new DefaultConfiguration();
+    Genotype.setConfiguration(conf);
+    Gene gene = new BooleanGene();
+    Chromosome chrom = new Chromosome(gene, 4);
+    chrom.setFitnessValue(5);
+    Chromosome[] chroms1 = new Chromosome[3];
+    chroms1[0] = chrom;
+    chrom = new Chromosome(gene, 1);
+    chrom.setFitnessValue(7);
+    chroms1[1] = chrom;
+    chrom = new Chromosome(gene, 1);
+    chrom.setFitnessValue(2);
+    chroms1[2] = chrom;
+    Chromosome[] chroms2 = new Chromosome[4];
+    chrom = new Chromosome(gene, 1);
+    chrom.setFitnessValue(4);
+    chroms2[0] = chrom;
+    chrom = new Chromosome(gene, 1);
+    chrom.setFitnessValue(7);
+    chroms2[1] = chrom;
+    chrom = new Chromosome(gene, 1);
+    chrom.setFitnessValue(1);
+    chroms2[2] = chrom;
+    chrom = new Chromosome(gene, 1);
+    chrom.setFitnessValue(10);
+    chroms2[3] = chrom;
+    Population pop1 = new Population(chroms1);
+    Population pop2 = new Population(chroms2);
+    IPopulationMerger merger = new FittestPopulationMerger();
+    Population result = merger.mergePopulations(pop1, pop2, 4);
+    assertEquals(4, result.size());
+    assertEquals(10.0d, result.getChromosome(0).getFitnessValue(), DELTA);
+    assertEquals(7.0d, result.getChromosome(1).getFitnessValue(), DELTA);
+    assertEquals(7.0d, result.getChromosome(2).getFitnessValue(), DELTA);
+    assertEquals(5.0d, result.getChromosome(3).getFitnessValue(), DELTA);
   }
-
 }
