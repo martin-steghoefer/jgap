f1011bf9a90ad6fea3a79979a0b6f51abc4c5e63
Klaus Meffert <kontakt@doktor-meffert.de>
2005-04-02 18:06:07 +0000

    Added CVS_REVISION, removed blank lines
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1047 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/gui/GUIManager.java
+++ src/org/jgap/gui/GUIManager.java
@@ -7,11 +7,10 @@
  * or have a look at the top of class org.jgap.Chromosome which representatively
  * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
-
 package org.jgap.gui;
 
-import java.util.*;
 import javax.swing.*;
+
 import org.jgap.*;
 
 /**
@@ -20,65 +19,71 @@ import org.jgap.*;
  * creates a ConfigFrame for the root object, that is a Configuration itself.
  * In the future it will do the same for objects on the Root ConfigFrame that
  * are in turn Configurable.
+ *
  * @author Siddhartha Azad.
  */
 public class GUIManager {
-	
-	/**
-	 * Return a singleton GUIManager instance.
-	 * */
-	public static GUIManager instance() {
-		if(gm == null)
-			gm = new GUIManager();
-		return gm;
-	}
-	
-	/**
-	 * Constructor for the frame
-	 */
-	private GUIManager() {
-		frame = null;
-	}
-	
-	/**
-	 * Create and show a new frame for a Configurable.
-	 * */
-	private void showFrame(Configurable _con) {
-		try {
-			// create the frame
-			frame= new ConfigFrame("JGAP Configurator:" +
-				_con.getConfigurationHandler().getName());
-			con = _con;
-			frame.createAndShowGUI(_con.getConfigurationHandler());
-		}
-		catch(Exception ex) {
-			ex.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Main method for the GUI
-	 */
-	public static void main(String args[]) {
-	    try {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				Configuration con = new Configuration();
-				GUIManager.instance().showFrame(con);
-			}
-		});
-	    }
-	    catch(Exception ex) {
-		ex.printStackTrace();
-	    }
-	}
-	
-	// The root frame
-	private ConfigFrame frame;
-	// The entity to configure
-	protected Configurable con;
-	/**
-	 * Singleton Instance of GUIManager;
-	 */
-	private static GUIManager gm;
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
+
+  /**
+   * Return a singleton GUIManager instance.
+   * */
+  public static GUIManager instance() {
+    if (gm == null)
+      gm = new GUIManager();
+    return gm;
+  }
+
+  /**
+   * Constructor for the frame
+   */
+  private GUIManager() {
+    frame = null;
+  }
+
+  /**
+   * Create and show a new frame for a Configurable.
+   * */
+  private void showFrame(Configurable _con) {
+    try {
+      // create the frame
+      frame = new ConfigFrame("JGAP Configurator:" +
+                              _con.getConfigurationHandler().getName());
+      con = _con;
+      frame.createAndShowGUI(_con.getConfigurationHandler());
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  /**
+   * Main method for the GUI
+   */
+  public static void main(String args[]) {
+    try {
+      SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          Configuration con = new Configuration();
+          GUIManager.instance().showFrame(con);
+        }
+      });
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  // The root frame
+  private ConfigFrame frame;
+
+  // The entity to configure
+  protected Configurable con;
+
+  /**
+   * Singleton Instance of GUIManager;
+   */
+  private static GUIManager gm;
 }
