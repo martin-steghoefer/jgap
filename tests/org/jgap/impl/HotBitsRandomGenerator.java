9bf2f0f60a9947199fb0c2fc79d98b6213364fd4
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-01 13:50:31 +0000

    Added HotBits random generator adapter class, see request 1193361
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1067 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/impl/HotBitsRandomGenerator.java
@@ -0,0 +1,67 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+     * or have a look at the top of class org.jgap.Chromosome which representatively
+     * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.impl;
+
+import org.jgap.*;
+import org.jgap.util.*;
+
+/**
+ * Random generator using the HotBits random generators (they are pluggable
+ * here) and adapts the interface to org.jgap.RandomGenerator
+ *
+ * @author Klaus Meffert
+ * @since 2.3
+ */
+public class HotBitsRandomGenerator
+    implements RandomGenerator {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private randomX m_randomGenerator;
+
+  public HotBitsRandomGenerator(randomX a_randomGenerator) {
+    m_randomGenerator = a_randomGenerator;
+  }
+
+  public randomX getRandomGenerator() {
+    return m_randomGenerator;
+  }
+
+  public byte nextByte() {
+    return m_randomGenerator.nextByte();
+  }
+
+  public int nextInt() {
+    return m_randomGenerator.nextInt();
+  }
+
+  public int nextInt(int ceiling) {
+    return m_randomGenerator.nextInt() % ceiling;
+  }
+
+  public long nextLong() {
+    return m_randomGenerator.nextLong();
+
+  }
+
+  public double nextDouble() {
+    return m_randomGenerator.nextDouble();
+
+  }
+
+  public float nextFloat() {
+    return m_randomGenerator.nextFloat();
+
+  }
+
+  public boolean nextBoolean() {
+    return m_randomGenerator.nextBit();
+  }
+}
