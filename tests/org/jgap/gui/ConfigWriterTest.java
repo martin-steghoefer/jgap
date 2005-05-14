293493955213cd3ed9543b6640af2dc7a0bec675
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-14 15:55:37 +0000

    Did formatting
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1103 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/gui/ConfigWriterTest.java
+++ tests/org/jgap/gui/ConfigWriterTest.java
@@ -8,25 +8,26 @@
  * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
 package org.jgap.gui;
+
+import java.io.*;
+import java.util.*;
+
 import org.jgap.*;
-import org.jgap.impl.*;
 import org.jgap.event.*;
+import org.jgap.impl.*;
 
 import junit.framework.*;
-import junitx.util.*;
 
-import java.util.*;
-import java.io.*;
 /**
  * Tests for ConfigWriter class
+ *
  * @author Siddhartha Azad
+ * @since 2.3
  */
 public class ConfigWriterTest
     extends TestCase {
-
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
-
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   public static Test suite() {
     TestSuite suite = new TestSuite(ConfigWriterTest.class);
@@ -39,106 +40,108 @@ public class ConfigWriterTest
    * written correctly.
    * @author Siddhartha Azad.
    * */
-  public void testConfigData_0() throws Exception {
-      IConfigInfo ici = new MockIConfigInfo();
-  	  ConfigWriter.instance().write(ici);
-  	  // read the file and check the values
-  	  int totalProps = 0;
-  	  
-  	  Properties props = new Properties();
-  	  try {
-  	  	  props.load(new FileInputStream("jgapTmp.con"));
-  	  	  ConfigData cd = ici.getConfigData();
-  	  	  String nsPrefix = cd.getNS() + ".";
-  	  	  String name;
-  	  	  ArrayList values;
-  	  	  for (int i = 0; i < cd.getNumLists(); i++) {
-  	  	  	  name = cd.getListNameAt(i);
-	  		  values = cd.getListValuesAt(i);
-	  		  int idx = 0;
-	  		  for (Iterator iter = values.iterator(); iter.hasNext(); idx++) {
-	  		  	  // append an index for same key elements
-	  		  	  String tmpName = nsPrefix + name + "[" + idx + "]";
-	  		  	  try {
-	  		  	  	  assertEquals(props.getProperty(tmpName), (String)iter.next());	  
-	  		  	  }	
-	  		  	  catch(Exception ex) {
-	  		  	  	  ex.printStackTrace();
-	  		  	  	  fail();
-	  		  	  }
-	  		  	  totalProps++;
-	  		  }
-  		}
-  		// test the TextField properties
-  		String value = "", tmpName = "";
-  		for (int i = 0; i < cd.getNumTexts(); i++) {
-  	      	name = cd.getTextNameAt(i);
-  	      	value = cd.getTextValueAt(i);
-  	      	tmpName = nsPrefix + name;
-  	      	try {
-		  	    assertEquals(props.getProperty(tmpName), value);
-	        }
-	        catch(Exception ex) {
-	            ex.printStackTrace();
-	            fail();
-	        }
-	        totalProps++;
-  	    }
-  		assertEquals(totalProps, props.size());
-  	}
-  	catch(IOException ioEx) {
-  		ioEx.printStackTrace();
-  		fail();
-  	}
+  public void testConfigData_0()
+      throws Exception {
+    IConfigInfo ici = new MockIConfigInfo();
+    ConfigWriter.instance().write(ici);
+    // read the file and check the values
+    int totalProps = 0;
+    Properties props = new Properties();
+    try {
+      props.load(new FileInputStream("jgapTmp.con"));
+      ConfigData cd = ici.getConfigData();
+      String nsPrefix = cd.getNS() + ".";
+      String name;
+      ArrayList values;
+      for (int i = 0; i < cd.getNumLists(); i++) {
+        name = cd.getListNameAt(i);
+        values = cd.getListValuesAt(i);
+        int idx = 0;
+        for (Iterator iter = values.iterator(); iter.hasNext(); idx++) {
+          // append an index for same key elements
+          String tmpName = nsPrefix + name + "[" + idx + "]";
+          try {
+            assertEquals(props.getProperty(tmpName), (String) iter.next());
+          }
+          catch (Exception ex) {
+            ex.printStackTrace();
+            fail();
+          }
+          totalProps++;
+        }
+      }
+      // test the TextField properties
+      String value = "", tmpName = "";
+      for (int i = 0; i < cd.getNumTexts(); i++) {
+        name = cd.getTextNameAt(i);
+        value = cd.getTextValueAt(i);
+        tmpName = nsPrefix + name;
+        try {
+          assertEquals(props.getProperty(tmpName), value);
+        }
+        catch (Exception ex) {
+          ex.printStackTrace();
+          fail();
+        }
+        totalProps++;
+      }
+      assertEquals(totalProps, props.size());
+    }
+    catch (IOException ioEx) {
+      ioEx.printStackTrace();
+      fail();
+    }
   }
+
   /**
    * Test the reading of a config file and loading of the m_populatioSize
    * variable of the Configuration Configurable.
    * @author Siddhartha Azad.
    * */
-  public void testConfigReader_0() throws Exception {
-  	Configuration config;
-	try {
-	  config = new CustomConfiguration("jgapTest.con");
-	}
-	catch(Exception ex) {
-	  ex.printStackTrace();
-	  return;
+  public void testConfigReader_0()
+      throws Exception {
+    Configuration config;
+    try {
+      config = new CustomConfiguration("jgapTest.con");
     }
-	// Some setup so that the Configuration doesn't throw an Exception. We do
-	// not set the population size of the Configuration since this is supposed
-	// to be read directly from the config file.
-	// set up a sample chromosome
-	Gene[] sampleGenes = new Gene[3];
-	sampleGenes[0] = new IntegerGene(60, 100);
-	sampleGenes[1] = new IntegerGene(1, 50);
-	sampleGenes[2] = new IntegerGene(100, 150);
-	Chromosome sampleChromosome = new Chromosome(sampleGenes);
-	Genotype population;
-	FitnessFunction fitFunc = new MockFitnessFunction();
-	try {
-	  config.setFitnessFunction(fitFunc);
-	  // The higher the value, the better
-	  config.setFitnessEvaluator(new DefaultFitnessEvaluator());
-	  config.setSampleChromosome(sampleChromosome);
-	  BestChromosomesSelector bestChromsSelector = new 
-	  BestChromosomesSelector(1.0d);
-	  bestChromsSelector.setDoubletteChromosomesAllowed(false);
-	  config.addNaturalSelector(bestChromsSelector, true);
-	  config.setRandomGenerator(new StockRandomGenerator());
-	  config.setEventManager(new EventManager());
+    catch (Exception ex) {
+      ex.printStackTrace();
+      return;
+    }
+    // Some setup so that the Configuration doesn't throw an Exception. We do
+    // not set the population size of the Configuration since this is supposed
+    // to be read directly from the config file.
+    // set up a sample chromosome
+    Gene[] sampleGenes = new Gene[3];
+    sampleGenes[0] = new IntegerGene(60, 100);
+    sampleGenes[1] = new IntegerGene(1, 50);
+    sampleGenes[2] = new IntegerGene(100, 150);
+    Chromosome sampleChromosome = new Chromosome(sampleGenes);
+    Genotype population;
+    FitnessFunction fitFunc = new MockFitnessFunction();
+    try {
+      config.setFitnessFunction(fitFunc);
+      // The higher the value, the better
+      config.setFitnessEvaluator(new DefaultFitnessEvaluator());
+      config.setSampleChromosome(sampleChromosome);
+      BestChromosomesSelector bestChromsSelector = new
+          BestChromosomesSelector(1.0d);
+      bestChromsSelector.setDoubletteChromosomesAllowed(false);
+      config.addNaturalSelector(bestChromsSelector, true);
+      config.setRandomGenerator(new StockRandomGenerator());
+      config.setEventManager(new EventManager());
       config.addGeneticOperator(new CrossoverOperator());
-	  config.addGeneticOperator(new MutationOperator(15));
-	  population = Genotype.randomInitialGenotype(config);
-	}
-	catch(InvalidConfigurationException icEx) {
-	  icEx.printStackTrace();
-	  fail();
+      config.addGeneticOperator(new MutationOperator(15));
+      population = Genotype.randomInitialGenotype(config);
     }
-	catch(Exception ex) {
-	  ex.printStackTrace();
-	  fail();
-	}
-	assertEquals(config.getPopulationSize(), 35);
-  }	
-}	
+    catch (InvalidConfigurationException icEx) {
+      icEx.printStackTrace();
+      fail();
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+      fail();
+    }
+    assertEquals(config.getPopulationSize(), 35);
+  }
+}
