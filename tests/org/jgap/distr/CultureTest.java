874523d6f79eea84042a694cc724e6c7db187324
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-21 19:02:29 +0000

    Added skeletons/comments
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1127 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/distr/CultureTest.java
+++ tests/org/jgap/distr/CultureTest.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package org.jgap.distr;
 
 import org.jgap.*;
@@ -13,7 +22,7 @@ import junit.framework.*;
 public class CultureTest extends TestCase {
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.2 $";
+  private static final String CVS_REVISION = "$Revision: 1.3 $";
 
   public void setUp() {
     Genotype.setConfiguration(null);
@@ -25,7 +34,11 @@ public class CultureTest extends TestCase {
   }
 
   public void testConstruct_0() {
-    /**@todo*/
+    /**@todo implement*/
+  }
+
+  public void testToString_0() {
+    /**@todo implement*/
   }
 
   /**@todo implement tests*/
