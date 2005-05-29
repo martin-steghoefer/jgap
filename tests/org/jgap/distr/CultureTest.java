8eeb6118ff87c14c2a01fcdc4329e0d3544fde73
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-29 17:55:43 +0000

    Added tests
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1141 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/distr/CultureTest.java
+++ tests/org/jgap/distr/CultureTest.java
@@ -13,7 +13,7 @@ import org.jgap.*;
 import junit.framework.*;
 
 /**
- * Test class for Culture class
+ * Tests for Culture class
  *
  * @author Klaus Meffert
  * @since 2.3
@@ -21,7 +21,9 @@ import junit.framework.*;
 public class CultureTest
     extends TestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.4 $";
+  private static final String CVS_REVISION = "$Revision: 1.5 $";
+
+  private final static double DELTA = 0.00000001d;
 
   public void setUp() {
     Genotype.setConfiguration(null);
@@ -34,7 +36,7 @@ public class CultureTest
 
   public void testConstruct_0() {
     try {
-      Culture c = new Culture(0);
+      new Culture(0);
       fail();
     }
     catch (IllegalArgumentException iex) {
@@ -44,7 +46,7 @@ public class CultureTest
 
   public void testConstruct_1() {
     try {
-      Culture c = new Culture( -3);
+      new Culture( -3);
       fail();
     }
     catch (IllegalArgumentException iex) {
@@ -53,12 +55,60 @@ public class CultureTest
   }
 
   public void testConstruct_2() {
-      Culture c = new Culture( 7);
-      assertEquals(7,c.size());
+    Culture c = new Culture(7);
+    assertEquals(7, c.size());
+  }
+
+  public void testGet_0() {
+    try {
+      Culture c = new Culture(35);
+      c.set( -1, 2, 5, "");
+      fail();
+    }
+    catch (IllegalArgumentException iex) {
+      //this is OK
+    }
+  }
+
+  public void testGet_1() {
+    try {
+      Culture c = new Culture(35);
+      c.set(35, 2, 5, "");
+      fail();
+    }
+    catch (IllegalArgumentException iex) {
+      //this is OK
+    }
+  }
+
+  public void testGet_2() {
+    Culture c = new Culture(9);
+    c.set(3, 5.7d, 0, "");
+    CultureMemoryCell cell = c.get(3);
+    assertEquals(5.7d, cell.getCurrentValueAsDouble(), DELTA);
+    assertEquals(0, cell.getHistorySize());
+    assertEquals("", cell.getName());
+  }
+
+  public void testGet_3() {
+    Culture c = new Culture(9);
+    c.set(3, -5.7d, -1, null);
+    CultureMemoryCell cell = c.get(3);
+    assertEquals( -5.7d, cell.getCurrentValueAsDouble(), DELTA);
+    assertEquals(0, cell.getHistorySize());
+    assertEquals(null, cell.getName());
+  }
+
+  public void testGet_4() {
+    Culture c = new Culture(11);
+    c.set(0, 0.0d, 17, "aName");
+    CultureMemoryCell cell = c.get(0);
+    assertEquals(0.0d, cell.getCurrentValueAsDouble(), DELTA);
+    assertEquals(17, cell.getHistorySize());
+    assertEquals("aName", cell.getName());
   }
 
   public void testToString_0() {
     /**@todo implement*/
   }
-  /**@todo implement tests*/
 }
