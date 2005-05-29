6954c54aaab5b70fca34007d2649e3a1587e7e05
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-29 17:41:10 +0000

    Added tests
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1139 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/distr/CultureTest.java
+++ tests/org/jgap/distr/CultureTest.java
@@ -10,7 +10,6 @@
 package org.jgap.distr;
 
 import org.jgap.*;
-
 import junit.framework.*;
 
 /**
@@ -19,10 +18,10 @@ import junit.framework.*;
  * @author Klaus Meffert
  * @since 2.3
  */
-public class CultureTest extends TestCase {
-
+public class CultureTest
+    extends TestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.3 $";
+  private static final String CVS_REVISION = "$Revision: 1.4 $";
 
   public void setUp() {
     Genotype.setConfiguration(null);
@@ -34,13 +33,32 @@ public class CultureTest extends TestCase {
   }
 
   public void testConstruct_0() {
-    /**@todo implement*/
+    try {
+      Culture c = new Culture(0);
+      fail();
+    }
+    catch (IllegalArgumentException iex) {
+      ; //this is OK
+    }
+  }
+
+  public void testConstruct_1() {
+    try {
+      Culture c = new Culture( -3);
+      fail();
+    }
+    catch (IllegalArgumentException iex) {
+      ; //this is OK
+    }
+  }
+
+  public void testConstruct_2() {
+      Culture c = new Culture( 7);
+      assertEquals(7,c.size());
   }
 
   public void testToString_0() {
     /**@todo implement*/
   }
-
   /**@todo implement tests*/
-
 }
