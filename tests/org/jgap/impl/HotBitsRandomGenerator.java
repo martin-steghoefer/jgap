8dda8d927216f2f7ac757f60a54427621f466389
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-30 18:36:16 +0000

    formatting
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1182 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/HotBitsRandomGenerator.java
+++ tests/org/jgap/impl/HotBitsRandomGenerator.java
@@ -4,8 +4,8 @@
  * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
  * For licencing information please see the file license.txt included with JGAP
-     * or have a look at the top of class org.jgap.Chromosome which representatively
-     * includes the JGAP license policy applicable for any file delivered with JGAP.
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
 package org.jgap.impl;
 
@@ -22,7 +22,7 @@ import org.jgap.util.*;
 public class HotBitsRandomGenerator
     implements RandomGenerator {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   private randomX m_randomGenerator;
 
@@ -48,17 +48,14 @@ public class HotBitsRandomGenerator
 
   public long nextLong() {
     return m_randomGenerator.nextLong();
-
   }
 
   public double nextDouble() {
     return m_randomGenerator.nextDouble();
-
   }
 
   public float nextFloat() {
     return m_randomGenerator.nextFloat();
-
   }
 
   public boolean nextBoolean() {
