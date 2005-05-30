bd4297fbbe8a7d7941c2bd2796be365d4baaf592
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-30 18:20:33 +0000

    formatted, javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1180 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/gui/MockIConfigInfo.java
+++ tests/org/jgap/gui/MockIConfigInfo.java
@@ -11,51 +11,55 @@ package org.jgap.gui;
 
 import org.jgap.*;
 import java.util.*;
-/**
- * A Mock implementaion of IConfigInfo to test the proper generation of a
- * Config File.
- * @author Siddhartha Azad.
- * */
-public class MockIConfigInfo implements IConfigInfo {
-	
-	final static String CVS_REVISION = "$Revision: 1.2 $";
-	
-	public MockIConfigInfo() {
-		conData = new ConfigData();
-		// populate the ConfigData instance
-		conData.setNS("Configurable");
-		// add the TextField properties
-		conData.addTextData("text1", "text1_value");
-		conData.addTextData("text2", "text2_value");
-		conData.addTextData("text3", "text3_value");
-		
-		// add the List properties
-		ArrayList listData;
-		String listName = "";
-		for(int i = 0; i < 3; i++) {
-			listName = "List" + (i+1);
-			listData = new ArrayList();
-			listData.add(listName+"_value_1");
-			listData.add(listName+"_value_2");
-			listData.add(listName+"_value_3");
-			conData.addListData(listName, listData);
-		}
-	}
-	
-	public ConfigData getConfigData() {
-		return conData;
-	}
 
-	/**
-	 * Get the config file to write to.
-	 * @return The config file name to write to.
-	 * */
-	
-	 public String getFileName() {
-	 	// A temporary config file
-	 	return "jgapTmp.con";
-	 }
-	 
-	// The ConfigData instance that will be populated and returned
-    private ConfigData conData;
+/**
+ * A Mock implementation of IConfigInfo to test the proper generation of a
+ * Config File.
+ * @author Siddhartha Azad
+ * @since 2.3
+ * */
+public class MockIConfigInfo
+    implements IConfigInfo {
+  final static String CVS_REVISION = "$Revision: 1.3 $";
+
+  /**
+   * @author Siddhartha Azad
+   * @since 2.3
+   */
+  public MockIConfigInfo() {
+    conData = new ConfigData();
+    // populate the ConfigData instance
+    conData.setNS("Configurable");
+    // add the TextField properties
+    conData.addTextData("text1", "text1_value");
+    conData.addTextData("text2", "text2_value");
+    conData.addTextData("text3", "text3_value");
+    // add the List properties
+    ArrayList listData;
+    String listName = "";
+    for (int i = 0; i < 3; i++) {
+      listName = "List" + (i + 1);
+      listData = new ArrayList();
+      listData.add(listName + "_value_1");
+      listData.add(listName + "_value_2");
+      listData.add(listName + "_value_3");
+      conData.addListData(listName, listData);
+    }
+  }
+
+  public ConfigData getConfigData() {
+    return conData;
+  }
+
+  /**
+   * Get the config file to write to.
+   * @return The config file name to write to.
+   * */
+  public String getFileName() {
+    // A temporary config file
+    return "jgapTmp.con";
+  }
+
+  // The ConfigData instance that will be populated and returned
+  private ConfigData conData;
 }
