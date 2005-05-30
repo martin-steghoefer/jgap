e9f53937828a0d7db8ba371cbe55bab2bf11874b
Klaus Meffert <kontakt@doktor-meffert.de>
2005-05-30 15:41:32 +0000

    Added toString test, javadocs
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1171 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/ConfigurationTest.java
+++ tests/org/jgap/ConfigurationTest.java
@@ -4,8 +4,8 @@
  * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
  * For licencing information please see the file license.txt included with JGAP
-     * or have a look at the top of class org.jgap.Chromosome which representatively
-     * includes the JGAP license policy applicable for any file delivered with JGAP.
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
 package org.jgap;
 
@@ -23,7 +23,7 @@ import junitx.util.*;
 public class ConfigurationTest
     extends TestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.20 $";
+  private final static String CVS_REVISION = "$Revision: 1.21 $";
 
   public void setUp() {
     Genotype.setConfiguration(null);
@@ -34,6 +34,9 @@ public class ConfigurationTest
     return suite;
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testAddGeneticOperator_0() {
     Configuration conf = new Configuration();
     try {
@@ -45,6 +48,9 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testAddGeneticOperator_1() {
     Configuration conf = new Configuration();
     try {
@@ -57,9 +63,12 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testAddGeneticOperator_2()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     conf.addGeneticOperator(new MutationOperator());
     assertEquals(1, conf.getGeneticOperators().size());
@@ -67,9 +76,12 @@ public class ConfigurationTest
     assertEquals(2, conf.getGeneticOperators().size());
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_0()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -82,9 +94,12 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_1()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -99,9 +114,12 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_2()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -117,9 +135,12 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_3()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -136,9 +157,12 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_4()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -156,9 +180,12 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_5()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -177,9 +204,12 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_6()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -194,9 +224,12 @@ public class ConfigurationTest
     conf.verifyStateIsValid();
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testVerifyStateIsValid_7()
-      throws
-      InvalidConfigurationException {
+      throws InvalidConfigurationException {
     Configuration conf = new Configuration();
     assertEquals(false, conf.isLocked());
     conf.setFitnessFunction(new StaticFitnessFunction(2));
@@ -216,6 +249,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testIsLocked_0()
       throws InvalidConfigurationException {
     Configuration conf = new Configuration();
@@ -234,6 +271,10 @@ public class ConfigurationTest
     assertEquals(true, conf.isLocked());
   }
 
+  /**
+   * @throws InvalidConfigurationException
+   * @author Klaus Meffert
+   */
   public void testGetter_0()
       throws InvalidConfigurationException {
     Configuration conf = new Configuration();
@@ -300,6 +341,10 @@ public class ConfigurationTest
     assertEquals(selector, conf.getNaturalSelector());
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testGetNaturalSelector_2()
       throws Exception {
     Configuration conf = new Configuration();
@@ -312,6 +357,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testGetNaturalSelector_3()
       throws Exception {
     Configuration conf = new Configuration();
@@ -338,6 +387,10 @@ public class ConfigurationTest
     assertEquals(selector, conf.getNaturalSelector(false, 0));
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testAddNaturalSelector_0()
       throws Exception {
     Configuration conf = new Configuration();
@@ -346,6 +399,10 @@ public class ConfigurationTest
     assertEquals(selector, conf.getNaturalSelectors(true).get(0));
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testAddNaturalSelector_1()
       throws Exception {
     Configuration conf = new Configuration();
@@ -354,6 +411,10 @@ public class ConfigurationTest
     assertEquals(selector, conf.getNaturalSelectors(false).get(0));
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testAddNaturalSelector_2()
       throws Exception {
     Configuration conf = new Configuration();
@@ -373,6 +434,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testAddNaturalSelector_3()
       throws Exception {
     Configuration conf = new Configuration();
@@ -385,6 +450,10 @@ public class ConfigurationTest
     assertEquals(1, i.intValue());
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testAddNaturalSelector_4()
       throws Exception {
     Configuration conf = new Configuration();
@@ -397,6 +466,9 @@ public class ConfigurationTest
     assertEquals(1, i.intValue());
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testSetFitnessFunction_0() {
     Configuration conf = new Configuration();
     try {
@@ -408,6 +480,9 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testSetFitnessEvaluator_0() {
     Configuration conf = new Configuration();
     try {
@@ -419,6 +494,9 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testSetPreserveFittestIndividual_0() {
     Configuration conf = new Configuration();
     assertFalse(conf.isPreserveFittestIndividual());
@@ -428,6 +506,9 @@ public class ConfigurationTest
     assertFalse(conf.isPreserveFittestIndividual());
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testGenerationNr_0() {
     Configuration conf = new Configuration();
     assertEquals(0, conf.getGenerationNr());
@@ -438,6 +519,10 @@ public class ConfigurationTest
     assertEquals(3, conf.getGenerationNr());
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testSetBulkFitnessFunction_0() {
     Configuration conf = new Configuration();
     try {
@@ -449,6 +534,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testSetBulkFitnessFunction_1()
       throws Exception {
     Configuration conf = new Configuration();
@@ -462,6 +551,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testSetBulkFitnessFunction_2()
       throws Exception {
     Configuration conf = new Configuration();
@@ -475,12 +568,20 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testSetBulkFitnessFunction_3()
       throws Exception {
     Configuration conf = new Configuration();
     conf.setBulkFitnessFunction(new TestBulkFitnessFunction());
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testGetPopulationSize_0()
       throws Exception {
     Configuration conf = new Configuration();
@@ -490,6 +591,10 @@ public class ConfigurationTest
     assertEquals(SIZE, conf.getPopulationSize());
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testSetPopulationSize_1()
       throws Exception {
     Configuration conf = new Configuration();
@@ -502,6 +607,9 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testSetRandomGenerator_0() {
     Configuration conf = new Configuration();
     try {
@@ -513,6 +621,9 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testSetEventManager_0() {
     Configuration conf = new Configuration();
     try {
@@ -524,6 +635,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testLock_0()
       throws Exception {
     Configuration conf = new Configuration();
@@ -536,6 +651,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testLock_1()
       throws Exception {
     Configuration conf = new Configuration();
@@ -560,6 +679,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testSetSampleChromosome_0()
       throws Exception {
     Configuration conf = new Configuration();
@@ -574,6 +697,10 @@ public class ConfigurationTest
     }
   }
 
+  /**
+   * @throws Exception
+   * @author Klaus Meffert
+   */
   public void testGetChromosomeSize_0()
       throws Exception {
     Configuration conf = new Configuration();
@@ -593,29 +720,171 @@ public class ConfigurationTest
    */
   public void testToString_0()
       throws Exception {
-    /**@todo implement*/
+    Configuration conf = new ConfigurationForTest();
+    String s = trimString(conf.toString());
+    String eventmgr = conf.getEventManager() != null ?
+        conf.getEventManager().getClass().getName() : conf.S_NONE;
+    String genops = "";
+    if (conf.getGeneticOperators().size() < 1) {
+      genops = conf.S_NONE;
+    }
+    else {
+      for (int i = 0; i < conf.getGeneticOperators().size(); i++) {
+        if (i > 0) {
+          genops += ";";
+        }
+        genops += conf.getGeneticOperators().get(i).getClass().getName(); ;
+      }
+    }
+    // natural selectors (pre)
+    String natselsPre = "";
+    int natsize = conf.getNaturalSelectors(true).size();
+    if (natsize < 1) {
+      natselsPre = conf.S_NONE;
+    }
+    else {
+      for (int i = 0; i < natsize; i++) {
+        if (i > 0) {
+          natselsPre += "; ";
+        }
+        natselsPre += " " +
+            conf.getNaturalSelectors(true).get(i).getClass().getName();
+      }
+    }
+    // natural selectors (post)
+    String natselsPost = "";
+    natsize = conf.getNaturalSelectors(false).size();
+    if (natsize < 1) {
+      natselsPost = conf.S_NONE;
+    }
+    else {
+      for (int i = 0; i < natsize; i++) {
+        if (i > 0) {
+          natselsPost += "; ";
+        }
+        natselsPost += " " +
+            conf.getNaturalSelectors(false).get(i).getClass().getName();
+      }
+    }
+    assertEquals(trimString(conf.S_CONFIGURATION + ":"
+                            + conf.S_CONFIGURATION_NAME + ":"
+                            + conf.getName() + " "
+                            + conf.S_POPULATION_SIZE + ":"
+                            + conf.getPopulationSize() + " "
+                            + conf.S_MINPOPSIZE + ":"
+                            + conf.getMinimumPopSizePercent() + " "
+                            + conf.S_CHROMOSOME_SIZE + ":"
+                            + conf.getChromosomeSize() + " "
+                            + conf.S_SAMPLE_CHROM + ":"
+                            + conf.S_SIZE + ":"
+                            + conf.getSampleChromosome().size() + " "
+                            + conf.S_TOSTRING + ":"
+                            + conf.getSampleChromosome().toString() + " "
+                            + conf.S_RANDOM_GENERATOR + ":"
+                            + conf.getRandomGenerator().getClass().getName() +
+                            " "
+                            + conf.S_EVENT_MANAGER + ":"
+                            + eventmgr + " "
+                            + conf.S_CONFIGURATION_HANDLER + ":"
+                            + conf.getConfigurationHandler().getName() + " "
+                            + conf.S_FITNESS_FUNCTION + ":"
+                            + conf.getFitnessFunction().getClass().getName() +
+                            " "
+                            + conf.S_FITNESS_EVALUATOR + ":"
+                            + conf.getFitnessEvaluator().getClass().getName() +
+                            " "
+                            + conf.S_GENETIC_OPERATORS + ":"
+                            + genops + " "
+                            + conf.S_NATURAL_SELECTORS + "(" + conf.S_PRE + ")" +
+                            ":"
+                            + natselsPre + " "
+                            + conf.S_NATURAL_SELECTORS + "(" + conf.S_POST +
+                            ")" + ":"
+                            + natselsPost + " "
+                            ), s);
   }
 
+  /**
+   *
+   * @param s1 String
+   * @return String
+   *
+   * @author Klaus Meffert
+   */
+  private String trimString(String s1) {
+    String result = s1;
+    int index = 0;
+    while (index < result.length()) {
+      if (result.charAt(index) == '\n') {
+        result = result.substring(0, index) + result.substring(index + 1);
+      }
+      else {
+        if (result.charAt(index) == ':') {
+          if (index < result.length() - 1) {
+            if (result.charAt(index + 1) == ' ' ||
+                result.charAt(index + 1) == '\n') {
+              result = result.substring(0, index + 1) +
+                  result.substring(index + 2);
+            }
+            else {
+              index++;
+            }
+          }
+          else {
+            index++;
+          }
+        }
+        else if (result.charAt(index) == ' ') {
+          if (index == 0) {
+            result = result.substring(1);
+          }
+          else if (index == result.length() - 1) {
+            result = result.substring(0, result.length() - 1);
+          }
+          else
+          if (result.charAt(index + 1) == ' ' ||
+              result.charAt(index + 1) == '\n') {
+            result = result.substring(0, index) + result.substring(index + 1);
+          }
+          else {
+            index++;
+          }
+        }
+        else {
+          index++;
+        }
+      }
+    }
+    return result;
+  }
+
+  /**
+   * @author Klaus Meffert
+   */
   public void testGetName_0() {
     Configuration conf = new Configuration("tEstName");
-    assertEquals("tEstName",conf.getName());
+    assertEquals("tEstName", conf.getName());
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testGetName_1() {
     Configuration conf = new Configuration("tEstName");
     conf.setName("HallI");
-    assertEquals("HallI",conf.getName());
+    assertEquals("HallI", conf.getName());
     conf.setName("HallA");
-    assertEquals("HallA",conf.getName());
+    assertEquals("HallA", conf.getName());
   }
 
+  /**
+   * @author Klaus Meffert
+   */
   public void testGetName_2() {
     Configuration conf = new Configuration();
-    assertEquals(null,conf.getName());
+    assertEquals(null, conf.getName());
   }
-
 }
-
 class TestFitnessFunction
     extends FitnessFunction {
   /**
@@ -628,7 +897,6 @@ class TestFitnessFunction
     return 1.0000000d;
   }
 }
-
 class TestBulkFitnessFunction
     extends BulkFitnessFunction {
   public void evaluate(Population a_subjects) {
