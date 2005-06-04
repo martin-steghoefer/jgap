903a0c55d540604b16d3abcf9ba13e9880d1ef63
Klaus Meffert <kontakt@doktor-meffert.de>
2005-06-04 22:26:11 +0000

    formatting
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1256 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/impl/InversionOperator.java
+++ src/org/jgap/impl/InversionOperator.java
@@ -22,11 +22,10 @@ import org.jgap.*;
  */
 public class InversionOperator
     implements GeneticOperator {
-
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
-   /**
+  /**
    * Constructs a new instance of this operator
    *
    * @author Klaus Meffert
@@ -36,57 +35,51 @@ public class InversionOperator
   }
 
   /**
-   * @param a_population The population of chromosomes from the current
-   *                     evolution prior to exposure to any genetic operators.
-   *                     Chromosomes in this array should never be modified.
-   * @param a_candidateChromosomes The pool of chromosomes that are candidates
+   * @param a_population the population of chromosomes from the current
+   * evolution prior to exposure to any genetic operators. Chromosomes in this
+   * array should never be modified.
+   * @param a_candidateChromosomes the pool of chromosomes that are candidates
    * for the next evolved population. Only these chromosomes will go to the
    * natural phase, so it's important to add any modified copies of Chromosomes
    * to this list if it's desired for them to be considered for natural
-   * selection.
+   * selection
    *
    * @author Klaus Meffert
    * @since 2.3
    */
   public void operate(final Population a_population,
                       final List a_candidateChromosomes) {
-
-    // Work out the number of crossovers that should be performed
+    // Work out the number of crossovers that should be performed.
+    // -----------------------------------------------------------
     int size = Math.min(Genotype.getConfiguration().getPopulationSize(),
                         a_population.size());
-
     RandomGenerator generator = Genotype.getConfiguration().getRandomGenerator();
     // For the inversion, grab a random chromosome, pick a random
     // locus (gene location), and then swap that gene and all genes
     // to the "right" (those with greater loci) of that gene with the left
     // genes.
     // -------------------------------------------------------------------
-    int index1, index2;
-
-      index1 = generator.nextInt(size);
-      Chromosome chrom1 = a_population.getChromosome(index1);
-      Chromosome firstMate = (Chromosome)chrom1.clone();
-      Gene[] firstGenes = firstMate.getGenes();
-      int locus = generator.nextInt(firstGenes.length);
-      // Swap the genes.
-      // ---------------
-      Gene[] invertedGenes = new Gene[firstGenes.length];
-      Gene gene1;
-      Gene gene2;
-      Object firstAllele;
-      int index = 0;
-      for (int j = locus; j < firstGenes.length; j++) {
-        invertedGenes[index++] = firstGenes[j];
-      }
-      for (int j = 0; j < locus; j++) {
-        invertedGenes[index++] = firstGenes[j];
-      }
-      firstMate.setGenes(invertedGenes);
-
-      // Add the modified chromosome to the candidate pool so that it'll be
-      // considered for natural selection during the next phase of evolution.
-      // --------------------------------------------------------------------
-      a_candidateChromosomes.add(firstMate);
+    int index1;
+    index1 = generator.nextInt(size);
+    Chromosome chrom1 = a_population.getChromosome(index1);
+    Chromosome firstMate = (Chromosome) chrom1.clone();
+    Gene[] firstGenes = firstMate.getGenes();
+    int locus = generator.nextInt(firstGenes.length);
+    // Swap the genes.
+    // ---------------
+    Gene[] invertedGenes = new Gene[firstGenes.length];
+    int index = 0;
+    int len = firstGenes.length;
+    for (int j = locus; j < len; j++) {
+      invertedGenes[index++] = firstGenes[j];
+    }
+    for (int j = 0; j < locus; j++) {
+      invertedGenes[index++] = firstGenes[j];
+    }
+    firstMate.setGenes(invertedGenes);
+    // Add the modified chromosome to the candidate pool so that it'll be
+    // considered for natural selection during the next phase of evolution.
+    // --------------------------------------------------------------------
+    a_candidateChromosomes.add(firstMate);
   }
-
 }
