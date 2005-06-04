ec8e073098f694bec16b922872391bbaaf4cd066
Klaus Meffert <kontakt@doktor-meffert.de>
2005-06-04 22:27:16 +0000

    fixed tests
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1257 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/InversionOperatorTest.java
+++ tests/org/jgap/impl/InversionOperatorTest.java
@@ -10,11 +10,14 @@
 package org.jgap.impl;
 
 import java.util.*;
+
 import org.jgap.*;
+import org.jgap.JGAPTestCase.*;
+
 import junit.framework.*;
 
 /**
- * Test class for InversionOperator class
+ * Tests the InversionOperator class
  *
  * @author Klaus Meffert
  * @since 2.3
@@ -22,7 +25,7 @@ import junit.framework.*;
 public class InversionOperatorTest
     extends JGAPTestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.3 $";
+  private static final String CVS_REVISION = "$Revision: 1.4 $";
 
   public static Test suite() {
     TestSuite suite = new TestSuite(InversionOperatorTest.class);
@@ -44,12 +47,12 @@ public class InversionOperatorTest
    */
   public void testOperate_0()
       throws Exception {
-    /**@todo adapt all of the following as it is copied from CrossoverOperator*/
     DefaultConfiguration conf = new DefaultConfiguration();
     Genotype.setConfiguration(conf);
     RandomGeneratorForTest rand = new RandomGeneratorForTest();
+    // tupels: (index of chromosome; locus), see InversionOperator.operate
     rand.setNextIntSequence(new int[] {
-                            0, 1, 0, 1, 2});
+                            0, 1, 1, 1});
     conf.setRandomGenerator(rand);
     conf.setFitnessFunction(new TestFitnessFunction());
     Gene sampleGene = new IntegerGene(1, 10);
@@ -64,8 +67,10 @@ public class InversionOperatorTest
     Chromosome chrom1 = new Chromosome(genes1);
     Gene cgene2 = new IntegerGene(1, 10);
     cgene2.setAllele(new Integer(8));
+    Gene cgene3 = new IntegerGene(2, 120);
+    cgene3.setAllele(new Integer(99));
     Gene[] genes2 = new Gene[] {
-        cgene2};
+        cgene2, cgene3};
     Chromosome chrom2 = new Chromosome(genes2);
     Chromosome[] population = new Chromosome[] {
         chrom1, chrom2};
@@ -79,12 +84,16 @@ public class InversionOperatorTest
     Gene gene3 = new IntegerGene(1, 10);
     gene3.setAllele(new Integer(4));
     chroms.add(gene3);
+    final int size = chroms.size();
     op.operate(new Population(population), chroms);
-    assertEquals(5, chroms.size());
-    Chromosome target = (Chromosome) chroms.get(4);
+    assertEquals(size+1, chroms.size());
+    Chromosome target = (Chromosome) chroms.get(size);
     assertEquals(6, ( (Integer) target.getGene(0).getAllele()).intValue());
-    target = (Chromosome) chroms.get(3);
-    assertEquals(8, ( (Integer) target.getGene(0).getAllele()).intValue());
+    op.operate(new Population(population), chroms);
+    assertEquals(size+2, chroms.size());
+    target = (Chromosome) chroms.get(size+1);
+    assertEquals(99, ( (Integer) target.getGene(0).getAllele()).intValue());
+    assertEquals(8, ( (Integer) target.getGene(1).getAllele()).intValue());
   }
 
   /**
@@ -146,12 +155,12 @@ public class InversionOperatorTest
    */
   public void testOperate_2()
       throws Exception {
-    /**@todo adapt all of the following as it is copied from CrossoverOperator*/
     DefaultConfiguration conf = new DefaultConfiguration();
     Genotype.setConfiguration(conf);
     RandomGeneratorForTest rand = new RandomGeneratorForTest();
+    // tupels: (index of chromosome; locus), see InversionOperator.operate
     rand.setNextIntSequence(new int[] {
-                            0, 1, 0, 1, 2});
+                            0, 1, 1, 1, 2});
     conf.setRandomGenerator(rand);
     conf.setFitnessFunction(new TestFitnessFunction());
     Gene sampleGene = new IntegerGene(1, 10);
@@ -183,13 +192,17 @@ public class InversionOperatorTest
     Gene gene3 = new IntegerGene(1, 10);
     gene3.setAllele(new Integer(4));
     chroms.add(gene3);
+    int size = chroms.size();
     op.operate(new Population(population), chroms);
-    assertEquals(5, chroms.size());
-    Chromosome target = (Chromosome) chroms.get(4);
-    assertEquals(6, ( (Integer) target.getGene(0).getAllele()).intValue());
-    target = (Chromosome) chroms.get(3);
-    CompositeGene result = (CompositeGene) target.getGene(0);
-    assertEquals(8, ( (Integer) ( (Vector) result.getAllele()).get(0)).intValue());
+    assertEquals(size+1, chroms.size());
+    Chromosome target = (Chromosome) chroms.get(size);
+    CompositeGene cog = (CompositeGene )target.getGene(0);
+    assertEquals(6, ((Integer)( (Vector) cog.getAllele()).get(0)).intValue());
+    op.operate(new Population(population), chroms);
+    assertEquals(size+2, chroms.size());
+    target = (Chromosome) chroms.get(size+1);
+    IntegerGene result = (IntegerGene) target.getGene(0);
+    assertEquals(8, ( (Integer) result.getAllele()).intValue());
   }
 
 }
