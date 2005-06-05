20afd9c8e6d25b9b140ebc6bbce8be9cb5b1d574
Klaus Meffert <kontakt@doktor-meffert.de>
2005-06-05 15:27:20 +0000

    due to class rename
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1275 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/supergenes/SupergeneSampleApplicationTest.java
+++ tests/org/jgap/supergenes/SupergeneSampleApplicationTest.java
@@ -10,58 +10,44 @@
 package org.jgap.supergenes;
 
 import junit.framework.*;
+import org.jgap.*;
 
 /**
-* Test Supergene sample application, verifying the "make change"
-* supergene and non-supergene versions. Both must return a zero error.
-* Performance is not verified.
-* @return true if the Supergene tests succeded.
-*
-* @author Meskauskas Audrius
-* @since 2.0
-*/
-public class SupergeneSampleApplicationTest extends TestCase {
-
+ * Test Supergene sample application, verifying the "make change"
+ * supergene and non-supergene versions. Both must return a zero error.
+ * Performance is not verified.
+ * @return true if the Supergene tests succeded.
+ *
+ * @author Meskauskas Audrius
+ * @since 2.0
+ */
+public class SupergeneSampleApplicationTest
+    extends JGAPTestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.5 $";
+  private final static String CVS_REVISION = "$Revision: 1.6 $";
 
-  public void testSupergeneTotal()
-  {
+  public void testSupergeneTotal() {
     int E_s = Integer.MAX_VALUE;
     Test:
-    for (int i = 0; i < 2; i++) {
-      p_abstractSupergeneTest.EXISTING_SOLUTIONS_ONLY = true;
-      p_abstractSupergeneTest.REPORT_ENABLED = false;
-      p_Force.REPORT_ENABLED = false;
-
-      p_abstractSupergeneTest.MAX_ALLOWED_EVOLUTIONS = 512;
-      p_abstractSupergeneTest.POPULATION_SIZE = 256;
-
-      abstractSupergene.reset ();
-      E_s = new p_SupergeneTest().test ();
-
+        for (int i = 0; i < 2; i++) {
+      AbstractSupergeneTest.EXISTING_SOLUTIONS_ONLY = true;
+      AbstractSupergeneTest.REPORT_ENABLED = false;
+      Force.REPORT_ENABLED = false;
+      AbstractSupergeneTest.MAX_ALLOWED_EVOLUTIONS = 512;
+      AbstractSupergeneTest.POPULATION_SIZE = 256;
+      abstractSupergene.reset();
+      E_s = new SupergeneTest().test();
       if (E_s == 0) {
         break Test;
       }
-
-      assertTrue("Correctness of solution: supergene "+E_s, E_s <3);
-
+      assertTrue("Correctness of solution: supergene " + E_s, E_s < 3);
     }
-    assertTrue("Correctness of solution: supergene "+E_s, E_s == 0);
-  }
-
-  protected void setUp() throws Exception {
-      super.setUp();
-  }
-
-  protected void tearDown() throws Exception {
-      super.tearDown();
+    assertTrue("Correctness of solution: supergene " + E_s, E_s == 0);
   }
 
   public static Test suite() {
     TestSuite suite =
-     new TestSuite(SupergeneSampleApplicationTest.class);
+        new TestSuite(SupergeneSampleApplicationTest.class);
     return suite;
   }
-
 }
