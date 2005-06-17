9777adf99e531cd5383c36cffec883ad114fcb7d
Klaus Meffert <kontakt@doktor-meffert.de>
2005-06-17 09:10:47 +0000

    old classes renamed and advanced
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1327 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ examples/src/examples/functionFinder/FormulaFitnessFunction.java
@@ -0,0 +1,189 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.functionFinder;
+
+import java.util.*;
+import org.jgap.*;
+import org.jgap.impl.*;
+import org.jgap.impl.fitness.*;
+import com.eteks.parser.*;
+import java.io.*;
+
+/**
+ * Fitness Function validating against a value table
+ *
+ * @author Klaus Meffert
+ * @since 2.4
+ */
+public class FormulaFitnessFunction
+    extends TruthTableFitnessFunction {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private static Syntax m_syntax;
+
+  private static ExpressionParser m_parser;
+
+  // Constants for calculating the fitness value
+  // -------------------------------------------
+  public static final float DELTA = 0.000001f;
+
+  public static final int MAX_FITNESS = 9999999;
+
+  private static final int RELATION_FITNESS = 10;
+
+  public static final int WORST = MAX_FITNESS / RELATION_FITNESS;
+
+  public static final int LEAST_FITNESS_VALUE = 0;
+
+  /**
+   * Constructor
+   * @param a_truthTable table of input/output pairs for feeding the formula
+   * and determining the fitness value thru delta computation
+   *
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public FormulaFitnessFunction(Map a_truthTable) {
+    super(a_truthTable);
+    m_syntax = new JavaSyntax();
+    m_parser = new ExpressionParser(m_syntax, null);
+    Repository.init();
+  }
+
+  /**
+   * Implementation of the evaluate method from class FitnessFunction.
+   * Calculates the fitness of a given Chromosome in a determined way.
+   * @param a_chromosome the Chromosome to be evaluated
+   * @return positive integer value representing the fitness of the Chromosome
+   *
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public double evaluate(Chromosome a_chromosome) {
+    String formula = null;
+    try {
+      //Calculcate result of formula
+      formula = Utility.getFormulaFromChromosome(a_chromosome);
+      return calcFitness(formula);
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+      return worstFitness();
+    }
+  }
+
+  public double calcFitness(String formula)
+      throws CompilationException {
+    if (formula == null || formula.length() < 6) {
+      // Minimal length of 6 because the minmimum prefix "F(x)=" has a length
+      // of 5
+      return worstFitness();
+    }
+    // Compile expression with parser
+    CompiledFunction ex1;
+    ex1 = m_parser.compileFunction(formula);
+    Map givenTruthTable = new HashMap();
+    Set keySet = getTruthTable().keySet();
+    Iterator keys = keySet.iterator();
+    while (keys.hasNext()) {
+      Double key = (Double) keys.next();
+      double[] input = new double[] {
+          key.doubleValue()};
+      double value = ex1.computeFunction(input);
+      if (Double.isNaN(value)) {
+        return worstFitness();
+      }
+      givenTruthTable.put(key, new Double(value));
+    }
+    //Calculate fitness
+    return getFitness(calcFitness(givenTruthTable));
+  }
+
+  /**
+   * Helper function: scaled calculation of fitness value
+   * @param a_input unscaled fitness value
+   * @return scaled fitness value
+   *
+   * @author Klaus Meffert
+   * @since 2.2
+   */
+  protected double getFitness(double a_input) {
+    if (Double.isNaN(a_input)) {
+      return worstFitness();
+    }
+    double result;
+    // Normal case
+    if (bestFitness() < worstFitness()) {
+      result = bestFitness() + a_input * RELATION_FITNESS;
+    }
+    else {
+      result = bestFitness() - a_input * RELATION_FITNESS;
+    }
+    if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(result,
+        bestFitness())) {
+      result = bestFitness();
+    }
+    if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(
+      worstFitness(), result)) {
+      // Input too bad --> ignore
+      result = worstFitness();
+    }
+    return result;
+  }
+
+  public static void main(String[] args)
+      throws Exception {
+    Configuration conf = new DefaultConfiguration();
+//    conf.setFitnessEvaluator(new DeltaFitnessEvaluator());
+    Genotype.setConfiguration(conf);
+    File f = new File("C:\\JavaProjekte\\JGAP_CVS\\simpleFormula.properties");
+    FileInputStream fin = new FileInputStream(f);
+    Properties props = new Properties();
+    props.load(fin);
+    Enumeration enum = props.keys();
+    // Read in problem configuration
+    // -----------------------------
+    props.remove("minFitness");
+    props.remove("loopsMin");
+    props.remove("loopsMax");
+    props.remove("maxTerms");
+    props.remove("populationSize");
+    //Read in input-/output-value pairs
+    Map truthTable = new HashMap();
+    while (enum.hasMoreElements()) {
+      String s = (String) enum.nextElement();
+      float inputValue = Float.parseFloat(s);
+      String outS = (String) props.get(s);
+      float outputValue = Float.parseFloat(outS);
+      truthTable.put(new Double(inputValue), new Double(outputValue));
+    }
+    FormulaFitnessFunction ff = new FormulaFitnessFunction(truthTable);
+    System.err.println(ff.calcFitness("F(X)=X*Math.PI*2"));
+  }
+
+  public double worstFitness() {
+    if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(1, 2)) {
+      return MAX_FITNESS;
+    }
+    else {
+      return LEAST_FITNESS_VALUE;
+    }
+  }
+
+  public double bestFitness() {
+    if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(2, 1)) {
+      return MAX_FITNESS;
+    }
+    else {
+      return LEAST_FITNESS_VALUE;
+    }
+  }
+}
