5d1dc889b400eff5f1c54c30b675885b94b99698
Klaus Meffert <kontakt@doktor-meffert.de>
2005-06-18 18:41:03 +0000

    added new class for network-related functionality
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1344 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/util/NetworkKit.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.util;
+
+import java.net.*;
+
+/**
+ * Houses network-related functionality.
+ *
+ * @author Klaus Meffert
+ * @since 2.4
+ */
+public class NetworkKit {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * @return IP address string in textual presentation
+   * @throws UnknownHostException
+   *
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public static String getLocalIPAddress()
+      throws UnknownHostException {
+    return InetAddress.getLocalHost().getHostAddress();
+  }
+
+  /**
+   * @return name of the computer calling this function locally
+   * @throws UnknownHostException
+   *
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public static String getLocalHostName()
+      throws UnknownHostException {
+    return InetAddress.getLocalHost().getHostName();
+  }
+
+}
