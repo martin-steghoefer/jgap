b91d7e02af7fdd698948ef2b18db14ef5d7935bf
Klaus Meffert <kontakt@doktor-meffert.de>
2005-06-25 14:43:33 +0000

    new example, see request 1227407
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1391 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ examples/src/examples/chromInit/ChromosomeInit.java
@@ -0,0 +1,111 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.chromInit;
+
+import org.jgap.*;
+import org.jgap.impl.*;
+
+/**
+ * Simple test class that demonstrates how to initialize chromosomes with
+ * different numbers of Gene's.
+ *
+ * @author Klaus Meffert
+ * @since 2.4
+ */
+public class ChromosomeInit {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private static final String CVS_REVISION = "$Revision: 1.1 $";
+
+  public static void main(String[] args) {
+    int numEvolutions = 500;
+    Configuration gaConf = new DefaultConfiguration();
+    gaConf.setPreservFittestIndividual(true);
+    gaConf.setKeepPopulationSizeConstant(false);
+    Genotype.setConfiguration(gaConf);
+    try {
+      int chromeSize = 16;
+      if (chromeSize > 32) {
+        System.err.println("This example does not handle " +
+                           "Chromosomes greater than 32 bits in length.");
+        System.exit( -1);
+      }
+      Chromosome sampleChromosome = new Chromosome(new BooleanGene(),
+          chromeSize);
+      gaConf.setSampleChromosome(sampleChromosome);
+      gaConf.setPopulationSize(20);
+      gaConf.setFitnessFunction(new MaxFunction());
+      // Completely initialize the population with custom code.
+      // Notice that we assign the double number of Gene's to
+      // each other Chromosome.
+      // ------------------------------------------------------
+      int populationSize = gaConf.getPopulationSize();
+      Population pop = new Population(populationSize);
+      for (int i = 0; i < populationSize; i++) {
+        int mult;
+        // Every second Chromosome has double the number of Gene's.
+        // --------------------------------------------------------
+        if (i % 2 == 0) {
+          mult = 1;
+        }
+        else {
+          mult = 2;
+        }
+        Chromosome chrom = Chromosome.randomInitialChromosome();
+        Gene[] sampleGenes = sampleChromosome.getGenes();
+        Gene[] newGenes = new Gene[sampleGenes.length * mult];
+        RandomGenerator generator = Genotype.getConfiguration().
+            getRandomGenerator();
+        for (int j = 0; j < newGenes.length; j = j + mult) {
+          // We use the newGene() method on each of the genes in the
+          // sample Chromosome to generate our new Gene instances for
+          // the Chromosome we're returning. This guarantees that the
+          // new Genes are setup with all of the correct internal state
+          // for the respective gene position they're going to inhabit.
+          // ----------------------------------------------------------
+          newGenes[j] = sampleGenes[j / mult].newGene();
+          // Set the gene's value (allele) to a random value.
+          // ------------------------------------------------
+          newGenes[j].setToRandomValue(generator);
+          if (mult > 1) {
+            newGenes[j + 1] = sampleGenes[j / 2].newGene();
+            // Set the gene's value (allele) to a random value.
+            // ------------------------------------------------
+            newGenes[j + 1].setToRandomValue(generator);
+          }
+        }
+        pop.addChromosome(chrom);
+      }
+      // Now we need to construct the Genotype. This could otherwise be
+      // accomplished more easily by writing
+      // Genotype genotype = Genotype.randomInitialGenotype(...)
+      Genotype genotype = new Genotype(gaConf, pop);
+      int progress = 0;
+      int percentEvolution = numEvolutions / 100;
+      for (int i = 0; i < numEvolutions; i++) {
+        genotype.evolve();
+        // Print progress.
+        // ---------------
+        if (percentEvolution > 0 && i % percentEvolution == 0) {
+          progress++;
+          Chromosome fittest = genotype.getFittestChromosome();
+          double fitness = fittest.getFitnessValue();
+          System.out.println("Fittest Chromosome has value " + fitness);
+        }
+      }
+      Chromosome fittest = genotype.getFittestChromosome();
+      System.out.println("Fittest Chromosome has value " +
+                         fittest.getFitnessValue());
+    }
+    catch (InvalidConfigurationException e) {
+      e.printStackTrace();
+      System.exit( -2);
+    }
+  }
+}
---
+++ examples/src/examples/chromInit/MaxFunction.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.chromInit;
+
+import org.jgap.*;
+import org.jgap.impl.*;
+
+/**
+ * Fitness function for our example. See evolve() method for details
+ *
+ * @author Neil Rotstan
+ * @author Klaus Meffert
+ * @since 2.4
+ */
+public class MaxFunction
+    extends FitnessFunction {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * See examples.simpleBoolean.MaxFunction for description.
+   * Please notice that in this example here, we have chromosomes with one and
+   * Chromosomes with two Genes, so that the description from the original
+   * MaxFunction does not apply directly here.
+   * @param a_subject the Chromosome to be evaluated
+   * @return defect rate of our problem
+   *
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public double evaluate(Chromosome a_subject) {
+    int total = 0;
+
+    for (int i = 0; i < a_subject.size(); i++) {
+      BooleanGene value = (BooleanGene) a_subject.getGene(a_subject.size() -
+          (i + 1));
+      if (value.booleanValue()) {
+        total += Math.pow(2.0, (double) i);
+      }
+    }
+
+    return total;
+  }
+}
