6ab366fcd9840680f655c8843d41671fb8662196
Klaus Meffert <kontakt@doktor-meffert.de>
2005-07-04 15:14:55 +0000

    added test
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1413 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/audit/EvaluatorTest.java
+++ tests/org/jgap/audit/EvaluatorTest.java
@@ -11,7 +11,6 @@ package org.jgap.audit;
 
 import org.jgap.*;
 import org.jgap.impl.*;
-
 import junit.framework.*;
 
 /**
@@ -23,7 +22,7 @@ import junit.framework.*;
 public class EvaluatorTest
     extends TestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   private final static double DELTA = 0.00000001d;
 
@@ -92,7 +91,6 @@ public class EvaluatorTest
     pop.addChromosome(chrom);
     Genotype genotype = new Genotype(conf, pop);
     eval.storeGenotype(0, 0, genotype);
-
     Evaluator.GenotypeDataAvg avg = eval.calcPerformance(0);
     assertEquals(pop.determineFittestChromosome().getFitnessValue(),
                  avg.bestFitnessValue, DELTA);
@@ -124,7 +122,6 @@ public class EvaluatorTest
     pop.addChromosome(chrom);
     Genotype genotype = new Genotype(conf, pop);
     eval.storeGenotype(0, 0, genotype);
-
     // run 1
     pop = new Population();
     genes = new Gene[1];
@@ -141,7 +138,6 @@ public class EvaluatorTest
     pop.addChromosome(chrom);
     genotype = new Genotype(conf, pop);
     eval.storeGenotype(0, 1, genotype);
-
     Evaluator.GenotypeDataAvg avg = eval.calcPerformance(0);
     assertEquals(pop.determineFittestChromosome().getFitnessValue(),
                  avg.bestFitnessValue, DELTA);
@@ -153,7 +149,7 @@ public class EvaluatorTest
                  avg.avgDiversityFitnessValue, DELTA);
     assertEquals( (Math.abs(19 - 11.4d)) / 1, avg.avgBestDeltaFitnessValue,
                  DELTA);
-    assertEquals((double)3 / 2 + (double)3 / 2, avg.sizeAvg, DELTA);
+    assertEquals( (double) 3 / 2 + (double) 3 / 2, avg.sizeAvg, DELTA);
   }
 
   public void testGetNumberOfRuns_0()
@@ -179,5 +175,107 @@ public class EvaluatorTest
     assertEquals(1, eval.getNumberOfRuns(0));
     assertEquals(0, eval.getNumberOfRuns(1));
   }
-  /**@todo do with 2 generations and 2 runs per generation*/
+
+  /**
+   * @throws Exception
+   *
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public void testCalcPerformance_2()
+      throws Exception {
+    Configuration conf = new ConfigurationForTest();
+    PermutingConfiguration pconf = new PermutingConfiguration(conf);
+    Evaluator eval = new Evaluator(pconf);
+    // run 0, permutation 0
+    Population pop00 = new Population();
+    Gene[] genes = new Gene[1];
+    Gene gene = new BooleanGene();
+    genes[0] = gene;
+    Chromosome chrom = new Chromosome(genes);
+    chrom.setFitnessValue(7.3d);
+    pop00.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(4.8d);
+    pop00.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(11.4d);
+    pop00.addChromosome(chrom);
+    Genotype genotype = new Genotype(conf, pop00);
+    eval.storeGenotype(0, 0, genotype);
+    // run 1, permutation 0
+    Population pop10 = new Population();
+    genes = new Gene[1];
+    gene = new BooleanGene();
+    genes[0] = gene;
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(7);
+    pop10.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(17);
+    pop10.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(19);
+    pop10.addChromosome(chrom);
+    genotype = new Genotype(conf, pop10);
+    eval.storeGenotype(0, 1, genotype);
+    // run 0, permutation 1
+    Population pop01 = new Population();
+    genes = new Gene[1];
+    gene = new BooleanGene();
+    genes[0] = gene;
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(4);
+    pop01.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(9);
+    pop01.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(8);
+    pop01.addChromosome(chrom);
+    genotype = new Genotype(conf, pop01);
+    eval.storeGenotype(1, 0, genotype);
+    // run 1, permutation 1
+    Population pop11 = new Population();
+    genes = new Gene[1];
+    gene = new BooleanGene();
+    genes[0] = gene;
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(14);
+    pop11.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(11);
+    pop11.addChromosome(chrom);
+    chrom = new Chromosome(genes);
+    chrom.setFitnessValue(28);
+    pop11.addChromosome(chrom);
+    genotype = new Genotype(conf, pop11);
+    eval.storeGenotype(1, 1, genotype);
+    Evaluator.GenotypeDataAvg avg = eval.calcPerformance(0);
+    assertEquals(Math.max(pop00.determineFittestChromosome().getFitnessValue(),
+                          pop10.determineFittestChromosome().getFitnessValue()),
+                 avg.bestFitnessValue, DELTA);
+    assertEquals(0, avg.bestFitnessValueGeneration);
+    assertEquals( ( (7.3 + 4.8 + 11.4) / 3) / 2 +
+                 ( (7.0d + 17.0d + 19) / 3) / 2, avg.avgFitnessValue, DELTA);
+    assertEquals( (Math.abs(4.8 - 7.3) / 2 + Math.abs(11.4 - 4.8) / 2) / 2
+                 + (Math.abs(17.0 - 7) / 2 + Math.abs(19.0 - 17) / 2) / 2,
+                 avg.avgDiversityFitnessValue, DELTA);
+    assertEquals( (Math.abs(19 - 11.4d)) / 1, avg.avgBestDeltaFitnessValue,
+                 DELTA);
+    assertEquals( (double) 3 / 2 + (double) 3 / 2, avg.sizeAvg, DELTA);
+    avg = eval.calcPerformance(1);
+    assertEquals(Math.max(pop01.determineFittestChromosome().getFitnessValue(),
+                          pop11.determineFittestChromosome().getFitnessValue()),
+                 avg.bestFitnessValue, DELTA);
+    assertEquals(0, avg.bestFitnessValueGeneration);
+    assertEquals( ( (4 + 9 + 8.0d) / 3) / 2 +
+                 ( (14 + 11.0d + 28) / 3) / 2, avg.avgFitnessValue, DELTA);
+    assertEquals( (Math.abs(9.0d - 4) / 2 + Math.abs(8.0d - 9) / 2) / 2
+                 + (Math.abs(11.0d - 14) / 2 + Math.abs(28.0d - 11) / 2) / 2,
+                 avg.avgDiversityFitnessValue, DELTA);
+    assertEquals( (Math.abs(28 - 9)) / 1, avg.avgBestDeltaFitnessValue,
+                 DELTA);
+    assertEquals( (double) 3 / 2 + (double) 3 / 2, avg.sizeAvg, DELTA);
+  }
 }
