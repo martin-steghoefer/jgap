074c053952cd96efdd56f6b9badd97aeb33b3ab6
Klaus Meffert <kontakt@doktor-meffert.de>
2005-10-08 20:18:39 +0000

    added new classes for tests
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1498 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/util/AllUtilTests.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.util;
+
+import junit.framework.*;
+
+/**
+ * Test suite for all tests of package org.jgap.util
+*
+ * @author Klaus Meffert
+ * @since 2.5
+ */
+public class AllUtilTests
+    extends TestSuite {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite();
+    suite.addTest(PluginDiscovererTest.suite());
+    return suite;
+  }
+}
---
+++ tests/org/jgap/util/PluginDiscovererTest.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.util;
+
+import org.jgap.*;
+import junit.framework.*;
+
+/**
+ * Tests the PluginDiscoverer class
+ *
+ * @author Klaus Meffert
+ * @since 2.5
+ */
+public class PluginDiscovererTest
+    extends JGAPTestCase {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite(PluginDiscovererTest.class);
+    return suite;
+  }
+
+  public void testConstruct_0() {
+    /**@todo implement*/
+  }
+  /**@todo add further tests*/
+}
