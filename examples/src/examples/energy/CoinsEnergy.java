51212690f10fa9d607d31018c5d55d99557192e5
Klaus Meffert <kontakt@doktor-meffert.de>
2005-12-02 09:28:04 +0000

    added to CVS
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1530 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ examples/src/examples/energy/CoinsEnergy.java
@@ -0,0 +1,211 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.energy;
+
+import org.jgap.*;
+import org.jgap.impl.*;
+
+/**
+ * THIS EXAMPLE IS NOT IMPLEMENTED FULLY!
+ * For general description, see examples.MinimizingMakeChange.<p>
+ * Additionally, each to coin an energy value is assigned (new feature since
+ * JGAP version 2.4). Energy is interpreted here as weight of a coin. You could
+ * think of a coins holder that wants a low total weight as possible and that
+ * is capable of only holding a given maximum weight.
+ *
+ * @author Klaus Meffert
+ * @since 2.4
+ */
+public class CoinsEnergy {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * The total number of times we'll let the population evolve.
+   */
+  private static final int MAX_ALLOWED_EVOLUTIONS = 200;
+
+  /**
+   * Executes the genetic algorithm to determine the minimum number of
+   * coins necessary to make up the given target amount of change. The
+   * solution will then be written to System.out.
+   *
+   * @param a_targetChangeAmount the target amount of change for which this
+   * method is attempting to produce the minimum number of coins
+   * @param a_maxWeight the maximum weight allowed in sum over all coins
+   * @throws Exception
+   *
+   * @author Neil Rotstan
+   * @author Klaus Meffert
+   * @since 1.0
+   */
+  public static void makeChangeForAmount(int a_targetChangeAmount,
+                                         double a_maxWeight)
+      throws Exception {
+    // Start with a DefaultConfiguration, which comes setup with the
+    // most common settings.
+    // -------------------------------------------------------------
+    Configuration conf = new DefaultConfiguration();
+    conf.setPreservFittestIndividual(true);
+    conf.setKeepPopulationSizeConstant(false);
+    // Set the fitness function we want to use, which is our
+    // MinimizingMakeChangeFitnessFunction. We construct it with
+    // the target amount of change passed in to this method.
+    // ---------------------------------------------------------
+    FitnessFunction myFunc =
+        new CoinsEnergyFitnessFunction(a_targetChangeAmount, a_maxWeight);
+//    conf.setFitnessFunction(myFunc);
+    conf.setBulkFitnessFunction(new BulkFitnessOffsetRemover(myFunc));
+    // Now we need to tell the Configuration object how we want our
+    // Chromosomes to be setup. We do that by actually creating a
+    // sample Chromosome and then setting it on the Configuration
+    // object. As mentioned earlier, we want our Chromosomes to each
+    // have four genes, one for each of the coin types. We want the
+    // values (alleles) of those genes to be integers, which represent
+    // how many coins of that type we have. We therefore use the
+    // IntegerGene class to represent each of the genes. That class
+    // also lets us specify a lower and upper bound, which we set
+    // to sensible values for each coin type.
+    // --------------------------------------------------------------
+    Gene[] sampleGenes = new Gene[4];
+    IntegerGene gene = new IntegerGene(0, 3 * 10);
+    gene.setConstraintChecker(new EnergyGeneConstraintChecker());
+    sampleGenes[0] = gene; // Quarters
+    sampleGenes[1] = new IntegerGene(0, 2 * 10); // Dimes
+    sampleGenes[2] = new IntegerGene(0, 1 * 10); // Nickels
+    sampleGenes[3] = new IntegerGene(0, 4 * 10); // Pennies
+    Chromosome sampleChromosome = new Chromosome(sampleGenes);
+    conf.setSampleChromosome(sampleChromosome);
+    // Finally, we need to tell the Configuration object how many
+    // Chromosomes we want in our population. The more Chromosomes,
+    // the larger number of potential solutions (which is good for
+    // finding the answer), but the longer it will take to evolve
+    // the population (which could be seen as bad).
+    // ------------------------------------------------------------
+    conf.setPopulationSize(80);
+
+    // Create random initial population of Chromosomes.
+    // ------------------------------------------------
+    Genotype population = Genotype.randomInitialGenotype(conf);
+    // Initialize energys of Gene's. Each Gene represents a coin with a
+    // specific value, and each coin with different value has a specific
+    // weight. Not necessarily a higher weight for higher coin values!
+    // (as in real life!).
+    /**@todo */
+
+    // Evolve the population. Since we don't know what the best answer
+    // is going to be, we just evolve the max number of times.
+    // ---------------------------------------------------------------
+    for (int i = 0; i < MAX_ALLOWED_EVOLUTIONS; i++) {
+      population.evolve();
+    }
+
+    // Display the best solution we found.
+    // -----------------------------------
+    Chromosome bestSolutionSoFar = population.getFittestChromosome();
+    System.out.println("The best solution has a fitness value of " +
+                       bestSolutionSoFar.getFitnessValue());
+    System.out.println("It contained the following: ");
+    System.out.println("\t" +
+                       CoinsEnergyFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 0) + " quarters.");
+    System.out.println("\t" +
+                       CoinsEnergyFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 1) + " dimes.");
+    System.out.println("\t" +
+                       CoinsEnergyFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 2) + " nickels.");
+    System.out.println("\t" +
+                       CoinsEnergyFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 3) + " pennies.");
+    System.out.println("For a total of " +
+                       CoinsEnergyFitnessFunction.amountOfChange(
+        bestSolutionSoFar) + " cents in " +
+                       CoinsEnergyFitnessFunction.
+                       getTotalNumberOfCoins(
+        bestSolutionSoFar) + " coins.");
+  }
+
+  /**
+   * Main method. A single command-line argument is expected, which is the
+   * amount of change to create (in other words, 75 would be equal to 75
+   * cents).
+   *
+   * @param args amount of change in cents to create
+   * @throws Exception
+   *
+   * @author Neil Rotstan
+   * @author Klaus Meffert
+   * @since 1.0
+   */
+  public static void main(String[] args)
+      throws Exception {
+    if (args.length != 2) {
+      System.out.println("Syntax: MinimizingMakeChange <amount> <max weight>");
+    }
+    else {
+      int amount = getValue(args, 0);
+      int weight = getValue(args, 1);
+      makeChangeForAmount(amount, weight);
+    }
+  }
+
+  protected static int getValue(String[] args, int index) {
+    int value;
+    try {
+      value = Integer.parseInt(args[index]);
+      return value;
+    }
+    catch (NumberFormatException e) {
+      System.out.println(
+          "The " + (index+1) + ". argument must be a valid integer value");
+      System.exit(1);
+      return -1; // does not matter
+    }
+  }
+
+  /**
+   * Uses to set the energy when a new allele is set
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public static class EnergyGeneConstraintChecker implements IGeneConstraintChecker {
+    public static double[] coinWeights = {1.0d, 2.0d, 8.0d, 3.0d};
+
+    /**
+     * Check if a given allele value is valid for the given gene instance.
+     * @param a_gene the gene the given allele is to be validated for
+     * @param a_alleleValue the allele value to be validated
+     * @return true: allele may be set for gene; false: validity check failed
+     * @throws RuntimeException if the checker cannot decide whether the given
+     * allele is valid or not
+     *
+     * @author Klaus Meffert
+     * @since 2.4
+     */
+    public boolean verify(Gene a_gene, Object a_alleleValue)
+        throws RuntimeException {
+      double computedWeight = 0.0d;/**todo compute*/
+      // We need to figure out what type of coin (penny, nickel, dime, quarter)
+      // the current Gene represents. This is not trivial as it depends on the
+      // index of the Gene within the Chromosome. The Chromosome is not
+      // accessible by the Gene!
+      // ----------------------------------------------------------------------
+      a_gene.setEnergy(computedWeight);
+      // No verification here, always conform.
+      // -------------------------------------
+      return true;
+    }
+  }
+}
---
+++ examples/src/examples/energy/CoinsEnergyFitnessFunction.java
@@ -0,0 +1,272 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.energy;
+
+import org.jgap.*;
+
+/**
+ * Sample fitness function for the CoinsEnergy example. Adapted from
+ * examples.MinimizingMakeChangeFitnessFunction
+ *
+ * @author Klaus Meffert
+ * @since 2.4
+ */
+public class CoinsEnergyFitnessFunction
+    extends FitnessFunction {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private final int m_targetAmount;
+
+  private final double m_maxWeight;
+
+  public static final int MAX_BOUND = 1000;
+  public static final double MAX_WEIGHT = 100;
+
+  private static final double ZERO_DIFFERENCE_FITNESS = Math.sqrt(MAX_BOUND);
+
+  public CoinsEnergyFitnessFunction(int a_targetAmount, double a_maxWeight) {
+    if (a_targetAmount < 1 || a_targetAmount >= MAX_BOUND) {
+      throw new IllegalArgumentException(
+          "Change amount must be between 1 and " + MAX_BOUND + " cents.");
+    }
+
+    if (a_maxWeight < 0 || a_maxWeight >= MAX_WEIGHT) {
+      throw new IllegalArgumentException(
+          "Max weight must be greater than 0 and not greater than "
+          + MAX_WEIGHT + " grammes");
+    }
+    m_targetAmount = a_targetAmount;
+    m_maxWeight = a_maxWeight;
+  }
+
+  /**
+   * Determine the fitness of the given Chromosome instance. The higher the
+   * return value, the more fit the instance. This method should always
+   * return the same fitness value for two equivalent Chromosome instances.
+   *
+   * @param a_subject the Chromosome instance to evaluate
+   *
+   * @return positive double reflecting the fitness rating of the given
+   * Chromosome
+   * @since 2.0 (until 1.1: return type int)
+   * @author Neil Rotstan, Klaus Meffert, John Serri
+   */
+  public double evaluate(Chromosome a_subject) {
+    // The fitness value measures both how close the value is to the
+    // target amount supplied by the user and the total number of coins
+    // represented by the solution. We do this in two steps: first,
+    // we consider only the represented amount of change vs. the target
+    // amount of change and return higher fitness values for amounts
+    // closer to the target, and lower fitness values for amounts further
+    // away from the target. Then we go to step 2, which returns a higher
+    // fitness value for solutions representing fewer total coins, and
+    // lower fitness values for solutions representing more total coins.
+    // ------------------------------------------------------------------
+    int changeAmount = amountOfChange(a_subject);
+    int totalCoins = getTotalNumberOfCoins(a_subject);
+    int changeDifference = Math.abs(m_targetAmount - changeAmount);
+
+    double fitness = 0.0d;
+
+    // Step 1: Determine total sum of energies (interpreted as weights here)
+    // of coins. If higher than the given maximum value, the solution is not
+    // accepted in any way, i.e. the fitness value is then set to the worst
+    // value.
+    double totalWeight = getTotalWeight(a_subject);
+    if (totalWeight > m_maxWeight) {
+      if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(2, 1)) {
+        return 1.0d;
+      }
+      else {
+        return MAX_BOUND;
+      }
+    }
+
+    // Step 2: Determine distance of amount represented by solution from
+    // the target amount.
+    // -----------------------------------------------------------------
+    if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(2, 1)) {
+      fitness += changeDifferenceBonus(MAX_BOUND, changeDifference);
+    }
+    else {
+      fitness -= changeDifferenceBonus(MAX_BOUND, changeDifference);
+    }
+
+    // Step 3: We divide the fitness value by a penalty based on the number of
+    // coins. The higher the number of coins the higher the penalty and the
+    // smaller the fitness value.
+    // And inversely the smaller number of coins in the solution the higher
+    // the resulting fitness value.
+    // -----------------------------------------------------------------------
+    if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(2, 1)) {
+      fitness -= computeCoinNumberPenalty(MAX_BOUND, totalCoins);
+    }
+    else {
+      fitness += computeCoinNumberPenalty(MAX_BOUND, totalCoins);
+    }
+
+    // Step 4: Penalize higher weight (= engery) values.
+    // -------------------------------------------------
+    if (Genotype.getConfiguration().getFitnessEvaluator().isFitter(2, 1)) {
+      fitness -= computeWeightPenalty(MAX_BOUND, totalWeight);
+    }
+    else {
+      fitness += computeWeightPenalty(MAX_BOUND, totalWeight);
+    }
+
+
+    // Make sure fitness value is always positive.
+    // -------------------------------------------
+    return Math.max(1.0d, fitness);
+  }
+
+  /**
+   * Bonus calculation of fitness value.
+   * @param a_maxFitness maximum fitness value appliable
+   * @param a_changeDifference change difference in coins for the coins problem
+   * @return bonus for given change difference
+   * @author Klaus Meffert
+   * @since 2.3
+   */
+  protected double changeDifferenceBonus(double a_maxFitness,
+                                         int a_changeDifference) {
+    if (a_changeDifference == 0) {
+      return a_maxFitness;
+    }
+    else {
+      // we arbitrarily work with half of the maximum fitness as basis for
+      // non-optimal solutions (concerning change difference)
+      return a_maxFitness / 2 - a_changeDifference * a_changeDifference;
+    }
+  }
+
+  /**
+   * Calculates the penalty to apply to the fitness value based on the ammount
+   * of coins in the solution
+   *
+   * @param a_maxFitness maximum fitness value allowed
+   * @param a_coins number of coins in the solution
+   * @return penalty for the fitness value base on the number of coins
+   *
+   * @author John Serri
+   * @since 2.4
+   */
+  protected double computeCoinNumberPenalty(double a_maxFitness, int a_coins) {
+    if (a_coins == 1) {
+      // we know the solution cannot have less than one coin
+      return 0;
+    }
+    else {
+      // The more coins the more penalty, but not more than the maximum
+      // fitness value possible. Let's avoid linear behavior and use
+      // exponential penalty calculation instead
+      return (Math.min(a_maxFitness, a_coins * a_coins));
+    }
+  }
+
+  /**
+   * Calculates the total amount of change (in cents) represented by
+   * the given potential solution and returns that amount.
+   *
+   * @param a_potentialSolution the potential solution to evaluate
+   * @return The total amount of change (in cents) represented by the
+   * given solution
+   *
+   * @author Neil Rotstan
+   * @since 1.0
+   */
+  public static int amountOfChange(Chromosome a_potentialSolution) {
+    int numQuarters = getNumberOfCoinsAtGene(a_potentialSolution, 0);
+    int numDimes = getNumberOfCoinsAtGene(a_potentialSolution, 1);
+    int numNickels = getNumberOfCoinsAtGene(a_potentialSolution, 2);
+    int numPennies = getNumberOfCoinsAtGene(a_potentialSolution, 3);
+    return (numQuarters * 25) + (numDimes * 10) + (numNickels * 5) +
+        numPennies;
+  }
+
+  /**
+   * Retrieves the number of coins represented by the given potential
+   * solution at the given gene position.
+   *
+   * @param a_potentialSolution the potential solution to evaluate
+   * @param a_position the gene position to evaluate
+   * @return the number of coins represented by the potential solution at the
+   * given gene position
+   *
+   * @author Neil Rotstan
+   * @since 1.0
+   */
+  public static int getNumberOfCoinsAtGene(Chromosome a_potentialSolution,
+                                           int a_position) {
+    Integer numCoins =
+        (Integer) a_potentialSolution.getGene(a_position).getAllele();
+    return numCoins.intValue();
+  }
+
+  /**
+   * Returns the total number of coins represented by all of the genes in
+   * the given potential solution.
+   *
+   * @param a_potentialsolution the potential solution to evaluate
+   * @return total number of coins represented by the given Chromosome
+   *
+   * @author Neil Rotstan
+   * @since 2.4
+   */
+  public static int getTotalNumberOfCoins(Chromosome a_potentialsolution) {
+    int totalCoins = 0;
+    int numberOfGenes = a_potentialsolution.size();
+    for (int i = 0; i < numberOfGenes; i++) {
+      totalCoins += getNumberOfCoinsAtGene(a_potentialsolution, i);
+    }
+    return totalCoins;
+  }
+
+  /**
+   * Returns the total weight of all coins.
+   *
+   * @param a_potentialSolution the potential solution to evaluate
+   * @return total weight of all coins
+   *
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  public static double getTotalWeight(Chromosome a_potentialSolution) {
+    double totalWeight = 0.0d;
+    int numberOfGenes = a_potentialSolution.size();
+    for (int i = 0; i < numberOfGenes; i++) {
+      totalWeight += a_potentialSolution.getGene(i).getEnergy();
+    }
+    return totalWeight;
+  }
+
+  /**
+   *
+   * @param a_maxFitness
+   * @param a_weight
+   * @return
+   * @author Klaus Meffert
+   * @since 2.4
+   */
+  protected double computeWeightPenalty(double a_maxFitness, double a_weight) {
+    if (a_weight <= 0) {
+      // we know the solution cannot have less than one coin
+      return 0;
+    }
+    else {
+      // The more weight the more penalty, but not more than the maximum
+      // fitness value possible. Let's avoid linear behavior and use
+      // exponential penalty calculation instead
+      return (Math.min(a_maxFitness, a_weight * a_weight));
+    }
+  }
+
+}
