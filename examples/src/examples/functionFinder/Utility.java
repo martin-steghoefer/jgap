8fab970aab35abcca4d98df75147f8416abeae89
Klaus Meffert <kontakt@doktor-meffert.de>
2005-12-29 21:25:55 +0000

    fixed cvs problem
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1562 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ examples/src/examples/functionFinder/Utility.java
@@ -0,0 +1,205 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.functionFinder;
+
+import java.util.*;
+
+import org.jgap.*;
+import org.jgap.impl.*;
+
+/**
+ * Utility functions
+ *
+ * @author Klaus Meffert
+ * @since 2.2
+ */
+public class Utility {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.5 $";
+
+  private static int numberOfFunctions;
+
+  private static int numberOfConstants;
+
+  private static int numberOfOperators;
+
+  /**
+   * Static initializers
+   */
+  static {
+    numberOfFunctions = Repository.getFunctions().size();
+    numberOfConstants = Repository.getConstants().size();
+    numberOfOperators = Repository.getOperators().size();
+  }
+
+  /**
+   * Constructs terms that are represented by a list of genes
+   * @param genes Gene[] list of genes representing terms
+   * @return Vector list list of terms
+   *
+   * @author Klaus Meffert
+   * @since 2.2
+   */
+  public static List constructTerms(Gene[] genes) {
+    List result = new Vector();
+    Term term;
+    Gene[] geneTupel;
+    CompositeGene comp;
+    Gene gene1;
+    Gene gene2;
+    for (int i = 0; i < genes.length; i++) {
+      comp = (CompositeGene) genes[i];
+      gene1 = comp.geneAt(0);
+      gene2 = comp.geneAt(1);
+      geneTupel = new Gene[] {
+          gene1, gene2};
+      term = constructTerm(geneTupel);
+
+      result.add(term);
+    }
+    return result;
+  }
+
+  /**
+   * Constructs a term that is represented by a gene tupel (containing a tupel)
+   * @param genes Gene[]
+   * @return Term
+   *
+   * @author Klaus Meffert
+   * @since 2.2
+   */
+  public static Term constructTerm(Gene[] genes) {
+    Gene op = genes[1];
+    Integer allele = (Integer) genes[0].getAllele();
+    int fktNr = allele.intValue();
+    fktNr = fktNr % (numberOfFunctions + numberOfConstants);
+    String fktName;
+    int type;
+    if (fktNr >= numberOfFunctions) {
+      // Read out constant
+      fktName = (String) Repository.getConstants().get(fktNr -
+          numberOfFunctions);
+      type = 1;
+    }
+    else {
+      // Read out function
+      fktName = (String) Repository.getFunctions().get(fktNr);
+      type = 2;
+    }
+
+    allele = (Integer) op.getAllele();
+    int opNr = allele.intValue();
+    opNr = opNr % numberOfOperators;
+    char opCh = ( (String) Repository.getOperators().get(opNr)).charAt(0);
+    Term elem = new Term(type, fktName, 1, opCh);
+    return elem;
+  }
+
+  public static String getFormulaFromChromosome(Chromosome chromosome) {
+    List terms = constructTerms(chromosome.getGenes());
+    return getFormula(terms);
+  }
+
+  /**
+   * Constructs a formula string out of terms (each containing operators,
+   * if applicable)
+   * @param elements ordered list of terms
+   * @return constructed formula
+   *
+   * @author Klaus Meffert
+   * @since 2.2
+   */
+  public static String getFormula(List elements) {
+    if (elements == null || elements.isEmpty()) {
+      return "";
+    }
+    else {
+      String result = "";
+      do {
+        result += getFormula_int(elements, result);
+      }
+      while (elements.size() > 0);
+      if (result.length() < 1) {
+        return "";
+      }
+      else {
+        return "F(X)=" + result;
+      }
+    }
+  }
+
+  /**
+   * Recursive part of getFormula
+   * @param elements ordered list of terms
+   * @param previous needed for recursion
+   * @return constructed part of formula
+   *
+   * @author Klaus Meffert
+   * @since 2.2
+   */
+  protected static String getFormula_int(List elements, String previous) {
+    if (elements.size() < 1) {
+      return "";
+    }
+    Term element = (Term) elements.get(0);
+    element = replaceSubstitute(element);
+    String result = "";
+    // Ignore illegal operator specification
+    if (element.operator != ' ' && previous.length() > 0 &&
+        !previous.endsWith("(")) {
+      result += element.operator;
+    }
+    int type = element.termType;
+    result += element.termName;
+    elements.remove(0);
+    if (type == 2) {
+      result += "(";
+      boolean compensationPossible = true;
+      do {
+        element.depth--;
+        String tempRes = getFormula_int(elements, result);
+        if (tempRes.length() > 0) {
+          compensationPossible = false;
+        }
+        result += tempRes;
+      }
+      while (element.depth > 0);
+      if (compensationPossible) {
+        result += "X";
+      }
+      result += ")";
+    }
+    else if (type == 1) {
+      element.depth--;
+    }
+    else {
+      throw new RuntimeException("Invalid operator type: " + type);
+    }
+    return result;
+  }
+
+  private static Term replaceSubstitute(Term element) {
+    if (element.termName.equals("+I")) {
+      element.termName = String.valueOf(new Random().nextInt(3) + 1);
+    }
+    else if (element.termName.equals("-I")) {
+      element.termName = String.valueOf( - (new Random().nextInt(10) + 1));
+    }
+    else if (element.termName.equals("+D")) {
+      element.termName = String.valueOf(new Random().nextDouble() * 10);
+      /**@todo recalculate fitnessValue of all Chromosomes using "+D"*/
+    }
+    else if (element.termName.equals("-D")) {
+      element.termName = String.valueOf( - (new Random().nextDouble() * 10));
+      /**@todo recalculate fitnessValue of all Chromosomes using "-D"*/
+    }
+    return element;
+  }
+}
---
+++ examples/src/examples/functionFinder/test/FitnessValueTest.java
@@ -0,0 +1,152 @@
+package examples.functionFinder.test;
+
+import examples.functionFinder.*;
+import org.jgap.*;
+
+/**
+ * Tests the FitnessValue class
+ *
+ * @author Klaus Meffert
+ * @since 2.2
+ */
+public class FitnessValueTest extends JGAPTestCase {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private static final String CVS_REVISION = "$Revision: 1.4 $";
+
+  private MatchAgainstTruthTable matcher;
+
+  public FitnessValueTest() {
+    super();
+    matcher = new MatchAgainstTruthTable(null);
+  }
+
+  public double getFitness(float input) throws Throwable {
+    double fitness = ((Double)privateAccessor.invoke(matcher, "scaleFitness", new Class[] {float.class}
+                                            , new Object[] {new Float(input)})).doubleValue();
+    assertTrue(fitness >= 0);
+    return fitness;
+  }
+
+  public void testFitness0_1() throws Throwable{
+    //fitness for diff 0 must be maximum
+    assertEquals(MatchAgainstTruthTable.MAX_FITNESS, getFitness(0),MatchAgainstTruthTable.DELTA);
+    //fitness for diff 0 must be greater than for diff 1.1
+    assertTrue(getFitness(0) > getFitness(1.1f));
+    //fitness for diff 0 must be greater than for diff 1.0001
+    assertTrue(getFitness(0) > getFitness(1.0001f));
+  }
+
+  public void testFitness0_2() throws Throwable{
+    //fitness for diff 0 must be greater than for diff 1.0
+    double diff1 = getFitness(0);
+    double diff2 = getFitness(1.0f);
+    assertTrue( diff1 > diff2);
+  }
+
+  public void testFitness0_05() throws Throwable{
+    //fitness for diff 0 must be greater than for diff 0.5
+    assertTrue(getFitness(0) > getFitness(0.5f));
+  }
+
+  public void testFitness01_02() throws Throwable{
+    //fitness for diff 0.01 must be greater/equal than for diff 0.2
+    double diff1 = getFitness(0.01f);
+    double diff2 = getFitness(0.2f);
+    assertTrue(diff1 >= diff2);
+  }
+
+  public void testFitness001_01() throws Throwable{
+    //fitness for diff 0.01 must be greater/equal than for diff 0.1
+    assertTrue(getFitness(0.01f) >= getFitness(0.1f));
+  }
+
+  public void testFitness0_01() throws Throwable{
+    //fitness for diff 0 must be greater than for diff 0.1
+    assertTrue(getFitness(0) > getFitness(0.1f));
+  }
+
+  public void testFitness0_DELTA() throws Throwable{
+    //fitness for diff 0 must be greater than for diff 1.0
+    double diff1 =getFitness(0);
+    double diff2 =getFitness(MatchAgainstTruthTable.DELTA);
+    assertTrue( diff1 >= diff2);
+  }
+
+  public void testFitness01_05() throws Throwable{
+    //fitness for diff 0.1 must be greater/equal than for diff 0.5
+    assertTrue(getFitness(0.1f) >= getFitness(0.5f));
+  }
+
+  /**
+   * fitness for diff 2 must be greater than 90% of MAX
+   */
+  public void testFitness2() throws Throwable{
+    double diff1 = getFitness(2);
+    assertTrue(diff1 > MatchAgainstTruthTable.MAX_FITNESS*0.9);
+  }
+
+  /**
+   * fitness for diff 10 must be less than MAX
+   */
+  public void testFitness10() throws Throwable{
+    double diff1 = getFitness(10);
+    assertTrue(diff1 < MatchAgainstTruthTable.MAX_FITNESS);
+  }
+
+  public void testFitness10_100() throws Throwable{
+    //fitness for diff 10 must be greater than fitness for diff 100
+    double diff1 = getFitness(10);
+    double diff2 = getFitness(100);
+    assertTrue(diff1 > diff2);
+  }
+
+  public void testFitness20_21() throws Throwable{
+    //fitness for diff 20 must be greater/equal than fitness for diff 21
+    double diff1 = getFitness(20);
+    double diff2 = getFitness(21);
+    assertTrue(diff1 >= diff2);
+  }
+
+  public void testFitness2048() throws Throwable{
+    assertTrue(getFitness(20)>5);
+  }
+
+  public void testFitness20000_20001() throws Throwable{
+    //fitness for diff 20000 must be greater/equal than fitness for diff 20001
+    double diff1 = getFitness(20000);
+    double diff2 = getFitness(20001);
+    assertTrue(diff1 >= diff2);
+  }
+
+  public void testFitnessWORST() throws Throwable{
+    //fitness for worst input must be 0
+    double diff = getFitness(MatchAgainstTruthTable.MAX_FITNESS);
+    assertTrue(diff == 0);
+    diff = getFitness(MatchAgainstTruthTable.MAX_FITNESS + 100);
+    assertTrue(diff == 0);
+  }
+
+  public void testFitnessNegative() throws Throwable{
+    //fitness for negative input must be equal to positive input
+    double diff1 = getFitness(-200);
+    double diff2 = getFitness(200);
+    assertTrue(diff1 == diff2);
+  }
+
+  public void testStetigkeit() throws Throwable{
+    double oldFitness = -1;
+    double newFitness;
+    double diff1 = getFitness(81.92f);
+    for (float i=0.01f;i<20000;i=i*2) {
+//      System.err.print("i: "+i);
+      newFitness = getFitness(i);
+//      System.err.print(" --> "+newFitness);
+//      System.err.println(" EXP = "+Math.pow(1.05d, i));
+      if (oldFitness != -1) {
+        assertTrue(oldFitness >= newFitness);
+      }
+      oldFitness = newFitness;
+    }
+  }
+}
