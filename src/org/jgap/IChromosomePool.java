2298e8a5bfbd75382e16fb128c389e8fdc1e4b17
Klaus Meffert <kontakt@doktor-meffert.de>
2006-01-04 12:55:12 +0000

    added new interface for class ChromosomePool
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1609 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/IChromosomePool.java
@@ -0,0 +1,45 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap;
+
+/**
+ * Interface for chromosome pools (e.g., see class ChromosomePool).
+ *
+ * @author Klaus Meffert
+ * @since 2.6
+ */
+public interface IChromosomePool {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Attempts to acquire an Chromosome instance from the chromosome pool.
+   *
+   * @return a Chromosome instance from the pool or null if no Chromosome
+   * instances are available in the pool
+   *
+   * @author Neil Rostan
+   * @since 2.6 (since 1.0 in ChromosomePool)
+   */
+  Chromosome acquireChromosome();
+
+  /**
+   * Releases a Chromosome to the pool. It's not required that the Chromosome
+   * originated from the pool--any Chromosome can be released to it. This
+   * method should invoke the cleanup() method on each of the Chromosome's
+   * genes prior to adding it back to the pool.
+   *
+   * @param a_chromosome the Chromosome instance to be released into the pool
+   *
+   * @author Neil Rostan
+   * @since 2.6 (since 1.0 in ChromosomePool)
+   */
+  void releaseChromosome(Chromosome a_chromosome);
+}
