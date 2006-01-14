83cfa554f4f51e6c44ca6140261afed4cca47dda
Klaus Meffert <kontakt@doktor-meffert.de>
2006-01-14 12:18:14 +0000

    added ICompareToHandler
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1767 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/ICompareToHandler.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap;
+
+/**
+ * Interface for handlers capable of  comparing instances of specific classes.
+ * <p>A compareTo-handler answers via isHandlerFor(Class) whether he could
+ * compare instances of the given class.<p>
+ * The perform(Object, Object) method does the comparing acknowledged before
+ * with isHandlerFor(Class).
+ *
+ * @author Klaus Meffert
+ * @since 2.6
+ */
+public interface ICompareToHandler
+    extends IHandler {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+}
--- src/org/jgap/IJGAPFactory.java
+++ src/org/jgap/IJGAPFactory.java
@@ -19,17 +19,18 @@ import java.util.*;
  */
 public interface IJGAPFactory {
   /** String containing the CVS revision. Read out via reflection!*/
-  final static String CVS_REVISION = "$Revision: 1.4 $";
+  final static String CVS_REVISION = "$Revision: 1.5 $";
 
   void setParameters(Collection a_parameters);
 
   RandomGenerator createRandomGenerator();
 
   /**
-   * Retrieves a clone handler capable of clone the given class
+   * Retrieves a clone handler capable of clone the given class.
+   * @param a_obj the object to clone (maybe null)
    * @param a_classToClone the class to clone an object of
-   * @return ICloneHandler the clone handler found capable of clone the given
-   * class, or null if none registered
+   * @return the clone handler found capable of clone the given class, or null
+   * if none registered
    *
    * @author Klaus Meffert
    * @since 2.6
@@ -39,7 +40,7 @@ public interface IJGAPFactory {
 
   /**
    * Registers a clone handler that could be retrieved by
-   * getCloneHandlerFor(Class)
+   * getCloneHandlerFor(Class).
    * @param a_cloneHandler the ICloneHandler to register
    * @return index of the added clone handler, needed when removeCloneHandler
    * will be called
@@ -52,9 +53,10 @@ public interface IJGAPFactory {
 
   /**
    * Retrieves an initializer capable of initializing the Object of the given
-   * class
+   * class.
+   * @param a_obj the object to init (maybe null)
    * @param a_objToInit the object class to init
-   * @return  the initializer found capable of initializing an object of the
+   * @return the initializer found capable of initializing an object of the
    * given class, or null if none registered
    *
    * @author Klaus Meffert
@@ -63,7 +65,8 @@ public interface IJGAPFactory {
   IInitializer getInitializerFor(Object a_obj, Class a_objToInit);
 
   /**
-   * Registers an initializer that could be retrieved by getInitializerFor(Class)
+   * Registers an initializer that could be retrieved by
+   * getInitializerFor(Class).
    * @param a_chromIniter the IChromosomeInitializer to register
    * @return index of the added initializer, needed when
    * removeChromosomeInitializer will be called
@@ -71,10 +74,38 @@ public interface IJGAPFactory {
    * @author Klaus Meffert
    * @since 2.6
    */
+
   int registerInitializer(IInitializer a_chromIniter);
 
   void setGeneticOperatorConstraint(IGeneticOperatorConstraint
                                     a_constraint);
 
   IGeneticOperatorConstraint getGeneticOperatorConstraint();
+
+  /**
+   * Retrieves a handler capable of comparing two instances of the given class.
+   * @param a_obj the object to compare (maybe null)
+   * @param a_classToCompareTo the class instances to compare (maybe null)
+   * @return the handler found capable of comparing instances
+   * of the given class, or null if none registered
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
+
+  ICompareToHandler getCompareToHandlerFor(Object a_obj,
+                                           Class a_classToCompareTo);
+
+  /**
+   * Registers a compareTo-handler that could be retrieved by
+   * getCompareToHandlerFor(Class).
+   * @param a_compareToHandler the ICompareToHandler to register
+   * @return index of the added handler, needed when removeCompareToHandler
+   * will be called
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
+
+  int registerCompareToHandler(ICompareToHandler a_compareToHandler);
 }
