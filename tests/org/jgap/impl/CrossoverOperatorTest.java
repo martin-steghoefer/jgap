99c231aa1f2ca35121df14e26c1ae5f3c0643d3f
Klaus Meffert <kontakt@doktor-meffert.de>
2006-01-14 10:56:44 +0000

    added test
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1757 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/CrossoverOperatorTest.java
+++ tests/org/jgap/impl/CrossoverOperatorTest.java
@@ -23,7 +23,7 @@ import junit.framework.*;
 public class CrossoverOperatorTest
     extends JGAPTestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.22 $";
+  private static final String CVS_REVISION = "$Revision: 1.23 $";
 
   public static Test suite() {
     TestSuite suite = new TestSuite(CrossoverOperatorTest.class);
@@ -77,6 +77,8 @@ public class CrossoverOperatorTest
       throws Exception {
     DefaultConfiguration conf = new DefaultConfiguration();
     Genotype.setConfiguration(conf);
+    CrossoverOperator op = new CrossoverOperator();
+    conf.addGeneticOperator(op);
     RandomGeneratorForTest rand = new RandomGeneratorForTest();
     rand.setNextIntSequence(new int[] {
                             0, 1, 0, 1, 2});
@@ -86,7 +88,6 @@ public class CrossoverOperatorTest
     Chromosome chrom = new Chromosome(sampleGene, 3);
     conf.setSampleChromosome(chrom);
     conf.setPopulationSize(6);
-    CrossoverOperator op = new CrossoverOperator();
     Gene cgene1 = new IntegerGene(1, 10);
     cgene1.setAllele(new Integer(6));
     Gene[] genes1 = new Gene[] {
@@ -118,7 +119,62 @@ public class CrossoverOperatorTest
   }
 
   /**
-   * Tests if crossing over produces same results for two operate-runs
+   * Consider crossover rate calculator.
+   * @throws Exception
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
+  public void testOperate_0_2()
+      throws Exception {
+    DefaultConfiguration conf = new DefaultConfiguration();
+    Genotype.setConfiguration(conf);
+    CrossoverOperator op = new CrossoverOperator(
+        new DefaultCrossoverRateCalculator());
+    conf.addGeneticOperator(op);
+    RandomGeneratorForTest rand = new RandomGeneratorForTest();
+    rand.setNextIntSequence(new int[] {
+                            0, 1, 0, 1, 2});
+    conf.setRandomGenerator(rand);
+    conf.setFitnessFunction(new TestFitnessFunction());
+    Gene sampleGene = new IntegerGene(1, 10);
+    Chromosome chrom = new Chromosome(sampleGene, 2);
+    conf.setSampleChromosome(chrom);
+    conf.setPopulationSize(6);
+    Gene cgene1 = new IntegerGene(1, 10);
+    cgene1.setAllele(new Integer(6));
+    Gene[] genes1 = new Gene[] {
+        cgene1};
+    Chromosome chrom1 = new Chromosome(genes1);
+    Gene cgene2 = new IntegerGene(1, 10);
+    cgene2.setAllele(new Integer(8));
+    Gene[] genes2 = new Gene[] {
+        cgene2};
+    Chromosome chrom2 = new Chromosome(genes2);
+    Chromosome[] population = new Chromosome[] {
+        chrom1, chrom2};
+    List chroms = new Vector();
+    Gene gene1 = new IntegerGene(1, 10);
+    gene1.setAllele(new Integer(5));
+    chroms.add(gene1);
+    Gene gene2 = new IntegerGene(1, 10);
+    gene2.setAllele(new Integer(7));
+    chroms.add(gene2);
+    Gene gene3 = new IntegerGene(1, 10);
+    gene3.setAllele(new Integer(4));
+    chroms.add(gene3);
+    op.operate(new Population(population), chroms);
+    assertEquals(3 + 2, chroms.size());
+    Chromosome target = (Chromosome) chroms.get(4);
+    assertEquals(6, ( (Integer) target.getGene(0).getAllele()).intValue());
+    target = (Chromosome) chroms.get(3);
+    assertEquals(8, ( (Integer) target.getGene(0).getAllele()).intValue());
+    op.operate(new Population(population), chroms);
+    assertEquals(3 + 2 + 2, chroms.size());
+  }
+
+  /**
+   * Tests if crossing over produces same results for two operate-runs.
    * @throws Exception
    *
    * @author Klaus Meffert
@@ -168,7 +224,7 @@ public class CrossoverOperatorTest
   }
 
   /**
-   * Test with CompositeGene
+   * Test with CompositeGene.
    * @throws Exception
    *
    * @author Klaus Meffert
@@ -292,7 +348,7 @@ public class CrossoverOperatorTest
   }
 
   /**
-   * Ensures the operator is implementing Serializable
+   * Ensures the operator is implementing Serializable.
    * @throws Exception
    *
    * @author Klaus Meffert
@@ -305,7 +361,7 @@ public class CrossoverOperatorTest
   }
 
   /**
-   * Ensures that the operator and all objects contained implement Serializable
+   * Ensures that the operator and all objects contained implement Serializable.
    * @throws Exception
    *
    * @author Klaus Meffert
