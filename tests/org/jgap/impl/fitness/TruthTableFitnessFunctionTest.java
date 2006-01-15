effb7fc6d20fedc265a1a383bd72d6d75c7ad5ea
Klaus Meffert <kontakt@doktor-meffert.de>
2006-01-15 13:59:22 +0000

    added tests for TruthTableFitnessFunction
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1791 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/impl/fitness/TruthTableFitnessFunctionTest.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.impl.fitness;
+
+import java.util.*;
+import junit.framework.*;
+import org.jgap.*;
+
+/**
+ * Tests for the TruthTableFitnessFunction class.
+ *
+ * @author Klaus Meffert
+ * @since 1.1
+ */
+public class TruthTableFitnessFunctionTest
+    extends JGAPTestCase {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite(TruthTableFitnessFunctionTest.class);
+    return suite;
+  }
+
+  /**
+   * @throws Exception
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
+  public void testCalcFitness_0()
+      throws Exception {
+    Map inout = new HashMap();
+    inout.put(new Double(2), new Double(3));
+    inout.put(new Double(9), new Double(11));
+    TruthTableFitnessFunctionImpl fitfunc = new TruthTableFitnessFunctionImpl(7);
+    Map truthTable = new HashMap();
+    truthTable.put(new Double(2), new Double(4));
+    truthTable.put(new Double(8), new Double(1));
+    truthTable.put(new Double(9), new Double(11));
+    fitfunc.setTruthTable(truthTable);
+    assertEquals(2.0d, fitfunc.calcFitness(inout), DELTA);
+  }
+
+  /**
+   * Implementing class of abstract TruthTableFitnessFunction class.
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
+  private class TruthTableFitnessFunctionImpl
+      extends TruthTableFitnessFunction {
+    /**
+     * @since 2.0 (until 1.1: type int)
+     */
+    private double m_evaluationValue;
+
+    public TruthTableFitnessFunctionImpl(double a_evaluationValue) {
+      m_evaluationValue = a_evaluationValue;
+    }
+
+    protected double evaluate(IChromosome a_subject) {
+      return m_evaluationValue;
+    }
+  }
+}
