16d76f45771d57a1d318e3a4e6b2ab903f740a2b
Klaus Meffert <kontakt@doktor-meffert.de>
2006-01-20 07:53:22 +0000

    added interface IChromosome for class Chromosome
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1818 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src-3rdparty/examples/MinimizingMakeChangeWithChart.java
+++ src-3rdparty/examples/MinimizingMakeChangeWithChart.java
@@ -34,7 +34,7 @@ import org.w3c.dom.*;
  */
 public class MinimizingMakeChangeWithChart {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   /**
    * The total number of times we'll let the population evolve.
@@ -94,7 +94,7 @@ public class MinimizingMakeChangeWithChart {
     sampleGenes[1] = new IntegerGene(0, 2 * 10); // Dimes
     sampleGenes[2] = new IntegerGene(0, 1 * 10); // Nickels
     sampleGenes[3] = new IntegerGene(0, 4 * 10); // Pennies
-    Chromosome sampleChromosome = new Chromosome(sampleGenes);
+    IChromosome sampleChromosome = new Chromosome(sampleGenes);
     conf.setSampleChromosome(sampleChromosome);
     // Finally, we need to tell the Configuration object how many
     // Chromosomes we want in our population. The more Chromosomes,
@@ -134,7 +134,7 @@ public class MinimizingMakeChangeWithChart {
     XMLManager.writeFile(xmlDoc, new File("testJGAP.xml"));
     // Display the best solution we found.
     // -----------------------------------
-    Chromosome bestSolutionSoFar = population.getFittestChromosome();
+    IChromosome bestSolutionSoFar = population.getFittestChromosome();
     System.out.println("The best solution has a fitness value of " +
                        bestSolutionSoFar.getFitnessValue());
     System.out.println("It contained the following: ");
