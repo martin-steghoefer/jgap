d8a3e2b19e1e9e0401d68fa8c1ad8f31017a339e
Klaus Meffert <kontakt@doktor-meffert.de>
2006-02-08 07:10:57 +0000

    added javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1887 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/IHandler.java
+++ src/org/jgap/IHandler.java
@@ -19,10 +19,39 @@ package org.jgap;
  */
 public interface IHandler {
   /** String containing the CVS revision. Read out via reflection!*/
-  final static String CVS_REVISION = "$Revision: 1.1 $";
+  final static String CVS_REVISION = "$Revision: 1.2 $";
 
+  /**
+   * Determines whether the handler is suitable for the given object instance
+   * or class.
+   *
+   * @param a_obj the object instance to check
+   * @param a_class the class to check, alternatively if no object instance is
+   * available (some handlers could need to get an object and would always
+   * return false if only a class is provided)
+   * @return true: handler suitable for given object or class
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
   boolean isHandlerFor(Object a_obj, Class a_class);
 
+  /**
+   * Performs a task for the given object or class. For some handlers,
+   * additional parameters are necessary, which could be provided.
+   *
+   * @param a_obj the object instance to perform the handler task for
+   * @param a_class the class to perform the handler task for, in case no object
+   * instance is available (some handlers could need to get an object and would
+   * always throw an exception if only a class is provided)
+   * @param a_params optional parameters needed for the handler to perform its
+   * task
+   * @throws Exception in case of any error
+   * @return Object
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
   Object perform(Object a_obj, Class a_class, Object a_params)
       throws Exception;
 }
