b5a61d02dc5e2da693dc1f6e2a22e441eb3e3c15
Klaus Meffert <kontakt@doktor-meffert.de>
2006-04-20 14:15:58 +0000

    added abse class for GeneticOperator's
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1981 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/BaseGeneticOperator.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap;
+
+import java.util.*;
+
+/**
+ * Base class for any implementation of interface GeneticOperator.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public abstract class BaseGeneticOperator
+    implements GeneticOperator, Comparable {
+  private Configuration m_configuration;
+
+  /**
+   * The only constructor in this class. Sets the immutable configuration.
+   * @param a_configuration the configuration to set
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public BaseGeneticOperator(Configuration a_configuration) {
+    m_configuration = a_configuration;
+  }
+
+  /**
+   * @todo make returned object immutable
+   * @return the configuration set
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public Configuration getConfiguration() {
+    return m_configuration;
+  }
+
+  /**
+   * Compares this GeneticOperator against the specified object. The result is
+   * true if and the argument is an instance of this class and is equal wrt the
+   * data.
+   *
+   * @param a_other the object to compare against
+   * @return true: if the objects are the same, false otherwise
+   *
+   * @author Klaus Meffert
+   * @since 2.6
+   */
+  public boolean equals(final Object a_other) {
+    try {
+      return compareTo(a_other) == 0;
+    }
+    catch (ClassCastException cex) {
+      return false;
+    }
+  }
+
+}
