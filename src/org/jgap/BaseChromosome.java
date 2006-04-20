66910fd6fd023f6019afa84aff6f94e71ef581ed
Klaus Meffert <kontakt@doktor-meffert.de>
2006-04-20 18:53:08 +0000

    added base class for Chromosome's
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@1983 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/BaseChromosome.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap;
+
+/**
+ * Base class for any implementation of interface IChromosome.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public abstract class BaseChromosome
+    implements IChromosome, IInitializer {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private transient Configuration m_configuration;
+
+  /**
+   * The only constructor in this class. Sets the immutable configuration.
+   * @param a_configuration the configuration to set
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public BaseChromosome(Configuration a_configuration) {
+    m_configuration = a_configuration;
+  }
+
+  /**
+   * @todo make returned object immutable
+   * @return the configuration set
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public Configuration getConfiguration() {
+    return m_configuration;
+  }
+
+  public abstract Object clone();
+}
