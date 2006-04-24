953a6e656de49bf84ee4e008c8e538378e1490ce
Klaus Meffert <kontakt@doktor-meffert.de>
2006-04-24 11:37:14 +0000

    converted to unit test; adapted to nonstatic configuration object
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2008 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/supergenes/SupergenePersistentRepresentationTest.java
+++ tests/org/jgap/supergenes/SupergenePersistentRepresentationTest.java
@@ -20,72 +20,77 @@ import junit.framework.*;
  * @since 2.0
  */
 public class SupergenePersistentRepresentationTest
-    extends TestCase {
+    extends JGAPTestCase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.6 $";
+  private final static String CVS_REVISION = "$Revision: 1.7 $";
 
   public static class InstantiableSupergene
       extends AbstractSupergene {
+    public InstantiableSupergene(final Configuration a_config)
+        throws InvalidConfigurationException {
+      super(a_config, null);
+    }
+
+    public InstantiableSupergene()
+        throws InvalidConfigurationException {
+      super();
+    }
+
     public boolean isValid(Gene[] a_gene) {
       return true;
     };
   }
-  public static boolean testRepresentation() {
-    try {
-      InstantiableSupergene gene = new InstantiableSupergene();
-      Gene i1 = new IntegerGene(1, 12);
-      Gene i2 = new DoubleGene(3, 4);
-      i1.setAllele(new Integer(7));
-      i2.setAllele(new Double(3.2));
-      gene.addGene(i1);
-      gene.addGene(i2);
-      InstantiableSupergene nested = new InstantiableSupergene();
-      Gene n1 = new IntegerGene(1, 12);
-      Gene n2 = new DoubleGene(3, 4);
-      n1.setAllele(new Integer(5));
-      n2.setAllele(new Double(3.6));
-      nested.addGene(n1);
-      nested.addGene(n2);
-      gene.addGene(nested);
-      InstantiableSupergene nested2 = new InstantiableSupergene();
-      nested2.setValidator(new Validator() {
-        public boolean isValid(Gene[] a_gene, Supergene a_supergene) {
-          return true;
-        }
-      });
-      Gene nn1 = new IntegerGene(1, 1000);
-      Gene nn2 = new DoubleGene(0, 1000);
-      nn1.setAllele(new Integer(22));
-      nn2.setAllele(new Double(44));
-      nested2.addGene(nn1);
-      nested2.addGene(nn2);
-      gene.addGene(nested2);
-      InstantiableSupergene nested3 = new InstantiableSupergene();
-      nested3.setValidator(null);
-      Gene nnn1 = new IntegerGene(1, 1000);
-      Gene nnn2 = new DoubleGene(0, 1000);
-      nnn1.setAllele(new Integer(555));
-      nnn2.setAllele(new Double(777));
-      nested3.addGene(nnn1);
-      nested3.addGene(nnn2);
-      nested2.addGene(nested3);
-      String representation =
-          gene.getPersistentRepresentation();
+  public void testRepresentation()
+      throws Exception {
+    InstantiableSupergene gene = new InstantiableSupergene(conf);
+    Gene i1 = new IntegerGene(conf, 1, 12);
+    Gene i2 = new DoubleGene(conf, 3, 4);
+    i1.setAllele(new Integer(7));
+    i2.setAllele(new Double(3.2));
+    gene.addGene(i1);
+    gene.addGene(i2);
+    InstantiableSupergene nested = new InstantiableSupergene(conf);
+    Gene n1 = new IntegerGene(conf, 1, 12);
+    Gene n2 = new DoubleGene(conf, 3, 4);
+    n1.setAllele(new Integer(5));
+    n2.setAllele(new Double(3.6));
+    nested.addGene(n1);
+    nested.addGene(n2);
+    gene.addGene(nested);
+    InstantiableSupergene nested2 = new InstantiableSupergene(conf);
+    nested2.setValidator(new Validator() {
+      public boolean isValid(Gene[] a_gene, Supergene a_supergene) {
+        return true;
+      }
+    });
+    Gene nn1 = new IntegerGene(conf, 1, 1000);
+    Gene nn2 = new DoubleGene(conf, 0, 1000);
+    nn1.setAllele(new Integer(22));
+    nn2.setAllele(new Double(44));
+    nested2.addGene(nn1);
+    nested2.addGene(nn2);
+    gene.addGene(nested2);
+    InstantiableSupergene nested3 = new InstantiableSupergene(conf);
+    nested3.setValidator(null);
+    Gene nnn1 = new IntegerGene(conf, 1, 1000);
+    Gene nnn2 = new DoubleGene(conf, 0, 1000);
+    nnn1.setAllele(new Integer(555));
+    nnn2.setAllele(new Double(777));
+    nested3.addGene(nnn1);
+    nested3.addGene(nnn2);
+    nested2.addGene(nested3);
+    String representation =
+        gene.getPersistentRepresentation();
 //            System.out.println ("Old representation: " + representation);
-      InstantiableSupergene restored = new InstantiableSupergene();
-      restored.setValueFromPersistentRepresentation(representation);
+    InstantiableSupergene restored = new InstantiableSupergene(conf);
+    restored.setValueFromPersistentRepresentation(representation);
 //            System.out.println ("New representation: " +
 //                                restored.getPersistentRepresentation ());
 
 //            System.out.println("Old gene "+gene);
 //            System.out.println("New gene "+restored);
-      return gene.equals(restored);
-    }
-    catch (Exception ex) {
-      ex.printStackTrace();
-      return false;
-    }
-  };
+    assertTrue(gene.equals(restored));
+  }
 
   public static Test suite() {
     TestSuite suite =
@@ -93,19 +98,8 @@ public class SupergenePersistentRepresentationTest
     return suite;
   }
 
-  protected void setUp()
-      throws Exception {
-    super.setUp();
-  }
-
   protected void tearDown()
       throws Exception {
     super.tearDown();
-  }
-
-  public void testRunTest() {
-    boolean expectedReturn = true;
-    boolean actualReturn = testRepresentation();
-    assertEquals("return value", expectedReturn, actualReturn);
   }
 }
