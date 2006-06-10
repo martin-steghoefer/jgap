eeaa36d2e3648c8b497fbf7516cdc3df69a2d094
Klaus Meffert <kontakt@doktor-meffert.de>
2006-06-10 12:10:43 +0000

    simplified code
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2146 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/impl/salesman/AllSalesmanTests.java
+++ tests/org/jgap/impl/salesman/AllSalesmanTests.java
@@ -21,11 +21,11 @@ public class AllSalesmanTests
     extends TestSuite {
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.3 $";
+  private final static String CVS_REVISION = "$Revision: 1.4 $";
 
   public static Test suite() {
     TestSuite suite = new TestSuite();
-    suite.addTestSuite(org.jgap.impl.salesman.TravellingSalesmanTest.class);
+    suite.addTestSuite(TravellingSalesmanTest.class);
     return suite;
   }
 }
