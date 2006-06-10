955b41d9c72a159a6ef02d7e0e99918685f2e1f0
Klaus Meffert <kontakt@doktor-meffert.de>
2006-06-10 10:13:09 +0000

    adhered to coding standards
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2133 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/data/config/MetaConfig.java
+++ src/org/jgap/data/config/MetaConfig.java
@@ -17,7 +17,8 @@ import java.io.*;
  * a JGAP Configuration GUI.
  * @author Siddhartha Azad
  * @since 2.4
- * */
+ *
+ */
 public class MetaConfig {
   // file to read the GUI Configuration information from
   private static final String METACON_FILENAME = "jgap-meta.con";
@@ -25,7 +26,7 @@ public class MetaConfig {
   private static final String CN = "MetaConfig";
 
   // singleton instance
-  private static MetaConfig instance = null;
+  private static MetaConfig instance;
 
   // ClassName-ConfigProperty mapping
   private Hashtable m_metaMap = new Hashtable();
@@ -87,19 +88,34 @@ public class MetaConfig {
    * @author Siddhartha Azad
    * @since 2.4
    */
-  private void init()
+  protected void init()
       throws MetaConfigException, IOException {
-    File metaFile = new File(METACON_FILENAME);
-    FileReader fr = new FileReader(metaFile);
+    Reader fr = getReader(METACON_FILENAME);
     LineNumberReader lr = new LineNumberReader(fr);
     String line = lr.readLine();
     while (line != null) {
-      if (!this.isComment(line))
+      if (!this.isComment(line)) {
         parseLine(line);
+      }
       line = lr.readLine();
     }
     endState();
     lr.close();
+  }
+
+  /**
+   * Returns a reader to a file
+   * @param a_filename the file to retrieve a reader for
+   * @throws IOException
+   * @return the Reader
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public Reader getReader(String a_filename) throws IOException {
+    File metaFile = new File(a_filename);
+    FileReader fr = new FileReader(metaFile);
+    return fr;
   }
 
   /**
@@ -113,8 +129,9 @@ public class MetaConfig {
   private boolean isComment(String line) {
     String tmpLine = line.trim();
     StringBuffer sb = new StringBuffer(tmpLine);
-    if (sb.charAt(0) == '#')
+    if (sb.charAt(0) == '#') {
       return true;
+    }
     return false;
   }
 
@@ -133,23 +150,30 @@ public class MetaConfig {
       throw new MetaConfigException(CN + ".parseLine():Exception while " +
                                     "parsing " + METACON_FILENAME + " line " +
                                     a_line + " is invalid");
-    if (m_state == MetaConfig.INIT && tokens[0].equals("class"))
+    if (m_state == MetaConfig.INIT && tokens[0].equals("class")) {
       handleClass(tokens[1]);
-    else if (m_state == MetaConfig.CLASS && tokens[0].equals("property"))
+    }
+    else if (m_state == MetaConfig.CLASS && tokens[0].equals("property")) {
       handleProperty(tokens[1]);
-    else if (m_state == MetaConfig.PROPERTY && tokens[0].equals("values"))
+    }
+    else if (m_state == MetaConfig.PROPERTY && tokens[0].equals("values")) {
       handleValues(tokens[1]);
-    else if (m_state == MetaConfig.PROPERTY && tokens[0].equals("class"))
+    }
+    else if (m_state == MetaConfig.PROPERTY && tokens[0].equals("class")) {
       handleClass(tokens[1]);
-    else if (m_state == MetaConfig.VALUES && tokens[0].equals("class"))
+    }
+    else if (m_state == MetaConfig.VALUES && tokens[0].equals("class")) {
       handleClass(tokens[1]);
-    else if (m_state == MetaConfig.VALUES && tokens[0].equals("property"))
+    }
+    else if (m_state == MetaConfig.VALUES && tokens[0].equals("property")) {
       handleProperty(tokens[1]);
-    else
+    }
+    else {
       throw new MetaConfigException(CN + ".parseLine():Exception while "
                                     + "parsing " + METACON_FILENAME + " state "
                                     + m_state
                                     + " incompatible with line " + a_line);
+    }
   }
 
   /**
@@ -159,8 +183,9 @@ public class MetaConfig {
    * */
   private void handleClass(final String a_token) {
     m_state = MetaConfig.CLASS;
-    if (m_currProperty != null)
+    if (m_currProperty != null) {
       add(m_currName, m_currProperty);
+    }
     m_currProperty = new ConfigProperty();
     m_currName = a_token;
   }
@@ -176,15 +201,17 @@ public class MetaConfig {
       throws MetaConfigException {
     int prevState = m_state;
     if (prevState == MetaConfig.VALUES) {
-      if (m_currProperty != null)
+      if (m_currProperty != null) {
         add(m_currName, m_currProperty);
+      }
     }
     m_currProperty = new ConfigProperty();
     m_state = MetaConfig.PROPERTY;
     String[] tokens = a_token.split(",");
-    if (tokens.length < 2 || tokens.length > 3)
+    if (tokens.length < 2 || tokens.length > 3) {
       throw new MetaConfigException("Invalid format of property line: " +
                                     a_token);
+    }
     m_currProperty.setName(tokens[0].trim());
     m_currProperty.setWidget(tokens[1].trim());
     if (tokens.length == 3) {
@@ -205,9 +232,10 @@ public class MetaConfig {
       throws MetaConfigException {
     m_state = MetaConfig.VALUES;
     String[] tokens = a_token.split(",");
-    if (tokens.length == 0)
+    if (tokens.length == 0) {
       throw new MetaConfigException("Invalid format of property line: " +
                                     a_token);
+    }
     for (int i = 0; i < tokens.length; i++) {
       m_currProperty.addValue(tokens[i].trim());
     }
@@ -223,11 +251,12 @@ public class MetaConfig {
    * */
   private void endState()
       throws MetaConfigException {
-    if (m_state != MetaConfig.PROPERTY && m_state != MetaConfig.VALUES)
+    if (m_state != MetaConfig.PROPERTY && m_state != MetaConfig.VALUES) {
       throw new MetaConfigException("Invalid format of JGAP MetaConfig "
                                     + "file: " + METACON_FILENAME
                                     + "Ending in Invalid state : "
                                     + m_state);
+    }
     if (m_currProperty != null) {
       add(m_currName, m_currProperty);
     }
@@ -237,7 +266,7 @@ public class MetaConfig {
    * Add a new ConfigProperty for a certain class to the hashtable of
    * properties.
    * @param currName name of the class to which the property belongs
-   * @param a_cp ConfigProperty to be added to the class.
+   * @param a_cp the ConfigProperty to be added to the class
    *
    * @author Siddhartha Azad
    * @since 2.4
