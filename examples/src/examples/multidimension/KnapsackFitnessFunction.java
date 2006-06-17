71b8dd069f3b5419d8993af927cd537ff056a93a
Klaus Meffert <kontakt@doktor-meffert.de>
2006-06-17 18:45:40 +0000

    added new example
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2207 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ examples/src/examples/multidimension/KnapsackFitnessFunction.java
@@ -0,0 +1,207 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.multidimension;
+
+import java.util.*;
+import org.jgap.*;
+import org.jgap.impl.*;
+
+/**
+ * Fitness function for the multidimension knapsack example.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class KnapsackFitnessFunction
+    extends FitnessFunction {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private final double m_knapsackVolume;
+
+  public static final double MAX_BOUND = 1000000000.0d;
+
+  private static final double ZERO_DIFFERENCE_FITNESS = Math.sqrt(MAX_BOUND);
+
+  public KnapsackFitnessFunction(double a_knapsackVolume) {
+    if (a_knapsackVolume < 1 || a_knapsackVolume >= MAX_BOUND) {
+      throw new IllegalArgumentException(
+          "Knapsack volumen must be between 1 and " + MAX_BOUND + ".");
+    }
+    m_knapsackVolume = a_knapsackVolume;
+  }
+
+  /**
+   * Determine the fitness of the given Chromosome instance. The higher the
+   * return value, the more fit the instance. This method should always
+   * return the same fitness value for two equivalent Chromosome instances.
+   *
+   * @param a_subject the Chromosome instance to evaluate
+   * @return a positive double reflecting the fitness rating of the given
+   * Chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public double evaluate(IChromosome a_subject) {
+    // The fitness value measures both how close the value is to the
+    // target amount supplied by the user and the total number of items
+    // represented by the solution. We do this in two steps: first,
+    // we consider only the represented amount of change vs. the target
+    // amount of change and return higher fitness values for amounts
+    // closer to the target, and lower fitness values for amounts further
+    // away from the target. Then we go to step 2, which returns a higher
+    // fitness value for solutions representing fewer total items, and
+    // lower fitness values for solutions representing more total items.
+    // ------------------------------------------------------------------
+    double totalVolume = getTotalVolume(a_subject);
+    int numberOfItems = getTotalNumberOfItems(a_subject);
+    double volumeDifference = Math.abs(m_knapsackVolume - totalVolume);
+    double fitness = 0.0d;
+    // Step 1: Determine distance of amount represented by solution from
+    // the target amount. If the change difference is greater than zero we
+    // will divide one by the difference in change between the
+    // solution amount and the target amount. That will give the desired
+    // effect of returning higher values for amounts closer to the target
+    // amount and lower values for amounts further away from the target
+    // amount.
+    // In the case where the change difference is zero it means that we have
+    // the correct amount and we assign a higher fitness value
+    // -----------------------------------------------------------------
+    fitness += volumeDifferenceBonus(MAX_BOUND, volumeDifference);
+    // Step 2: We divide the fitness value by a penalty based on the number of
+    // items. The higher the number of items the higher the penalty and the
+    // smaller the fitness value.
+    // And inversely the smaller number of items in the solution the higher
+    // the resulting fitness value.
+    // -----------------------------------------------------------------------
+    fitness -= computeItemNumberPenalty(a_subject, MAX_BOUND, numberOfItems);
+    // Make sure fitness value is always positive.
+    // -------------------------------------------
+    return Math.max(1.0d, fitness);
+  }
+
+  /**
+   * Bonus calculation of fitness value.
+   * @param a_maxFitness maximum fitness value appliable
+   * @param a_volumeDifference volume difference in ccm for the items problem
+   * @return bonus for given volume difference
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  protected double volumeDifferenceBonus(double a_maxFitness,
+                                         double a_volumeDifference) {
+    if (a_volumeDifference == 0) {
+      return a_maxFitness;
+    }
+    else {
+      // we arbitrarily work with half of the maximum fitness as basis for non-
+      // optimal solutions (concerning volume difference)
+      return a_maxFitness / 2 - (a_volumeDifference * a_volumeDifference);
+    }
+  }
+
+  /**
+   * Calculates the penalty to apply to the fitness value based on the amount
+   * of items in the solution.
+   *
+   * @param a_potentialSolution the potential solution to evaluate
+   * @param a_maxFitness upper boundary for fitness value possible
+   * @param a_items number of items in the solution
+   * @return a penalty for the fitness value based on the number of items
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  protected double computeItemNumberPenalty(IChromosome a_potentialSolution,
+                                            double a_maxFitness, int a_items) {
+    if (a_items == 0) {
+      // We know the solution cannot have less than zero items.
+      // ------------------------------------------------------
+      return 0;
+    }
+    else {
+      // The more items the more penalty, but not more than the maximum fitness
+      // value possible. Let's avoid linear behavior and use
+      // exponential penalty calculation instead.
+      // ----------------------------------------------------------------------
+      double penalty = (Math.min(a_maxFitness, a_items * a_items));
+      // The more different colors the more penalty.
+      // -------------------------------------------
+      HashSet colors = new HashSet();
+      for (int i = 0; i < a_potentialSolution.size(); i++) {
+        CompositeGene comp = (CompositeGene)a_potentialSolution.getGene(i);
+        IntegerGene color = (IntegerGene)comp.geneAt(0);
+        colors.add(color.getAllele());
+      }
+      int numColors = colors.size();
+      penalty += Math.pow(numColors, 10);
+      return Math.min(a_maxFitness, penalty);
+    }
+  }
+
+  /**
+   * Calculates the total amount of change (in cents) represented by
+   * the given potential solution and returns that amount.
+   *
+   * @param a_potentialSolution the potential solution to evaluate
+   * @return the total amount of change (in cents) represented by the
+   * given solution
+   *
+   * @author Klaus Meffert
+   * @since 2.3
+   */
+  public static double getTotalVolume(IChromosome a_potentialSolution) {
+    double volume = 0.0d;
+    for (int i = 0; i < a_potentialSolution.size(); i++) {
+      CompositeGene comp = (CompositeGene)a_potentialSolution.getGene(i);
+      volume += getNumberOfItemsAtGene(comp) * KnapsackMain.itemVolumes[i];
+    }
+    return volume;
+  }
+
+  /**
+   * Retrieves the number of items represented by the given potential
+   * solution at the given gene position.
+   *
+   * @param a_compositeGene the composite gene to evaluate
+   * @return the number of items represented by the potential solution
+   * at the given gene position
+   *
+   * @author Klaus Meffert
+   * @since 2.3
+   */
+  public static int getNumberOfItemsAtGene(CompositeGene a_compositeGene) {
+    Integer numItems =
+        (Integer) a_compositeGene.geneAt(1).getAllele();
+    return numItems.intValue();
+  }
+
+  /**
+   * Returns the total number of items represented by all of the genes in
+   * the given potential solution.
+   *
+   * @param a_potentialSolution the potential solution to evaluate
+   * @return the total number of items represented by the given Chromosome
+   *
+   * @author Klaus Meffert
+   * @since 2.3
+   */
+  public static int getTotalNumberOfItems(IChromosome a_potentialSolution) {
+    int totalItems = 0;
+    int numberOfGenes = a_potentialSolution.size();
+    for (int i = 0; i < numberOfGenes; i++) {
+      CompositeGene comp = (CompositeGene)a_potentialSolution.getGene(i);
+      totalItems += getNumberOfItemsAtGene(comp);
+    }
+    return totalItems;
+  }
+}
---
+++ examples/src/examples/multidimension/KnapsackMain.java
@@ -0,0 +1,211 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.multidimension;
+
+import java.io.*;
+import org.jgap.*;
+import org.jgap.data.*;
+import org.jgap.impl.*;
+import org.jgap.xml.*;
+import org.w3c.dom.*;
+
+/**
+ * This class provides an implementation of the extended classic knapsack
+ * problem using a genetic algorithm. The goal of the problem is to reach a given
+ * volume (of a knapsack) by putting a number of items into the knapsack.
+ * The closer the sum of the item volumes to the given volume the better.
+ * <p>
+ * The extension to the classic knapsack is that each item can have a specific
+ * color. The fewer colors the items in the packed knapsack have, the better
+ * the solution is regarded.<p>
+ * For further descriptions, compare the "coins" example also provided.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class KnapsackMain {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * The total number of times we'll let the population evolve.
+   */
+  private static final int MAX_ALLOWED_EVOLUTIONS = 170;
+
+  /** Volumes of arbitrary items in ccm*/
+  public final static double[] itemVolumes = {
+      50.2d, 14.8d, 27.5d, 6800.0d, 25.0d, 4.75d, 95.36d, 1500.7d, 18365.9d,
+      83571.1d};
+
+  /** Names of arbitrary items, only for outputting something imaginable*/
+  public final static String[] itemNames = {
+      "Torch", "Banana", "Miniradio", "TV", "Gameboy", "Small thingie",
+      "Medium thingie", "Big thingie", "Huge thingie", "Gigantic thingie"};
+
+  public final static String[] COLORS = {
+      "red", "green", "blue", "yellow", "brown", "orange",
+      "mint", "purple", "black", "white"};
+
+
+  /**
+   * Executes the genetic algorithm to determine the minimum number of
+   * items necessary to make up the given target volume. The solution will then
+   * be written to the console.
+   *
+   * @param a_knapsackVolume the target volume for which this method is
+   * attempting to produce the optimal list of items
+   * @param a_numCols max. number of colors being available for items
+   *
+   * @throws Exception
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public static void findItemsForVolume(double a_knapsackVolume, int a_numCols)
+      throws Exception {
+    // Start with a DefaultConfiguration, which comes setup with the
+    // most common settings.
+    // -------------------------------------------------------------
+    Configuration conf = new DefaultConfiguration();
+    conf.setPreservFittestIndividual(true);
+    // Set the fitness function we want to use. We construct it with
+    // the target volume passed in to this method.
+    // ---------------------------------------------------------
+    FitnessFunction myFunc =
+        new KnapsackFitnessFunction(a_knapsackVolume);
+    conf.setFitnessFunction(myFunc);
+    // Now we need to tell the Configuration object how we want our
+    // Chromosomes to be setup. We do that by actually creating a
+    // sample Chromosome and then setting it on the Configuration
+    // object. As mentioned earlier, we want our Chromosomes to each
+    // have as many genes as there are different items available. We want the
+    // values (alleles) of those genes to be integers, which represent
+    // how many items of that type we have. We therefore use the
+    // IntegerGene class to represent each of the genes. That class
+    // also lets us specify a lower and upper bound, which we set
+    // to senseful values (i.e. maximum possible) for each item type.
+    // --------------------------------------------------------------
+    Gene[] sampleGenes = new Gene[itemVolumes.length];
+    for (int i = 0; i < itemVolumes.length; i++) {
+      CompositeGene compositeGene = new CompositeGene(conf);
+      IntegerGene color = new IntegerGene(conf, 0, a_numCols - 1);
+      IntegerGene item = new IntegerGene(conf, 0,
+                                       (int) Math.ceil(a_knapsackVolume /
+          itemVolumes[i]));
+      compositeGene.addGene(color);
+      compositeGene.addGene(item);
+      sampleGenes[i] = compositeGene;
+    }
+    IChromosome sampleChromosome = new Chromosome(conf, sampleGenes);
+    conf.setSampleChromosome(sampleChromosome);
+    // Finally, we need to tell the Configuration object how many
+    // Chromosomes we want in our population. The more Chromosomes,
+    // the larger number of potential solutions (which is good for
+    // finding the answer), but the longer it will take to evolve
+    // the population (which could be seen as bad).
+    // ------------------------------------------------------------
+    conf.setPopulationSize(80);
+    // Create random initial population of Chromosomes.
+    // ------------------------------------------------
+    Genotype population = Genotype.randomInitialGenotype(conf);
+    // Evolve the population. Since we don't know what the best answer
+    // is going to be, we just evolve the max number of times.
+    // ---------------------------------------------------------------
+    for (int i = 0; i < MAX_ALLOWED_EVOLUTIONS; i++) {
+      population.evolve();
+    }
+    // Save progress to file. A new run of this example will then be able to
+    // resume where it stopped before!
+    // ---------------------------------------------------------------------
+
+    // represent Genotype as tree with elements Chromomes and Genes
+    // ------------------------------------------------------------
+    DataTreeBuilder builder = DataTreeBuilder.getInstance();
+    IDataCreators doc2 = builder.representGenotypeAsDocument(population);
+    // create XML document from generated tree
+    // ---------------------------------------
+    XMLDocumentBuilder docbuilder = new XMLDocumentBuilder();
+    Document xmlDoc = (Document) docbuilder.buildDocument(doc2);
+    XMLManager.writeFile(xmlDoc, new File("knapsackJGAP.xml"));
+    // Display the best solution we found.
+    // -----------------------------------
+    IChromosome bestSolutionSoFar = population.getFittestChromosome();
+    System.out.println("The best solution has a fitness value of " +
+                       bestSolutionSoFar.getFitnessValue());
+    System.out.println("It contained the following: ");
+    int count;
+    double totalVolume = 0.0d;
+    for (int i = 0; i < bestSolutionSoFar.size(); i++) {
+      CompositeGene comp = (CompositeGene)bestSolutionSoFar.getGene(i);
+      IntegerGene color = (IntegerGene)comp.geneAt(0);
+      IntegerGene item = (IntegerGene)comp.geneAt(1);
+      count = ( (Integer) item.getAllele()).intValue();
+      if (count > 0) {
+        String colorName = COLORS[color.intValue()];
+        System.out.println("\t " + count + " x " + itemNames[i] + " color "+colorName);
+        totalVolume += itemVolumes[i] * count;
+      }
+    }
+    System.out.println("\nFor a total volume of " + totalVolume + " ccm");
+    System.out.println("Expected volume was " + a_knapsackVolume + " ccm");
+    System.out.println("Volume difference is " +
+                       Math.abs(totalVolume - a_knapsackVolume) + " ccm");
+  }
+
+  /**
+   * Main method. A single command-line argument is expected, which is the
+   * volume to create (in other words, 75 would be equal to 75 ccm).
+   *
+   * @param args first element in the array = volume of the knapsack
+   * to fill as a double value, second element = max. no. of colors
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      System.out.println("Syntax: " + KnapsackMain.class.getName() +
+                         " <volume> <number of colors>");
+    }
+    else {
+      try {
+        double volume = Double.parseDouble(args[0]);
+        if (volume < 1 ||
+            volume >= KnapsackFitnessFunction.MAX_BOUND) {
+          System.out.println("The <volume> argument must be between 1 and "
+                             +
+                             (KnapsackFitnessFunction.MAX_BOUND - 1)
+                             + " and can be a decimal.");
+        }
+        else {
+          int colors = Integer.parseInt(args[1]);
+          if (colors < 1 || colors > KnapsackMain.COLORS.length) {
+            System.out.println("The <number of colors> argument must be between"
+                               +" 1 and "+
+                                KnapsackMain.COLORS.length);
+          }
+          else {
+            try {
+              findItemsForVolume(volume, colors);
+            }
+            catch (Exception e) {
+              e.printStackTrace();
+            }
+          }
+        }
+      }
+      catch (NumberFormatException e) {
+        System.out.println(
+            "The <volume> argument must be a valid double value,"
+            +" <colors> must be a valid integer number.");
+      }
+    }
+  }
+}
