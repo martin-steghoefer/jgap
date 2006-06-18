fe0cf0d33cfb72d28ed8707842f0a3954bbb6cda
Klaus Meffert <kontakt@doktor-meffert.de>
2006-06-18 18:57:05 +0000

    added logic
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2227 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/Problem.java
+++ src/org/jgap/distr/Problem.java
@@ -19,7 +19,7 @@ import org.jgap.*;
  */
 public class Problem {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   private FitnessFunction m_fitFunc;
 
@@ -32,26 +32,76 @@ public class Problem {
   public Problem() {
   }
 
+  /**
+   * @param a_fitFunc the fitness function to use
+   * @param a_popSize the population size to use by/suggest to the worker
+   * @param a_initialChroms initial chromosomes to use (e.g. from previous
+   * evolutions), or empty
+   * @throws IllegalArgumentException
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
   public Problem(FitnessFunction a_fitFunc, int a_popSize,
-                 Chromosome[] a_initialChroms) {
+                 Chromosome[] a_initialChroms) throws IllegalArgumentException {
+    if (a_fitFunc == null) {
+      throw new IllegalArgumentException("Fitness function must not be null!");
+    }
+    if (a_popSize <= 0) {
+      throw new IllegalArgumentException("Population size must be greater zero.");
+    }
     m_fitFunc = a_fitFunc;
     m_populationSize = a_popSize;
     m_initialChroms = a_initialChroms;
   }
 
+  /**
+   * @param a_ID internal ID of the problem, should be unique
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
   public void setID(Object a_ID) {
     m_ID = a_ID;
   }
 
+  /**
+   * @return internal ID of the problem, should be unique
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
   public Object getID() {
     return m_ID;
   }
 
+  /**
+   * @return population size to use
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
   public int getPopulationSize() {
     return m_populationSize;
   }
 
+  /**
+   * @return FitnessFunction to use
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
   public FitnessFunction getFitnessFunction() {
     return m_fitFunc;
+  }
+
+  /**
+   * @return Chromosome[] initial chromosomes to use, may be empty
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public Chromosome[] getChromosomes() {
+    return m_initialChroms;
   }
 }
