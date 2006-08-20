74e518e973cb77cc12be21b4a0d8282d43c85a58
Klaus Meffert <kontakt@doktor-meffert.de>
2006-08-20 16:00:54 +0000

    enhaned structure of the code
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2277 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/src/examples/gp/Fibonacci.java
+++ examples/src/examples/gp/Fibonacci.java
@@ -29,13 +29,17 @@ import org.jgap.gp.*;
 public class Fibonacci
     extends GPGenotype {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   static Variable vx;
 
-  static Integer[] x = new Integer[20];
+  static Variable va;
 
-  static int[] y = new int[20];
+  private final static int NUMFIB = 10;
+
+  static Integer[] x = new Integer[NUMFIB];
+
+  static int[] y = new int[NUMFIB];
 
   public Fibonacci(Population a_pop)
       throws InvalidConfigurationException {
@@ -52,16 +56,22 @@ public class Fibonacci
     CommandGene[][] nodeSets = {
         {
         vx = Variable.create(a_conf, "X", CommandGene.IntegerClass),
+//        new FreeVariable(a_conf, CommandGene.IntegerClass,"a"),
+//        new FreeVariable(a_conf, CommandGene.IntegerClass,"b"),
+//        new Terminal(a_conf, 0,100),
+//        new Terminal(a_conf, 0,100),
         new AddCommand(a_conf, CommandGene.IntegerClass),
-        new SubtractCommand(a_conf, CommandGene.IntegerClass),
-        new MultiplyCommand(a_conf, CommandGene.IntegerClass),
-        new ExpCommand(a_conf, CommandGene.IntegerClass),
+        new IncrementCommand(a_conf, CommandGene.IntegerClass),
+//        new MultiplyCommand(a_conf, CommandGene.IntegerClass),
+        new PushCommand(a_conf, CommandGene.IntegerClass),
+        new ForCommand(a_conf, CommandGene.IntegerClass),
+        new PopCommand(a_conf, CommandGene.IntegerClass),
     }
     };
     Random random = new Random();
     // randomly initialize function data (X-Y table) for Fib(x)
-    for (int i = 0; i < 20; i++) {
-      int index = random.nextInt(30);
+    for (int i = 0; i < NUMFIB; i++) {
+      int index = random.nextInt(NUMFIB * 2);
       x[i] = new Integer(index);
       y[i] = Fib(index);
       System.out.println(i + ") " + x[i] + "   " + y[i]);
@@ -74,7 +84,23 @@ public class Fibonacci
     if (a_index == 0 || a_index == 1) {
       return 1;
     }
-    return Fib(a_index-1)+Fib(a_index-2);
+    return Fib(a_index - 1) + Fib(a_index - 2);
+  }
+
+  //(Sort of) This is what we would like to find via GP:
+  private static int Fib_iter(int a_index) {
+    if (a_index == 0 || a_index == 1) {
+      return 1;
+    }
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    for (int i = 2; i <= a_index; i++) {
+      c = a + b;
+      a = b;
+      b = c;
+    }
+    return c;
   }
 
   /**
@@ -86,16 +112,20 @@ public class Fibonacci
    * @author Klaus Meffert
    * @since 3.0
    */
-  public static void main(String[] args)
-      throws Exception {
-    System.out.println("Formula to discover: Fibonacci(x)");
-    GPConfiguration config = new GPConfiguration();
-    config.setMaxInitDepth(8);
-    config.setPopulationSize(800);
-    config.setFitnessFunction(new Fibonacci.FormulaFitnessFunction());
-    GPGenotype gp = create(config);
-    gp.evolve(800);
-    gp.outputSolution(gp.getAllTimeBest());
+  public static void main(String[] args) {
+    try {
+      System.out.println("Formula to discover: Fibonacci(x)");
+      GPConfiguration config = new GPConfiguration();
+      config.setMaxInitDepth(8);
+      config.setPopulationSize(800);
+      config.setFitnessFunction(new Fibonacci.FormulaFitnessFunction());
+      GPGenotype gp = create(config);
+      gp.evolve(1200);
+      gp.outputSolution(gp.getAllTimeBest());
+    }
+    catch (Exception ex) {
+      System.exit(1);
+    }
   }
 
   public static class FormulaFitnessFunction
@@ -107,17 +137,26 @@ public class Fibonacci
     public double computeRawFitness(ProgramChromosome ind) {
       double error = 0.0f;
       Object[] noargs = new Object[0];
-      for (int i = 0; i < 20; i++) {
+      for (int i = 0; i < NUMFIB; i++) {
         vx.set(x[i]);
         try {
-          double result = ind.execute_int(noargs);
-          error += Math.abs(result - y[i]);
+          try {
+            double result = ind.execute_int(noargs);
+            error += Math.abs(result - y[i]);
+          }
+          catch (IllegalStateException iex) {
+            error = Double.MAX_VALUE / 2;
+            break;
+          }
         }
         catch (ArithmeticException ex) {
           System.out.println("x = " + x[i].intValue());
           System.out.println(ind);
           throw ex;
         }
+      }
+      if (getGPConfiguration().stackSize() > 0) {
+        error = Double.MAX_VALUE / 2;
       }
       if (error < 0.000001) {
         error = 0.0d;
