0f3fd71542a8861aaf7a6a51e32d580271b472b7
Klaus Meffert <kontakt@doktor-meffert.de>
2006-08-25 13:14:08 +0000

    new command
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2439 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/function/Multiply3.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.function;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+
+/**
+ * The multiply operation with three argument (X * Y * Z)
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class Multiply3
+    extends MathCommand {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private static final String CVS_REVISION = "$Revision: 1.1 $";
+
+  public Multiply3(final Configuration a_conf, Class a_type)
+      throws InvalidConfigurationException {
+    super(a_conf, 3, a_type);
+  }
+
+  protected Gene newGeneInternal() {
+    try {
+      Gene gene = new Multiply3(getConfiguration(), getReturnType());
+      return gene;
+    } catch (InvalidConfigurationException iex) {
+      throw new IllegalStateException(iex.getMessage());
+    }
+  }
+
+  public String toString() {
+    return "&1 * &2 * &3";
+  }
+
+  public int execute_int(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_int(n, 0, args) * c.execute_int(n, 1, args) *
+        c.execute_int(n, 2, args);
+  }
+
+  public long execute_long(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_long(n, 0, args) * c.execute_long(n, 1, args) *
+        c.execute_long(n, 2, args);
+  }
+
+  public float execute_float(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_float(n, 0, args) * c.execute_float(n, 1, args) *
+        c.execute_float(n, 2, args);
+  }
+
+  public double execute_double(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_double(n, 0, args) * c.execute_double(n, 1, args) *
+        c.execute_double(n, 2, args);
+  }
+
+  public Object execute_object(ProgramChromosome c, int n, Object[] args) {
+    return ( (Compatible) ( (Compatible) c.execute_object(n, 0, args)).
+            execute_multiply3(c.execute_object(n, 1, args))).execute_multiply3(
+                c.execute_object(n, 2, args));
+  }
+
+  public static interface Compatible {
+    public Object execute_multiply3(Object o);
+  }
+}
