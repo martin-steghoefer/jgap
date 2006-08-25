b27e41e877698df00a60d24cfccc5cbc0e227e5f
Klaus Meffert <kontakt@doktor-meffert.de>
2006-08-25 21:25:51 +0000

    new class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2456 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/DefaultGPFitnessEvaluator.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+/**
+ * A default implementation of a fitness evaluator. This implementation is
+ * straight forward: a higher fitness value is seen as fitter.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class DefaultGPFitnessEvaluator
+    implements IGPFitnessEvaluator {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Compares the first given fitness value with the second and returns true
+   * if the first one is greater than the second one. Otherwise returns false
+   * @param a_fitness_value1 first fitness value
+   * @param a_fitness_value2 second fitness value
+   * @return true: first fitness value greater than second
+   *
+   * @author Klaus Meffert
+   * @since 2.0 (until 1.1: input types int)
+   */
+  public boolean isFitter(final double a_fitness_value1,
+                          final double a_fitness_value2) {
+    return a_fitness_value1 > a_fitness_value2;
+  }
+
+  public boolean isFitter(GPProgram a_prog1, GPProgram a_prog2) {
+    return isFitter(a_prog1.getFitnessValue(), a_prog2.getFitnessValue());
+  }
+}
---
+++ src/org/jgap/gp/IGPFitnessEvaluator.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+/**
+ * Interface for a fitness evaluator used in a Genotype to determine how to
+ * interpret the fitness value. The fitness value can either be interpreted
+ * straight forward as a fitness indicator (the higher the better). Or it could
+ * be seen as a defect rate (the lower the better).
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public interface IGPFitnessEvaluator
+    extends java.io.Serializable {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Compares the first given fitness value with the second and returns true
+   * if the first one is greater than the second one. Otherwise returns false
+   * @param a_fitness_value1 first fitness value
+   * @param a_fitness_value2 second fitness value
+   * @return true: first fitness value greater than second
+   *
+   * @author Klaus Meffert
+   * @since 2.0 (until 1.1: input types int)
+   */
+  boolean isFitter(double a_fitness_value1, double a_fitness_value2);
+
+  boolean isFitter(GPProgram a_prog1, GPProgram a_prog2);
+}
