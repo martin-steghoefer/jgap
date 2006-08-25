379ca19e64205297e6ce587389fb537f4d3a7e4f
Klaus Meffert <kontakt@doktor-meffert.de>
2006-08-25 12:36:35 +0000

    new commands
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2435 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/function/Add3.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.function;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+
+/**
+ * The add operation with three parameters (X + Y + Z).
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class Add3
+    extends MathCommand {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public Add3(final Configuration a_conf, Class type)
+      throws InvalidConfigurationException {
+    super(a_conf, 3, type);
+  }
+
+  protected Gene newGeneInternal() {
+    try {
+      Gene gene = new Add3(getConfiguration(), getReturnType());
+      return gene;
+    } catch (InvalidConfigurationException iex) {
+      throw new IllegalStateException(iex.getMessage());
+    }
+  }
+
+  public String toString() {
+    return "&1 + &2 + &3";
+  }
+
+  public int execute_int(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_int(n, 0, args) + c.execute_int(n, 1, args) +
+        c.execute_int(n, 2, args);
+  }
+
+  public long execute_long(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_long(n, 0, args) + c.execute_long(n, 1, args) +
+        c.execute_long(n, 2, args);
+  }
+
+  public float execute_float(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_float(n, 0, args) + c.execute_float(n, 1, args) +
+        c.execute_float(n, 2, args);
+  }
+
+  public double execute_double(ProgramChromosome c, int n, Object[] args) {
+    return c.execute_double(n, 0, args) + c.execute_double(n, 1, args) +
+        c.execute_double(n, 2, args);
+  }
+
+  public Object execute_object(ProgramChromosome c, int n, Object[] args) {
+    return ( (Compatible) ( (Compatible) c.execute_object(n, 0, args)).
+            execute_add3(c.execute_object(n, 1, args))).execute_add3(
+                c.execute_object(n, 2, args));
+  }
+
+  public static interface Compatible {
+    public Object execute_add3(Object o);
+  }
+}
---
+++ src/org/jgap/gp/function/Pow.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.function;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+
+/**
+ * The power operation.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class Pow
+    extends MathCommand {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public Pow(final Configuration a_conf, Class a_type)
+      throws InvalidConfigurationException {
+    super(a_conf, 2, a_type);
+  }
+
+  protected Gene newGeneInternal() {
+    try {
+      Gene gene = new Pow(getConfiguration(), getReturnType());
+      return gene;
+    } catch (InvalidConfigurationException iex) {
+      throw new IllegalStateException(iex.getMessage());
+    }
+  }
+
+  public String toString() {
+    return "&1 ^ &2";
+  }
+
+  public int execute_int(ProgramChromosome c, int n, Object[] args) {
+    int i = c.execute_int(n, 0, args);
+    int j = c.execute_int(n, 1, args);
+    // clip to -10000 -> 20
+    return (int) Math.pow(Math.max( -10000.0f, Math.min(i, 20.0f)),
+                          Math.max( -10000.0f, Math.min(j, 20.0f)));
+  }
+
+  public float execute_float(ProgramChromosome c, int n, Object[] args) {
+    float f = c.execute_float(n, 0, args);
+    float g = c.execute_float(n, 1, args);
+    // clip to -10000 -> 20
+    return (float) Math.pow(Math.max( -10000.0f, Math.min(f, 20.0f)),
+                            Math.max( -10000.0f, Math.min(g, 20.0f)));
+  }
+
+  public double execute_double(ProgramChromosome c, int n, Object[] args) {
+    double f = c.execute_double(n, 0, args);
+    double g = c.execute_double(n, 1, args);
+    // clip to -10000 -> 20
+    return Math.pow(Math.max( -10000.0, Math.min(f, 20.0)),
+                    Math.max( -10000.0, Math.min(g, 20.0)));
+  }
+
+  public Object execute_object(ProgramChromosome c, int n, Object[] args) {
+    return ( (Compatible) c.execute_object(n, 0, args)).execute_pow(
+        c.execute_object(n, 1, args));
+  }
+
+  public static interface Compatible {
+    public Object execute_pow(Object o);
+  }
+}
