6c57856ac76e69473d783b31b93e6ebfd01c15f0
Klaus Meffert <kontakt@doktor-meffert.de>
2006-08-25 11:47:14 +0000

    enhanced code
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2425 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/gp/function/ForLoop.java
+++ src/org/jgap/gp/function/ForLoop.java
@@ -21,45 +21,62 @@ import org.jgap.gp.*;
 public class ForLoop
     extends MathCommand {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   private Class m_typeVar;
 
-  public ForLoop(final Configuration a_conf, Class a_typeVar)
+  private int m_maxLoop;
+
+  /**
+   *
+   * @param a_conf the configuration to use
+   * @param a_typeVar Class of the loop counter terminakl (e.g. IntegerClass)
+   * @param a_maxLoop the maximum number of loops to perform
+   * @throws InvalidConfigurationException
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public ForLoop(final Configuration a_conf, Class a_typeVar, int a_maxLoop)
       throws InvalidConfigurationException {
     super(a_conf, 2, CommandGene.VoidClass);
     m_typeVar = a_typeVar;
+    m_maxLoop = a_maxLoop;
   }
 
   protected Gene newGeneInternal() {
-    return null;/**@todo implement if necessary*/
+    return null; /**@todo implement if necessary*/
   }
 
   public String toString() {
-    return "for(int i=0;i<&1;i++) { &1 }";
+    return "for(int i=0;i<&1;i++) { &2 }";
   }
 
   public void execute_void(ProgramChromosome c, int n, Object[] args) {
-    int x = c.execute_int(n, 0, args);/**@todo consider m_typeVar*/
-    if (x > 15) {
-      x = 15;/**@todo parameterize*/
+    int x;
+    if (m_typeVar == CommandGene.IntegerClass) {
+      x = c.execute_int(n, 0, args);
+    }
+    else if (m_typeVar == CommandGene.LongClass) {
+      x = (int) c.execute_long(n, 0, args);
+    }
+    else if (m_typeVar == CommandGene.DoubleClass) {
+      x = (int) Math.round(c.execute_double(n, 0, args));
+    }
+    else if (m_typeVar == CommandGene.FloatClass) {
+      x = (int) Math.round(c.execute_float(n, 0, args));
+    }
+    else {
+      throw new RuntimeException("Type " + m_typeVar +
+                                 " not supported by ForLoop");
+    }
+    if (x > m_maxLoop) {
+      x = m_maxLoop;
     }
     for (int i = 0; i < x; i++) {
       c.execute_void(n, 1, args);
     }
   }
-
-//  public int execute_int(ProgramChromosome c, int n, Object[] args) {
-//    int x = c.execute_int(n, 0, args);
-//    if (x > 15) {
-//      x = 15;/**@todo parameterize*/
-//    }
-//    int value = 0;
-//    for (int i = 0; i < x; i++) {
-//      value = c.execute_int(n, 1, args);
-//    }
-//    return value;
-//  }
 
   public boolean isValid(ProgramChromosome a_program) {
     return true;
