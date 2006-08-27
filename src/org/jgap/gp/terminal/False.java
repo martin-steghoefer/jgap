01eb5b447de7b8210cc271670ea914075d346339
Klaus Meffert <kontakt@doktor-meffert.de>
2006-08-27 21:08:31 +0000

    new GP command
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2504 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/terminal/False.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.terminal;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+
+/**
+ * The boolean value false.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class False
+    extends MathCommand {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public False(final GPConfiguration a_conf)
+      throws InvalidConfigurationException {
+    super(a_conf, 0, CommandGene.BooleanClass);
+  }
+
+  protected CommandGene newGeneInternal() {
+    try {
+      CommandGene gene = new False(getGPConfiguration());
+      return gene;
+    } catch (InvalidConfigurationException iex) {
+      throw new IllegalStateException(iex.getMessage());
+    }
+  }
+
+  public String toString() {
+    return "false";
+  }
+
+  public boolean execute_boolean(ProgramChromosome c, int n, Object[] args) {
+    return true;
+  }
+
+  public Class getChildType(GPProgram a_ind, int a_index) {
+    return null;
+  }
+}
---
+++ src/org/jgap/gp/terminal/True.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.terminal;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+
+/**
+ * The boolean value true.
+ *
+ * @author Klaus Meffert
+ * @since 3.0
+ */
+public class True
+    extends MathCommand {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public True(final GPConfiguration a_conf)
+      throws InvalidConfigurationException {
+    super(a_conf, 0, CommandGene.BooleanClass);
+  }
+
+  protected CommandGene newGeneInternal() {
+    try {
+      CommandGene gene = new True(getGPConfiguration());
+      return gene;
+    } catch (InvalidConfigurationException iex) {
+      throw new IllegalStateException(iex.getMessage());
+    }
+  }
+
+  public String toString() {
+    return "true";
+  }
+
+  public boolean execute_boolean(ProgramChromosome c, int n, Object[] args) {
+    return true;
+  }
+
+  public Class getChildType(GPProgram a_ind, int a_index) {
+    return null;
+  }
+}
