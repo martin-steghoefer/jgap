e903aacadd98329b728121fe8cfa0f033f81378b
Klaus Meffert <kontakt@doktor-meffert.de>
2006-08-31 09:40:24 +0000

    javadoc; refactoring package structure
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2520 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/gp/INaturalGPSelector.java
+++ src/org/jgap/gp/INaturalGPSelector.java
@@ -9,27 +9,28 @@
  */
 package org.jgap.gp;
 
+import java.io.*;
+import org.jgap.gp.impl.*;
+
 /**
  * Interface for algorithms selecting individuals for evolutionary operations.
- * Classes extending this class must implement the select method.
  *
  * @author Klaus Meffert
  * @since 3.0
  */
 public interface INaturalGPSelector
-    extends java.io.Serializable {
+    extends Serializable {
   /** String containing the CVS revision. Read out via reflection!*/
-  static final String CVS_REVISION = "$Revision: 1.2 $";
+  static final String CVS_REVISION = "$Revision: 1.3 $";
 
   /**
    * Select an individual based on some algorithm.
    *
-   * @param a_world the World for the run
-   * @return the individual chosen from the world's population
+   * @param a_genotype the genotype used
+   * @return the individual chosen from the genotype's population
    *
    * @author Klaus Meffert
    * @since 3.0
    */
-//  ProgramChromosome select(GPGenotype a_world);
-  GPProgram select(GPGenotype a_world);
+  GPProgram select(GPGenotype a_genotype);
 }
