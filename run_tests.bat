d979f39b42c999990b896962f94d0f4b05120f38
Klaus Meffert <kontakt@doktor-meffert.de>
2006-09-01 17:45:54 +0000

    considered new jar
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2554 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- run_tests.bat
+++ run_tests.bat
@@ -1,2 +1,2 @@
-java -cp ".;lib\junit3.8.1.jar;lib\junit-addons-1.4.jar;jgap-tests.jar;jgap.jar" junit.textui.TestRunner org.jgap.AllTests
+java -cp ".;lib\junit3.8.1.jar;lib\junit-addons-1.4.jar;lib\commons-lang-2.1.jar;jgap-tests.jar;jgap.jar" junit.textui.TestRunner org.jgap.AllTests
 pause
