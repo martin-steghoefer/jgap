6ffdf214185425f59a1e2f4dccb8715dee33f811
Klaus Meffert <kontakt@doktor-meffert.de>
2006-09-02 18:50:00 +0000

    new base for ProgramChromosome
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2579 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/BaseGPChromosome.java
@@ -0,0 +1,326 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+import org.jgap.gp.terminal.*;
+import org.jgap.gp.impl.*;
+import org.jgap.*;
+
+/**
+ * Abstract base class for all implementations of IGPChromosome.
+ *
+ * @author Klaus Meffert
+ * @since 3.01
+ */
+public abstract class BaseGPChromosome
+    implements IGPChromosome {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * The configuration object to use
+   */
+  private
+  /*transient*/ GPConfiguration m_configuration;
+
+  /**
+   * The individual the chromosome belongs to.
+   */
+  private IGPProgram m_ind;
+
+  public BaseGPChromosome(GPConfiguration a_configuration)
+      throws InvalidConfigurationException {
+    if (a_configuration == null) {
+      throw new InvalidConfigurationException(
+          "Configuration to be set must not"
+          + " be null!");
+    }
+    m_configuration = a_configuration;
+  }
+
+  public BaseGPChromosome(GPConfiguration a_configuration, IGPProgram a_ind)
+      throws InvalidConfigurationException {
+    this(a_configuration);
+    m_ind = a_ind;
+  }
+
+  /**
+   * @return the individual containing this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public IGPProgram getIndividual() {
+    return m_ind;
+  }
+
+  /**
+   * Sets the individual the chromosome belongs to.
+   *
+   * @param a_ind the individual containing this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public void setIndividual(IGPProgram a_ind) {
+    m_ind = a_ind;
+  }
+
+  /**
+   * Gets the i'th terminal in this chromosome. The nodes are counted in a
+   * depth-first manner, with node 0 being the first terminal in this
+   * chromosome.
+   *
+   * @param a_index the i'th terminal to get
+   * @return the terminal
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int getTerminal(int a_index) {
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int j = 0; j < len && functions[j] != null; j++) {
+      if (functions[j].getArity(m_ind) == 0) {
+        if (--a_index < 0) {
+          return j;
+        }
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Gets the a_index'th function in this chromosome. The nodes are counted in a
+   * depth-first manner, with node 0 being the first function in this
+   * chromosome.
+   *
+   * @param a_index the a_index'th function to get
+   * @return the function
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int getFunction(int a_index) {
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int j = 0; j < len && functions[j] != null; j++) {
+      if (functions[j].getArity(m_ind) != 0) {
+        if (--a_index < 0) {
+          return j;
+        }
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Gets the a_index'th terminal of the given type in this chromosome. The nodes
+   * are counted in a depth-first manner, with node 0 being the first terminal of
+   * the given type in this chromosome.
+   *
+   * @param a_index the a_index'th terminal to get
+   * @param a_type the type of terminal to get
+   * @return the index of the terminal found, or -1 if no appropriate terminal
+   * was found
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int getTerminal(int a_index, Class a_type) {
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int j = 0; j < len && functions[j] != null; j++) {
+      if (functions[j].getReturnType() == a_type
+          && functions[j].getArity(m_ind) == 0) {
+        if (--a_index < 0) {
+          return j;
+        }
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Gets the i'th function of the given type in this chromosome. The nodes are
+   * counted in a depth-first manner, with node 0 being the first function of
+   * the given type in this chromosome.
+   *
+   * @param a_index the i'th function to get
+   * @param a_type the type of function to get
+   * @return the index of the function found, or -1 if no appropriate function
+   * was found
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int getFunction(int a_index, Class a_type) {
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int j = 0; j < len && functions[j] != null; j++) {
+      if (functions[j].getReturnType() == a_type
+          && functions[j].getArity(m_ind) != 0) {
+        if (--a_index < 0) {
+          return j;
+        }
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * @return the number of terminals in this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public int numTerminals() {
+    int count = 0;
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int i = 0; i < len && functions[i] != null; i++) {
+      if (functions[i].getArity(m_ind) == 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  /**
+   * @return the number of functions in this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.0
+   */
+  public int numFunctions() {
+    int count = 0;
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int i = 0; i < len && functions[i] != null; i++) {
+      if (functions[i].getArity(m_ind) != 0) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Counts the number of terminals of the given type in this chromosome.
+   *
+   * @param a_type the type of terminal to count
+   * @return the number of terminals in this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int numTerminals(Class a_type) {
+    int count = 0;
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int i = 0; i < len && functions[i] != null; i++) {
+      if (functions[i].getArity(m_ind) == 0
+          && functions[i].getReturnType() == a_type) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Counts the number of functions of the given type in this chromosome.
+   *
+   * @param a_type the type of function to count
+   * @return the number of functions in this chromosome.
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int numFunctions(Class a_type) {
+    int count = 0;
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int i = 0; i < len && functions[i] != null; i++) {
+      if (functions[i].getArity(m_ind) != 0
+          && functions[i].getReturnType() == a_type) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  /**
+   * Gets the a_index'th node in this chromosome. The nodes are counted in a
+   * depth-first manner, with node 0 being the root of this chromosome.
+   *
+   * @param a_index the node number to get
+   * @return the node
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public CommandGene getNode(int a_index) {
+    if (a_index >= getFunctions().length || getFunctions()[a_index] == null) {
+      return null;
+    }
+    return getFunctions()[a_index];
+  }
+
+  /**
+   * Helper: Find GP command with given class and return index of it
+   * @param a_n return the n'th found command
+   * @param a_class the class to find a command for
+   * @return index of first found matching GP command, or -1 if none found
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int getCommandOfClass(int a_n, Class a_class) {
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int j = 0; j < len && functions[j] != null; j++) {
+      if (functions[j].getClass() == a_class) {
+        if (--a_n < 0) {
+          return j;
+        }
+      }
+    }
+    return -1;
+  }
+
+  /**
+   * Helper: Find GP Variable with given return type and return index of it
+   * @param a_n return the n'th found command
+   * @param a_returnType the return type to find a Variable for
+   * @return index of first found matching GP command, or -1 if none found
+   *
+   * @author Klaus Meffert
+   * @since 3.01 (since 3.0 in ProgramChromosome)
+   */
+  public int getVariableWithReturnType(int a_n, Class a_returnType) {
+    CommandGene[] functions = getFunctions();
+    int len = functions.length;
+    for (int j = 0; j < len && functions[j] != null; j++) {
+      if (functions[j].getClass() == Variable.class) {
+        Variable v = (Variable) functions[j];
+        if (v.getReturnType() == a_returnType) {
+          if (--a_n < 0) {
+            return j;
+          }
+        }
+      }
+    }
+    return -1;
+  }
+
+  public GPConfiguration getGPConfiguration() {
+    return m_configuration;
+  }
+
+}
---
+++ src/org/jgap/gp/IGPChromosome.java
@@ -0,0 +1,192 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+import java.io.*;
+import org.jgap.gp.impl.*;
+/**
+ * Interface for GP chromosomes. See ProgramChromosome for an implementation.
+ *
+ * @author Klaus Meffert
+ * @since 3.01
+ */
+public interface IGPChromosome
+    extends Serializable {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * @return the individual containing this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  IGPProgram getIndividual();
+
+  /**
+   * Sets the individual the chromosome belongs to.
+   *
+   * @param a_ind the individual containing this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  void setIndividual(IGPProgram a_ind);
+
+  /**
+   * Clean up the chromosome.
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  void cleanup();
+
+  /**
+   * Output program in "natural" notion (e.g.: "X + Y" for "X + Y")
+   * @param a_startNode the node to start with
+   * @return output in normalized notion
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  String toStringNorm(final int a_startNode);
+
+  /**
+   * Recalculate the depths of each node.
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  void redepth();
+
+  /**
+   * @return the number of terminals in this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int numTerminals();
+
+  /**
+   * @return the number of functions in this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int numFunctions();
+
+  /**
+   * Counts the number of terminals of the given type in this chromosome.
+   *
+   * @param a_type the type of terminal to count
+   * @return the number of terminals in this chromosome
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int numTerminals(Class a_type);
+
+  /**
+   * Counts the number of functions of the given type in this chromosome.
+   *
+   * @param a_type the type of function to count
+   * @return the number of functions in this chromosome.
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int numFunctions(Class a_type);
+
+  /**
+   * Gets the a_index'th node in this chromosome. The nodes are counted in a
+   * depth-first manner, with node 0 being the root of this chromosome.
+   *
+   * @param a_index the node number to get
+   * @return the node
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  CommandGene getNode(int a_index);
+
+  /**
+   * Gets the a_child'th child of the a_index'th node in this chromosome. This
+   * is the same as the a_child'th node whose depth is one more than the depth
+   * of the a_index'th node.
+   *
+   * @param a_index the node number of the parent
+   * @param a_child the child number (starting from 0) of the parent
+   * @return the node number of the child, or -1 if not found
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int getChild(int a_index, int a_child);
+
+  /**
+   * Gets the i'th terminal in this chromosome. The nodes are counted in a
+   * depth-first manner, with node 0 being the first terminal in this
+   * chromosome.
+   *
+   * @param a_index the i'th terminal to get
+   * @return the terminal
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int getTerminal(int a_index);
+
+  /**
+   * Gets the a_index'th function in this chromosome. The nodes are counted in a
+   * depth-first manner, with node 0 being the first function in this
+   * chromosome.
+   *
+   * @param a_index the a_index'th function to get
+   * @return the function
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int getFunction(int a_index);
+
+  /**
+   * Gets the a_index'th terminal of the given type in this chromosome. The nodes
+   * are counted in a depth-first manner, with node 0 being the first terminal of
+   * the given type in this chromosome.
+   *
+   * @param a_index the a_index'th terminal to get
+   * @param a_type the type of terminal to get
+   * @return the index of the terminal found, or -1 if no appropriate terminal
+   * was found
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int getTerminal(int a_index, Class a_type);
+
+  /**
+   * Gets the i'th function of the given type in this chromosome. The nodes are
+   * counted in a depth-first manner, with node 0 being the first function of
+   * the given type in this chromosome.
+   *
+   * @param a_index the i'th function to get
+   * @param a_type the type of function to get
+   * @return the index of the function found, or -1 if no appropriate function
+   * was found
+   *
+   * @author Klaus Meffert
+   * @since 3.01
+   */
+  int getFunction(int a_index, Class a_type);
+
+  CommandGene[] getFunctions();
+
+  GPConfiguration getGPConfiguration();
+}
