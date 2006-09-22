44a96eec1d7ee6f1b90672474d62f04717f8b6b4
Klaus Meffert <kontakt@doktor-meffert.de>
2006-09-22 18:10:49 +0000

    moved from examples.grid package to here
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2683 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/distr/grid/JGAPServer.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import org.apache.commons.cli.*;
+import org.apache.log4j.*;
+import org.homedns.dade.jcgrid.cmd.*;
+import org.homedns.dade.jcgrid.server.*;
+
+/**
+ * A grid server able receiving work requests from JGAPClient's, sending
+ * work units to JGAPWorker's, receiving solutions from JGAPWorker's, and
+ * sending back these solutions to the requesting JGAPClient.
+ *
+ * @author Klaus Meffert
+ * @since 3.01
+ */
+public class JGAPServer {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private final static String className = JGAPServer.class.getName();
+
+  private static Logger log = Logger.getLogger(className);
+
+  public JGAPServer(String[] args)
+      throws Exception {
+    GridServer gs = new GridServer();
+    Options options = new Options();
+    CommandLine cmd = MainCmd.parseCommonOptions(options, gs.getNodeConfig(),
+        args);
+    // Start Server
+    gs.start();
+  }
+
+  public static void main(String[] args)
+      throws Exception {
+    MainCmd.setUpLog4J("server", true);
+    //start server
+    new JGAPServer(args);
+  }
+}
---
+++ src/org/jgap/distr/grid/JGAPWorker.java
@@ -0,0 +1,58 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import org.apache.log4j.*;
+import org.homedns.dade.jcgrid.*;
+import org.homedns.dade.jcgrid.worker.*;
+
+/**
+ * A worker receives work units from a JGAPServer and sends back computed
+ * solutions to the JGAPServer.
+ *
+ * @author Klaus Meffert
+ * @since 3.01
+ */
+public class JGAPWorker {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private final static String className = JGAPWorker.class.getName();
+
+  private static Logger log = Logger.getLogger(className);
+
+  public JGAPWorker(GridNodeWorkerConfig a_config, Class a_workerClass,
+                    Class a_workerFeedbackClaas)
+      throws Exception {
+    // Start all required Workers.
+    // ---------------------------
+    GridWorker[] gw = new GridWorker[a_config.getWorkerCount()];
+    for (int i = 0; i < a_config.getWorkerCount(); i++) {
+      gw[i] = new GridWorker();
+      gw[i].setNodeConfig( (GridNodeGenericConfig) a_config.clone());
+      ( (GridNodeGenericConfig) gw[i].getNodeConfig()).
+          setSessionName(a_config.getSessionName() + "_" + i);
+      ( (GridNodeGenericConfig) gw[i].getNodeConfig()).
+          setWorkingDir(a_config.getWorkingDir() + "_" + i);
+      // Instantiate worker and workerfeedback.
+      // --------------------------------------
+      Worker myWorker = (Worker) a_workerClass.newInstance();
+      GridWorkerFeedback myWorkerFeedback = (GridWorkerFeedback)
+          a_workerFeedbackClaas.newInstance();
+      gw[i].setWorker(myWorker);
+      gw[i].setWorkerFeedback(myWorkerFeedback);
+      gw[i].start();
+    }
+    // Wait for shutdown of workers.
+    // -----------------------------
+    for (int i = 0; i < a_config.getWorkerCount(); i++)
+      gw[i].waitShutdown();
+  }
+}
