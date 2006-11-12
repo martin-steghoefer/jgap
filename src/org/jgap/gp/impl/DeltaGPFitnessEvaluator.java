5a71cd437b984846294882adcf8165cc31e02e78
Klaus Meffert <kontakt@doktor-meffert.de>
2006-11-12 18:58:19 +0000

    new class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2706 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/impl/DeltaGPFitnessEvaluator.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.impl;
+
+import org.jgap.gp.*;
+
+/**
+ * Fitness evaluator taking input as delta values. Thus a lower value is seen
+ * as fitter.
+ *
+ * @author Klaus Meffert
+ * @since 3.1
+ */
+public class DeltaGPFitnessEvaluator
+    implements IGPFitnessEvaluator {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Compares the first given fitness value with the second and returns true
+   * if the first one is smaller than the second one. Otherwise returns false
+   * @param a_fitness_value1 first fitness value
+   * @param a_fitness_value2 second fitness value
+   * @return true: first fitness value greater than second
+   *
+   * @author Klaus Meffert
+   * @since 3.1
+   */
+  public boolean isFitter(final double a_fitness_value1,
+                          final double a_fitness_value2) {
+    if (!Double.isNaN(a_fitness_value1) &&
+        !Double.isNaN(a_fitness_value2)) {
+      return a_fitness_value1 < a_fitness_value2;
+    }
+    else if (Double.isNaN(a_fitness_value1)) {
+      return false;
+    }
+    return true;
+  }
+
+  /*
+   * @author Klaus Meffert
+   * @since 3.1
+   */
+  public boolean isFitter(IGPProgram a_prog1, IGPProgram a_prog2) {
+    return isFitter(a_prog1.getFitnessValue(), a_prog2.getFitnessValue());
+  }
+
+  /*
+   * @author Klaus Meffert
+   * @since 3.1
+   */
+  public boolean equals(Object a_object) {
+    DeltaGPFitnessEvaluator eval = (DeltaGPFitnessEvaluator) a_object;
+    return true;
+  }
+
+  /*
+   * @author Klaus Meffert
+   * @since 3.1
+   */
+  public int compareTo(Object a_object) {
+    DeltaGPFitnessEvaluator eval = (DeltaGPFitnessEvaluator) a_object;
+    return 0;
+  }
+}
