b80ce4e65b72d63469e7fbb6ba252f65969c3370
Klaus Meffert <kontakt@doktor-meffert.de>
2006-11-20 16:59:29 +0000

    was inner class from ExampleClient
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2724 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ examples/src/examples/grid/MyClientFeedback.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples.grid;
+
+import org.jgap.*;
+import org.jgap.distr.grid.*;
+
+/**
+ * Listener for feedback sent to the client. This is a simple sample
+ * implementation.
+ *
+ * @author Klaus Meffert
+ * @since 3.1
+ */
+public class MyClientFeedback
+    implements IClientFeedback {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
+
+  public MyClientFeedback() {
+  }
+
+  public void error(String msg, Exception ex) {
+    System.err.println(msg);
+  }
+
+  public void sendingFragmentRequest(JGAPRequest req) {
+    System.err.println("Sending work");
+  }
+
+  public void receivedFragmentResult(JGAPRequest req, JGAPResult res,
+                                     int idx) {
+    System.err.println("Receiving work. Solution " + res.getFittest());
+  }
+
+  public void beginWork() {
+  }
+
+  public void endWork() {
+  }
+
+  public void setProgressMaximum(int max) {
+  }
+
+  public void setProgressMinimum(int min) {
+  }
+
+  public void setProgressValue(int val) {
+  }
+
+  public void setRenderingTime(MyRequest req, long dt) {
+  }
+
+  public void completeFrame(int idx) {
+  }
+}
