ad5a0c46d7185481243a24d3388b4dd4020c4416
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-15 20:41:29 +0000

    new classes
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2784 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/distr/grid/DefaultEvolveStrategy.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import org.jgap.Genotype;
+
+/**
+ * Default and simple implementation of IEvolveStrategy.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class DefaultEvolveStrategy implements IEvolveStrategy {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public void evolve(Genotype a_genotype) {
+    a_genotype.evolve();
+  }
+}
---
+++ src/org/jgap/distr/grid/IEvolveStrategy.java
@@ -0,0 +1,26 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import org.jgap.*;
+
+/**
+ * Interface for defining a strategy that controls how a worker evolves a single
+ * request.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public interface IEvolveStrategy {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  void evolve(Genotype a_genotype);
+}
