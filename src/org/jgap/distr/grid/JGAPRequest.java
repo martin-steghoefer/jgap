4c41e9fc5ba679ed02b02ff0fea7f0a46eb9f3d0
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-15 20:42:55 +0000

    Javadoc; enhanced logic; supported IEvolveStrategy
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2786 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/grid/JGAPRequest.java
+++ src/org/jgap/distr/grid/JGAPRequest.java
@@ -13,7 +13,7 @@ import org.homedns.dade.jcgrid.*;
 import org.jgap.*;
 
 /**
- * An instance creating single requests to be sent to a worker.
+ * An instance that creates single requests to be sent to a worker.
  *
  * @author Klaus Meffert
  * @since 3.1
@@ -21,13 +21,103 @@ import org.jgap.*;
 public abstract class JGAPRequest
     extends WorkRequest {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   private Configuration m_config;
 
-  public JGAPRequest(String name, int id, Configuration a_config) {
+  private Population m_pop;
+
+  private IEvolveStrategy m_evolveStrategy;
+
+  /**
+   * Constructor.
+   *
+   * @param name String
+   * @param id int
+   * @param a_config Configuration
+   * @param a_strategy the strategy to choose for evolution
+   *
+   * @author Klaus Meffert
+   * @since 3.1
+   */
+  public JGAPRequest(String name, int id, Configuration a_config,
+                     IEvolveStrategy a_strategy) {
     super(name, id);
     m_config = a_config;
+    m_evolveStrategy = a_strategy;
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param name String
+   * @param id int
+   * @param a_config Configuration
+   *
+   * @author Klaus Meffert
+   * @since 3.1
+   */
+  public JGAPRequest(String name, int id, Configuration a_config) {
+    this(name, id, a_config, new DefaultEvolveStrategy());
+  }
+
+  /**
+   * Constructor. Allows to specify a preset population with which the genotype
+   * is initialized.
+   *
+   * @param name String
+   * @param id int
+   * @param a_config Configuration
+   * @param a_pop Population
+   * @param a_strategy the strategy to choose for evolution
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public JGAPRequest(String name, int id, Configuration a_config,
+                     Population a_pop, IEvolveStrategy a_strategy) {
+    this(name, id, a_config, a_strategy);
+    m_pop = a_pop;
+  }
+
+  /**
+   * Constructor. Allows to specify a preset population with which the genotype
+   * is initialized.
+   *
+   * @param name String
+   * @param id int
+   * @param a_config Configuration
+   * @param a_pop Population
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public JGAPRequest(String name, int id, Configuration a_config,
+                     Population a_pop) {
+    this(name, id, a_config, a_pop, new DefaultEvolveStrategy());
+  }
+
+  /**
+   * Sets the strategy to use for executing the evolution with a worker for
+   * a single request.
+   *
+   * @param a_evolveStrategy the evolve strategy to use
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public void setEvolveStrategy(IEvolveStrategy a_evolveStrategy) {
+    m_evolveStrategy = a_evolveStrategy;
+  }
+
+  /**
+   * @return the evolve strategy set
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public IEvolveStrategy getEvolveStrategy() {
+    return m_evolveStrategy;
   }
 
   /**
@@ -51,4 +141,15 @@ public abstract class JGAPRequest
    */
   public abstract JGAPRequest[] split()
       throws Exception;
+
+  /**
+   * @return the population used to initialize new requests. May be null or
+   * empty
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public Population getPopulation() {
+    return m_pop;
+  }
 }
