8d846e4706adeeebee58f061c36597e842154abd
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-16 15:27:14 +0000

    Javadoc only
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2804 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/IPopulationMerger.java
+++ src/org/jgap/distr/IPopulationMerger.java
@@ -13,21 +13,22 @@ import org.jgap.*;
 
 /**
  * Interface for implementations allowing to merge two or more independent
- * Population's to be merged together into one Population.
+ * Populations to be merged together into one Population.
  *
  * @author Klaus Meffert
  * @since 2.0
  */
 public interface IPopulationMerger {
   /** String containing the CVS revision. Read out via reflection!*/
-  final static String CVS_REVISION = "$Revision: 1.3 $";
+  final static String CVS_REVISION = "$Revision: 1.4 $";
 
   /**
    * Merges two Population's into one that has the given size.
-   * @param a_population1 Population first Population
-   * @param a_population2 Population second Population
-   * @param a_new_population_size int size of merged Population
-   * @return Population the resulting Population
+   *
+   * @param a_population1 first Population
+   * @param a_population2 second Population
+   * @param a_new_population_size size of merged Population
+   * @return the resulting Population
    *
    * @author Klaus Meffert
    * @since 2.0
