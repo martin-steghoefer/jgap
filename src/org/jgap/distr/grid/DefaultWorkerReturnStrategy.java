0f3216fd18cf309034813cb68857c0f51757a507
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-16 12:12:28 +0000

    new classes
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2796 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/distr/grid/DefaultWorkerReturnStrategy.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import org.jgap.*;
+
+/**
+ * Default and simple implementation for IWorkerReturnStrategy.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class DefaultWorkerReturnStrategy
+    implements IWorkerReturnStrategy {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Simple returns the fittest chromosome.
+   *
+   * @param a_req JGAPRequest
+   * @param a_genotype Genotype
+   * @return JGAPResult
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public JGAPResult assembleResult(JGAPRequest a_req, Genotype a_genotype) {
+    try {
+      IChromosome fittest = a_genotype.getFittestChromosome();
+      Population pop = new Population(a_req.getConfiguration(), fittest);
+      JGAPResult result = new JGAPResult(a_req.getSessionName(), a_req.getRID(),
+          pop, 1);
+      return result;
+    } catch (Throwable t) {
+      throw new RuntimeException(t);
+    }
+  }
+}
---
+++ src/org/jgap/distr/grid/IWorkerReturnStrategy.java
@@ -0,0 +1,27 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import java.io.*;
+import org.jgap.*;
+
+/**
+ * Defines which part of a result is returned by a worker.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public interface IWorkerReturnStrategy
+    extends Serializable {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  JGAPResult assembleResult(JGAPRequest a_request, Genotype a_genotype);
+}
