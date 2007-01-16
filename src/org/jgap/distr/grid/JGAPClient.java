35dba11b9b3a2fd63b3e7839ed60b48401a7ab80
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-16 21:49:46 +0000

    considered RequestSplitStrategy
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2824 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/grid/JGAPClient.java
+++ src/org/jgap/distr/grid/JGAPClient.java
@@ -24,7 +24,7 @@ import org.homedns.dade.jcgrid.message.GridMessageWorkRequest;
 public abstract class JGAPClient
     extends Thread {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.4 $";
+  private final static String CVS_REVISION = "$Revision: 1.5 $";
 
   private final static String className = JGAPClient.class.getName();
 
@@ -35,6 +35,8 @@ public abstract class JGAPClient
   protected IClientFeedback m_clientFeedback;
 
   protected JGAPRequest m_workReq;
+
+  protected RequestSplitStrategy m_splitStrategy;
 
   public JGAPClient(GridNodeClientConfig a_gridconfig, //Configuration cfg,
                     IClientFeedback feedback, JGAPRequest req) {
@@ -70,10 +72,14 @@ public abstract class JGAPClient
       gc.setNodeConfig(m_gridconfig);
       gc.start();
       try {
+        /**@todo here we can care about distributed fitness computation
+         * For that let compute the fitness of each individual by the workers.
+         * Afterwards do the evolution and continue with previous step until
+         * target reached*/
         // Split the work. This is done by the work request.
         // -------------------------------------------------
         JGAPRequest[] workList;
-        workList = m_workReq.split();
+        workList = m_splitStrategy.split(m_workReq);
         m_clientFeedback.setProgressMaximum(0);
         m_clientFeedback.setProgressMaximum(workList.length - 1);
         m_clientFeedback.beginWork();
@@ -110,5 +116,17 @@ public abstract class JGAPClient
       m_clientFeedback.error("Error while doing the work", ex);
     }
     m_clientFeedback.endWork();
+  }
+
+  public void setRequestSplitStrategy(RequestSplitStrategy a_splitStrategy) {
+    m_splitStrategy = a_splitStrategy;
+  }
+
+  public void start() {
+    if (m_splitStrategy == null) {
+      throw new RuntimeException("Please set the request split strategy first"
+                                 +" with JGAPClient before starting it!");
+    }
+    super.start();
   }
 }
