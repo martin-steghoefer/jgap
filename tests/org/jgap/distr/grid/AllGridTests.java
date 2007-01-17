32b6c32f5165c4a23e3f4da0c82eabae564d6a4c
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-17 12:14:49 +0000

    new test suite
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2864 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/distr/grid/AllGridTests.java
@@ -0,0 +1,30 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import junit.framework.*;
+
+/**
+ * Test suite for all tests of package org.jgap.distr.grid.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class AllGridTests
+    extends TestSuite {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite("AllGridTests");
+//    suite.addTest(....suite());
+    return suite;
+  }
+}
