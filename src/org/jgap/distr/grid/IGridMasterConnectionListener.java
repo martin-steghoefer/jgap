caacb8843bcf1eb47152cd8673468eecb7715a5c
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-22 14:53:17 +0000

    new class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2880 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/distr/grid/IGridMasterConnectionListener.java
@@ -0,0 +1,24 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+/**
+ * Listener that is called from GridMasterConnection.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public interface IGridMasterConnectionListener {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**@todo add signatures*/
+}
---
+++ src/org/jgap/distr/grid/JGAPClientHandlerThread.java
@@ -0,0 +1,57 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid;
+
+import org.homedns.dade.jcgrid.server.*;
+import org.homedns.dade.jcgrid.*;
+import org.homedns.dade.jcgrid.vfs.*;
+import org.homedns.dade.jcgrid.message.*;
+import org.homedns.dade.jcgrid.util.*;
+import java.io.*;
+import java.net.*;
+
+/**
+ * Handles JGAP-specific messages on the client-side that are not supported originally by JCGrid.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class JGAPClientHandlerThread
+    extends ClientHandlerThread {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public JGAPClientHandlerThread(GridServer server, Socket socket)
+      throws IOException {
+    super(server, socket);
+  }
+
+  protected void handleMsg(GridMessage msg)
+      throws Exception {
+    if (msg instanceof GridMessageVFSSessionFileRequest) {
+      String n = ( (GridMessageVFSSessionFileRequest) msg).getName();
+      // Read the file.
+      // --------------
+      File f = new File(super.gridServer.getVFSSessionPool().getPath(),n);
+      long fsize = f.length();
+      if (log.isDebugEnabled())
+        log.debug("  File size: " + fsize);
+      /**@todo consider 4GB limit*/
+      byte[] data = new byte[ (int) fsize];
+      FileInputStream fis = new FileInputStream(f);
+      fis.read(data);
+      fis.close();
+      handlerChannel.send(new GridMessageVFSSessionFileResult(data));
+    }
+    else {
+      super.handleMsg(msg);
+    }
+  }
+}
