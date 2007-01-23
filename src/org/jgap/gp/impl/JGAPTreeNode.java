817b95d4c7600b360c8ea589e3261ece4c999e83
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-23 21:51:42 +0000

    only javadoc and code conventions
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2893 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/gp/impl/JGAPTreeNode.java
+++ src/org/jgap/gp/impl/JGAPTreeNode.java
@@ -22,7 +22,7 @@ import org.jgap.gp.*;
 public class JGAPTreeNode
     implements TreeNode {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   private ProgramChromosome m_chrom;
 
@@ -39,16 +39,17 @@ public class JGAPTreeNode
   }
 
   /**
-   * @return the child TreeNode at index childIndex
+   * @param a_childIndex index of the child to get
+   * @return the child TreeNode at index a_childIndex
    */
-  public TreeNode getChildAt(int childIndex) {
-    int child = m_chrom.getChild(m_index, childIndex);
+  public TreeNode getChildAt(int a_childIndex) {
+    int child = m_chrom.getChild(m_index, a_childIndex);
     return new JGAPTreeNode(m_chrom, child);
   }
 
   /**
    * @return the number of children <code>TreeNode</code>s the receiver
-   * contains.
+   * contains
    */
   public int getChildCount() {
     int count = 0;
@@ -65,20 +66,21 @@ public class JGAPTreeNode
   }
 
   /**
-   * @return the parent <code>TreeNode</code> of the receiver.
+   * @return the parent <code>TreeNode</code> of the receiver
    */
   public TreeNode getParent() {
     return new JGAPTreeNode(m_chrom, m_chrom.getParentNode(m_index));
   }
 
   /**
+   * @param a_node the node to retrieve the index for
    * @return the index of <code>node</code> in the receivers children.
    * If the receiver does not contain <code>node</code>, -1 will be
-   * returned.
+   * returned
    */
-  public int getIndex(TreeNode node) {
+  public int getIndex(TreeNode a_node) {
     for (int i = 0; i < getChildCount(); i++) {
-      if (getChildAt(i).equals(node)) {
+      if (getChildAt(i).equals(a_node)) {
         return i;
       }
     }
@@ -86,21 +88,21 @@ public class JGAPTreeNode
   }
 
   /**
-   * @return true if the receiver allows children.
+   * @return true if the receiver allows children
    */
   public boolean getAllowsChildren() {
     return getChildCount() > 0;
   }
 
   /**
-   * @return true if the receiver is a leaf.
+   * @return true if the receiver is a leaf
    */
   public boolean isLeaf() {
     return getChildCount() == 0;
   }
 
   /**
-   * @return the children of the receiver as an <code>Enumeration</code>.
+   * @return the children of the receiver as an <code>Enumeration</code>
    */
   public Enumeration children() {
     Vector l = new Vector();
