34a7fda60aec418d69212ac873ff500cf08dca92
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-29 13:12:30 +0000

    new GP function
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2956 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/function/ExchangeMemory.java
@@ -0,0 +1,144 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.function;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+import org.apache.commons.lang.builder.*;
+import org.jgap.gp.impl.*;
+
+/**
+ * Exchanges the values of two memory cells.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class ExchangeMemory
+    extends CommandGene {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Symbolic name of the storage. Must correspond with a chosen name for
+   * ReadTerminalCommand.
+   */
+  private String m_sourceStorageName;
+
+  private String m_targetStorageName;
+
+  public ExchangeMemory(final GPConfiguration a_conf,
+                        String a_firstStorageName,
+                        String a_secondStorageName)
+      throws InvalidConfigurationException {
+    super(a_conf, 0, CommandGene.VoidClass);
+    if (a_firstStorageName == null || a_firstStorageName.length() < 1) {
+      throw new IllegalArgumentException(
+          "First memory name must not be empty!");
+    }
+    if (a_secondStorageName == null || a_secondStorageName.length() < 1) {
+      throw new IllegalArgumentException(
+          "Second memory name must not be empty!");
+    }
+    if (a_firstStorageName.equals(a_secondStorageName)) {
+      throw new IllegalArgumentException(
+          "First and second memory name must be different!");
+    }
+    m_sourceStorageName = a_firstStorageName;
+    m_targetStorageName = a_secondStorageName;
+  }
+
+  public String toString() {
+    return "exchange("
+        + m_sourceStorageName
+        + ", "
+        + m_targetStorageName
+        + ")";
+  }
+
+  public void execute_void(ProgramChromosome c, int n, Object[] args) {
+    check(c);
+    // Read from memory.
+    // -----------------
+    try {
+      Object value1 = getGPConfiguration().readFromMemory(
+          m_sourceStorageName);
+      Object value2 = getGPConfiguration().readFromMemory(
+          m_targetStorageName);
+      // Store in memory.
+      // ----------------
+      getGPConfiguration().storeInMemory(m_sourceStorageName, value2);
+      getGPConfiguration().storeInMemory(m_targetStorageName, value1);
+    } catch (IllegalArgumentException iex) {
+      throw new IllegalStateException(
+          "ExchangeMemory without preceeding StoreTerminal");
+    }
+  }
+
+  public boolean isAffectGlobalState() {
+    /**@todo subclass from new abstract class MemoryCommand?*/
+    return true;
+  }
+
+  public boolean isValid(ProgramChromosome a_program) {
+    return true;
+  }
+
+  public Class getChildType(IGPProgram a_ind, int a_chromNum) {
+    return null;
+  }
+
+  /**
+   * The compareTo-method.
+   *
+   * @param a_other the other object to compare
+   * @return -1, 0, 1
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public int compareTo(Object a_other) {
+    if (a_other == null) {
+      return 1;
+    }
+    else {
+      ExchangeMemory other = (ExchangeMemory) a_other;
+      return new CompareToBuilder()
+          .append(m_sourceStorageName, other.m_sourceStorageName)
+          .append(m_targetStorageName, other.m_targetStorageName)
+          .toComparison();
+    }
+  }
+
+  /**
+   * The equals-method.
+   *
+   * @param a_other the other object to compare
+   * @return true if the objects are seen as equal
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public boolean equals(Object a_other) {
+    if (a_other == null) {
+      return false;
+    }
+    else {
+      try {
+        ExchangeMemory other = (ExchangeMemory) a_other;
+        return new EqualsBuilder()
+            .append(m_sourceStorageName, other.m_sourceStorageName)
+            .append(m_targetStorageName, other.m_targetStorageName)
+            .isEquals();
+      } catch (ClassCastException cex) {
+        return false;
+      }
+    }
+  }
+}
