be358ce5e546dcb9dd19cf747f3a4c731281e3c1
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-31 09:10:26 +0000

    adapted a lot to make it working
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2991 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/util/JarClassLoader.java
+++ src/org/jgap/util/JarClassLoader.java
@@ -9,11 +9,6 @@
  */
 package org.jgap.util;
 
-import java.io.*;
-import java.lang.reflect.*;
-import java.net.*;
-import java.util.jar.*;
-
 /**
  * A class loader for loading jar files, both local and remote.
  *
@@ -22,87 +17,21 @@ import java.util.jar.*;
  * @since 3.2
  */
 public class JarClassLoader
-    extends URLClassLoader {
+    extends MultiClassLoader {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.4 $";
+  private final static String CVS_REVISION = "$Revision: 1.5 $";
 
-  private URL m_url;
+  private JarResources m_jarResources;
 
-  /**
-   * Construction with an ordinary filename.
-   *
-   * @param a_filename ordinary filename
-   * @throws MalformedURLException
-   *
-   * @author Klaus Meffert
-   * @since 3.2
-   */
-  public JarClassLoader(String a_filename)
-      throws MalformedURLException {
-    this(new URL("jar:file:" + a_filename + "!/"));
+  public JarClassLoader(final String a_jarName) {
+    // Create the JarResource and suck in the .jar file.
+    m_jarResources = new JarResources(a_jarName);
   }
 
-  /**
-   * Creates a new JarClassLoader for the specified url.
-   *
-   * @param a_url the url of the jar file
-   */
-  public JarClassLoader(URL a_url) {
-    super(new URL[] {a_url});
-    m_url = a_url;
-  }
-
-  /**
-   * @returns the name of the jar file main class, or null if no "Main-Class"
-   * manifest attributes was defined
-   *
-   * @throws IOException
-   *
-   * @author Klaus Meffert
-   * @since 3.2
-   */
-  public String getMainClassName()
-      throws IOException {
-    JarURLConnection uc = (JarURLConnection) m_url.openConnection();
-    Attributes attr = uc.getMainAttributes();
-    if (attr != null) {
-      return attr.getValue(Attributes.Name.MAIN_CLASS);
-    }
-    else {
-      return null;
-    }
-  }
-
-  /**
-   * Invokes the application in this jar file given the name of the
-   * main class and an array of arguments. The class must define a
-   * static method "main" which takes an array of String arguments
-   * and is of return type "void".
-   *
-   * @param a_className the name of the main class
-   * @param a_args the arguments for the application
-   *
-   * @throws ClassNotFoundException
-   * @throws NoSuchMethodException
-   * @throws InvocationTargetException
-   *
-   * @since 3.2
-   */
-  public void invokeClass(String a_className, String[] a_args)
-      throws ClassNotFoundException, NoSuchMethodException,
-      InvocationTargetException {
-    Class c = loadClass(a_className);
-    Method m = c.getMethod("main", new Class[] {a_args.getClass()});
-    m.setAccessible(true);
-    int mods = m.getModifiers();
-    if (m.getReturnType() != void.class || !Modifier.isStatic(mods) ||
-        !Modifier.isPublic(mods)) {
-      throw new NoSuchMethodException("main");
-    }
-    try {
-      m.invoke(null, new Object[] {a_args});
-    } catch (IllegalAccessException e) {
-      // This should not happen, as we have disabled access checks
-    }
+  protected byte[] loadClassBytes(final String a_className) {
+    // Support the MultiClassLoader's class name munging facility.
+    String className = formatClassName(a_className);
+    // Attempt to get the class data from the JarResource.
+    return (m_jarResources.getResource(className));
   }
 }
