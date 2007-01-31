c8c9e051c8b0b449bdb9d5695589bd0d2e1695c8
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-31 12:04:10 +0000

    added header comments
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3020 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/src/examples/gp/tictactoe/ReadBoard.java
+++ examples/src/examples/gp/tictactoe/ReadBoard.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package examples.gp.tictactoe;
 
 import org.jgap.gp.*;
@@ -7,7 +16,7 @@ import org.jgap.gp.impl.*;
 public class ReadBoard
     extends CommandGene {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   private Board m_board;
 
