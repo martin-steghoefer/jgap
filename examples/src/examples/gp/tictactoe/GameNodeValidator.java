95af8ebed1b8d8ab0adddeecdbd41235a49fddba
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-31 20:18:50 +0000

    adapted
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3048 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/src/examples/gp/tictactoe/GameNodeValidator.java
+++ examples/src/examples/gp/tictactoe/GameNodeValidator.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package examples.gp.tictactoe;
 
 import org.jgap.gp.impl.*;
@@ -22,7 +31,8 @@ public class GameNodeValidator
    * @param a_chrom the chromosome that will contain the node, if valid (ignored
    * in this implementation)
    * @param a_node the node selected and to be validated
-   * @param a_tries number of times the validator has been called, useful for
+   * @param a_rootNode the root node of a_node, may be null for top nodes
+ * @param a_tries number of times the validator has been called, useful for
    * stopping by returning true if the number exceeds a limit
    * @param a_num the chromosome's index in the individual of this chromosome
    * @param a_recurseLevel level of recursion, i.e. the depth of a node
@@ -32,6 +42,8 @@ public class GameNodeValidator
    * @param a_depth the allowed remaining depth of the program chromosome
    * @param a_grow true: use grow mode, false: use full mode (ignored in this
    * implementation)
+   * @param a_childIndex index of the child in the parent node to which it
+   * belongs (-1 if node is root node)
    * @return true: node is valid; false: node is invalid
    *
    * @author Klaus Meffert
@@ -41,49 +53,72 @@ public class GameNodeValidator
                           CommandGene a_rootNode,
                           int a_tries, int a_num, int a_recurseLevel,
                           Class a_type, CommandGene[] a_functionSet,
-                          int a_depth, boolean a_grow) {
+                          int a_depth, boolean a_grow, int a_childIndex) {
     // Guard to avoid endless validation.
     // ----------------------------------
-    if (true || a_tries > 50) {
+    if (a_tries > 10) {
       return true;
     }
-    // Chromosome 0.
+    // Chromosome 1.
     // -------------
-    if (a_num == 0) {
+    if (a_num == 1) {
+      // Program must start with a loop.
+      // -------------------------------
+      if (a_recurseLevel == 0 && a_node.getClass() != Loop.class) {
+        return false;
+      }
+      if (a_recurseLevel == 1 && a_node.getClass() != EvaluateBoard.class) {
+        return false;
+      }
+    }
+    // Chromosome 2.
+    // -------------
+    if (a_num == 2) {
       // SubProgram needed as root
-      if (a_recurseLevel == 0 && a_type == CommandGene.VoidClass
-          && a_node.getClass() != SubProgram.class) {
+      if (a_recurseLevel == 0 && a_node.getClass() != SubProgram.class) {
         return false;
       }
       // SubProgram forbidden other than at beginning
 //      if (a_recurseLevel > 1 && a_node.getClass() == SubProgram.class) {
 //        return false;
 //      }
-      // CountStones forbidden other than under root node
-      if (a_recurseLevel > 1 && a_node.getClass() == CountStones.class) {
+      // EvaluateBoard forbidden other than under root node and as not-first
+      // child
+      if (a_recurseLevel > 1 && a_node.getClass() == EvaluateBoard.class
+          && a_childIndex > 0) {
         return false;
       }
-
+      if (a_recurseLevel == 1 && a_childIndex == 0 &&
+          a_node.getClass() != EvaluateBoard.class) {
+        return false;
+      }
+      if (a_rootNode != null && a_rootNode.getClass() != SubProgram.class
+          && a_rootNode.getClass() != IfElse.class
+          && a_node.getClass() == IfElse.class
+          && a_childIndex <= 1) {
+        return false;
+      }
+      if (a_rootNode != null && a_rootNode.getClass() != IfElse.class &&
+          a_node.getClass() != Equals.class) {
+        return false;
+      }
       // CountStones forbidden other than under SubProgram
-      if ( (a_rootNode == null || a_rootNode.getClass() != SubProgram.class) &&
-          a_node.getClass() == CountStones.class) {
-        return false;
-      }
+//      if ( (a_rootNode == null || a_rootNode.getClass() != SubProgram.class) &&
+//          a_node.getClass() == CountStones.class) {
+//        return false;
+//      }
       // CountStones needed one under root
 //      if (a_recurseLevel == 1 && a_node.getClass() != CountStones.class) {
 //        return false;
 //      }
     }
-    // Chromosome 1.
+    // Chromosome 3.
     // -------------
-    if (a_num == 1) {
-      // SubProgram needed as root
-      if (a_recurseLevel == 0 && a_depth < 4 && a_type == CommandGene.VoidClass
-          && a_node.getClass() != SubProgram.class) {
+    if (a_num == 3) {
+      if (a_recurseLevel == 0 && a_node.getClass() != PutStone1.class) {
         return false;
       }
-      // ReadTerminal needed one under root
-      if (a_recurseLevel == 1 && a_depth < 2 && a_node.getClass() != ReadTerminal.class) {
+      if (a_recurseLevel == 1 && a_node.getClass() != ReadTerminalIndexed.class) {
         return false;
       }
     }
