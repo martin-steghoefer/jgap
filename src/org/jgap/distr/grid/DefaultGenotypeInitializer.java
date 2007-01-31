cc29b7671eb76a737b8d0844329004b3d2759259
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-31 10:25:04 +0000

    made Comparable
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3007 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/grid/DefaultGenotypeInitializer.java
+++ src/org/jgap/distr/grid/DefaultGenotypeInitializer.java
@@ -18,9 +18,9 @@ import org.jgap.*;
  * @since 3.2
  */
 public class DefaultGenotypeInitializer
-    implements IGenotypeInitializer {
+    implements IGenotypeInitializer, Comparable {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   /**
    * Sets up a Genotype by adding the content of the given Population to it
@@ -36,24 +36,36 @@ public class DefaultGenotypeInitializer
    */
   public Genotype setupGenotype(JGAPRequest a_req, Population a_initialPop)
       throws Exception {
-    try {
-      Genotype gen;
-      Configuration conf = a_req.getConfiguration();
-      if (a_initialPop == null || a_initialPop.size() < 1) {
-        gen = Genotype.randomInitialGenotype(conf);
-      }
-      else {
-        // Initialize genotype with given population.
-        // ------------------------------------------
-        gen = new Genotype(conf, a_initialPop);
-        // Fill up population to get the desired size.
-        // -------------------------------------------
-        int size = conf.getPopulationSize() - a_initialPop.size();
-        gen.fillPopulation(size);
-      }
-      return gen;
-    } catch (Throwable t) {
-      throw new RuntimeException(t);
+    Genotype gen;
+    Configuration conf = a_req.getConfiguration();
+    if (a_initialPop == null || a_initialPop.size() < 1) {
+      gen = Genotype.randomInitialGenotype(conf);
+    }
+    else {
+      // Initialize genotype with given population.
+      // ------------------------------------------
+      gen = new Genotype(conf, a_initialPop);
+      // Fill up population to get the desired size.
+      // -------------------------------------------
+      int size = conf.getPopulationSize() - a_initialPop.size();
+      gen.fillPopulation(size);
+    }
+    return gen;
+  }
+
+  /**
+   * @param a_other sic
+   * @return as always
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public int compareTo(Object a_other) {
+    if (a_other.getClass().equals(getClass())) {
+      return 0;
+    }
+    else {
+      return getClass().getName().compareTo(a_other.getClass().getName());
     }
   }
 }
