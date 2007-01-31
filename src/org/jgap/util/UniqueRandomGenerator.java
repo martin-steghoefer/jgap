163ffda582ee178f87ae55d2453a8522fd0cb624
Klaus Meffert <kontakt@doktor-meffert.de>
2007-01-31 09:06:48 +0000

    new (special) random generator needed for creating GP programs
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@2988 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/util/UniqueRandomGenerator.java
@@ -0,0 +1,33 @@
+package org.jgap.util;
+
+import org.jgap.*;
+import java.util.*;
+
+public class UniqueRandomGenerator {
+  private int m_upper;
+
+  private List m_resultSet;
+
+  private RandomGenerator m_generator;
+
+  public UniqueRandomGenerator(int a_upperBoundary, RandomGenerator a_generator) {
+    m_upper = a_upperBoundary;
+    m_resultSet = new Vector();
+    m_generator = a_generator;
+    for (int i = 0; i < m_upper; i++) {
+      m_resultSet.add(new Integer(i));
+    }
+  }
+
+  public int nextInt()
+      throws IllegalStateException {
+    int size = m_resultSet.size();
+    if (size < 1) {
+      throw new IllegalStateException("No more numbers left");
+    }
+    int index = m_generator.nextInt(size);
+    Integer result = (Integer) m_resultSet.get(index);
+    m_resultSet.remove(index);
+    return result.intValue();
+  }
+}
