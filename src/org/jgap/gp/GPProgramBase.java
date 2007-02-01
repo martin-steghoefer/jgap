fd4987093fe3143beea5be0c871e9d3e0555ac51
Klaus Meffert <kontakt@doktor-meffert.de>
2007-02-01 09:59:58 +0000

    made cloneable
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3065 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/gp/GPProgramBase.java
+++ src/org/jgap/gp/GPProgramBase.java
@@ -22,7 +22,7 @@ import org.jgap.gp.impl.*;
 public abstract class GPProgramBase
     implements IGPProgram {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.8 $";
+  private final static String CVS_REVISION = "$Revision: 1.9 $";
 
   private double m_fitnessValue = FitnessFunction.NO_FITNESS_VALUE;
 
@@ -233,4 +233,12 @@ public abstract class GPProgramBase
     return m_applicationData;
   }
 
+
+  /**
+   * @return deep clone of this instance
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public abstract Object clone();
 }
