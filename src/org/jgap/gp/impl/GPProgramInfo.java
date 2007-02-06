7cf73d2c0079c03f53138a8a39e3a50da6bb76e7
Klaus Meffert <kontakt@doktor-meffert.de>
2007-02-06 21:19:17 +0000

    new class for caching GPPrograms
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3079 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/impl/GPProgramInfo.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.impl;
+
+/**
+ * Holds information about a program. Used for caching GP programs during GP
+ * evolution.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class GPProgramInfo {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private double m_fitnessValue;
+
+  private String m_toStringNorm;
+
+  private boolean m_found;
+
+  public GPProgramInfo(GPProgram a_prog, boolean a_found) {
+    m_fitnessValue = a_prog.getFitnessValue();
+    m_toStringNorm = a_prog.toStringNorm(0);
+    m_found = a_found;
+  }
+
+  public String getToStringNorm() {
+    return m_toStringNorm;
+  }
+
+  public double getFitnessValue() {
+    return m_fitnessValue;
+  }
+
+  public boolean isFound() {
+    return m_found;
+  }
+
+  public void setFound(boolean a_found) {
+    m_found = a_found;
+  }
+
+  public boolean equals(Object a_other) {
+    GPProgramInfo other = (GPProgramInfo) a_other;
+    if (m_toStringNorm == null) {
+      if (other.m_toStringNorm == null) {
+        return true;
+      }
+      return false;
+    }
+    return m_toStringNorm.equals(other.m_toStringNorm);
+  }
+
+  public int compareTo(Object a_other) {
+    GPProgramInfo other = (GPProgramInfo) a_other;
+    if (m_toStringNorm == null) {
+      if (other.m_toStringNorm == null) {
+        return 0;
+      }
+    }
+    return m_toStringNorm.compareTo(other.m_toStringNorm);
+  }
+}
