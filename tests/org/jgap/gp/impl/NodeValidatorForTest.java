90458aa034004bf4683629f55f3ce94c56165aef
Klaus Meffert <kontakt@doktor-meffert.de>
2007-03-13 17:38:04 +0000

    new test classes
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3119 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ tests/org/jgap/gp/impl/NodeValidatorForTest.java
@@ -0,0 +1,21 @@
+package org.jgap.gp.impl;
+
+import org.jgap.gp.impl.*;
+import org.jgap.gp.*;
+
+/**
+ * For testing purposes only.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class NodeValidatorForTest
+    implements INodeValidator {
+  public boolean validate(ProgramChromosome a_chrom, CommandGene a_node,
+                          CommandGene a_rootNode,
+                          int a_tries, int a_num, int a_recurseLevel,
+                          Class a_type, CommandGene[] a_functionSet,
+                          int a_depth, boolean a_grow, int a_childIndex) {
+    return true;
+  }
+}
---
+++ tests/org/jgap/gp/impl/StaticGPFitnessFunction.java
@@ -0,0 +1,99 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.impl;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+
+/**
+ * GP Fitness function always returning the same value.<p>
+ * Only for testing purpose!
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class StaticGPFitnessFunction
+    extends GPFitnessFunction implements Comparable {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * @since 3.2
+   */
+  private double m_staticFitnessValue;
+
+  /**
+   * @param a_staticFitnessValue double
+   *
+   * @author Klaus Meffert
+   * @since 2.0 (until 1.1: input type int)
+   */
+  public StaticGPFitnessFunction(double a_staticFitnessValue) {
+    m_staticFitnessValue = a_staticFitnessValue;
+  }
+
+  /**
+   * @param a_subject ignored: the GP program to evaluate
+   * @return static fitness value
+   *
+   * @author Klaus  Meffert
+   * @since 3.2
+   */
+  protected double evaluate(IGPProgram a_subject) {
+    double result = m_staticFitnessValue;
+    return result;
+  }
+
+  /**
+   * @return double typed fitness value
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public double getStaticFitnessValue() {
+    return m_staticFitnessValue;
+  }
+
+  /**
+   * @param a_staticFitnessValue the value to return as fitness value when
+   * calling evaluate()
+   *
+   * @author Klaus Meffert
+   * @since 2.0 (until 1.1: type int)
+   */
+  public void setStaticFitnessValue(double a_staticFitnessValue) {
+    m_staticFitnessValue = a_staticFitnessValue;
+  }
+
+  public int hashCode() {
+    int result = new Double(m_staticFitnessValue).hashCode();
+    return result;
+  }
+
+  /**
+   * @param a_other sic
+   * @return as always
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public int compareTo(Object a_other) {
+    StaticGPFitnessFunction other = (StaticGPFitnessFunction) a_other;
+    if (m_staticFitnessValue > other.m_staticFitnessValue) {
+      return 1;
+    }
+    else if (m_staticFitnessValue < other.m_staticFitnessValue) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
+  }
+}
