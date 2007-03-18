9ce9bd83970189558750d67264396b723283ba2c
Klaus Meffert <kontakt@doktor-meffert.de>
2007-03-18 18:12:36 +0000

    new GP worker class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3142 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/distr/grid/gp/JGAPWorkersGP.java
@@ -0,0 +1,129 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.distr.grid.gp;
+
+import org.apache.log4j.*;
+import org.homedns.dade.jcgrid.*;
+import org.homedns.dade.jcgrid.worker.*;
+import org.apache.commons.cli.*;
+import org.homedns.dade.jcgrid.cmd.MainCmd;
+
+/**
+ * A GP worker receives work units from a JGAPServer and sends back computed
+ * solutions to a JGAPServer.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class JGAPWorkersGP {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private Class m_workerClass;
+
+  private Class m_workerFeedbackClaas;
+
+  private final static String className = JGAPWorkersGP.class.getName();
+
+  private static Logger log = Logger.getLogger(className);
+
+  public JGAPWorkersGP(String[] args)
+      throws Exception {
+    MainCmd.setUpLog4J("worker", true);
+    GridNodeWorkerConfig config = new GridNodeWorkerConfig();
+    Options options = new Options();
+    CommandLine cmd = MainCmd.parseCommonOptions(options, config, args);
+    if (config.getSessionName().equals("none")) {
+      config.setSessionName("MyGAWorker_session");
+    }
+    getNeededFiles(config);
+    startWork(config);
+  }
+
+  /**
+   * Get jar and other files needed for computation from server .
+   *
+   * @param a_config GridNodeWorkerConfig
+   */
+  public void getNeededFiles(GridNodeWorkerConfig a_config) {
+    /**@todo*/
+    // determine files
+
+    // request files
+
+    // get files
+
+    // class-load jar files
+  }
+
+  public JGAPWorkersGP(GridNodeWorkerConfig a_config)
+      throws Exception {
+    startWork(a_config);
+  }
+
+  protected void startWork(GridNodeWorkerConfig a_config)
+      throws Exception {
+    // Start all required workers.
+    // ---------------------------
+    GridWorker[] gw = new GridWorker[a_config.getWorkerCount()];
+    for (int i = 0; i < a_config.getWorkerCount(); i++) {
+      // Instantiate worker via reflection.
+      // ----------------------------------
+      gw[i] = new GridWorker();
+      gw[i].setNodeConfig( (GridNodeGenericConfig) a_config.clone());
+      ( (GridNodeGenericConfig) gw[i].getNodeConfig()).
+          setSessionName(a_config.getSessionName() + "_" + i);
+      ( (GridNodeGenericConfig) gw[i].getNodeConfig()).
+          setWorkingDir(a_config.getWorkingDir() + "_" + i);
+      Worker myWorker = new JGAPWorkerGP();
+      // Instantiate worker feedback.
+      // ----------------------------
+      gw[i].setWorker(myWorker);
+      //      GridWorkerFeedback myWorkerFeedback = (GridWorkerFeedback)
+//          a_workerFeedbackClaas.newInstance();
+//      gw[i].setWorkerFeedback(myWorkerFeedback);
+      // Start single worker.
+      // --------------------
+      gw[i].start();
+    }
+    // Wait for shutdown of workers.
+    // -----------------------------
+    for (int i = 0; i < a_config.getWorkerCount(); i++) {
+      gw[i].waitShutdown();
+    }
+  }
+
+  /**
+   * Convenience method to start a workers or multiple instances of it.
+   * For possible parameters see method
+   * parseCommonOptions in class org.homedns.dade.jcgrid.cmd.MainCmd. The most
+   * important parameters are:
+   * -n <session name without spaces>
+   * -s <server IP address>
+   * -d <working directory>
+   * -c <number of workers to run>
+   *
+   * @param args see above
+   * @throws Exception
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public static void main(String[] args)
+      throws Exception {
+    MainCmd.setUpLog4J("worker", true);
+    GridNodeWorkerConfig config = new GridNodeWorkerConfig();
+    Options options = new Options();
+    CommandLine cmd = MainCmd.parseCommonOptions(options, config, args);
+    // Start worker(s).
+    // ----------------
+    new JGAPWorkersGP(config);
+  }
+}
