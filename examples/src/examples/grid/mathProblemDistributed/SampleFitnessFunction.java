b27c39e3473fb707f893b224ba93da5d153acea7
Klaus Meffert <kontakt@doktor-meffert.de>
2007-05-20 14:09:41 +0000

    added init.
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3187 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/src/examples/grid/mathProblemDistributed/SampleFitnessFunction.java
+++ examples/src/examples/grid/mathProblemDistributed/SampleFitnessFunction.java
@@ -9,12 +9,10 @@
  */
 package examples.grid.mathProblemDistributed;
 
-import org.jgap.*;
-import org.jgap.impl.*;
-import examples.gp.MathProblem;
-import org.jgap.gp.IGPProgram;
-import org.jgap.gp.GPFitnessFunction;
-import org.jgap.gp.terminal.Variable;
+import java.util.*;
+
+import org.jgap.gp.*;
+import org.jgap.gp.terminal.*;
 
 /**
  * Fitness function for our example.
@@ -23,39 +21,50 @@ import org.jgap.gp.terminal.Variable;
  * @since 3.2
  */
 public class SampleFitnessFunction
-        extends GPFitnessFunction {
-      /** String containing the CVS revision. Read out via reflection!*/
-      private final static String CVS_REVISION = "$Revision: 1.1 $";
+    extends GPFitnessFunction {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
-      static Variable vx;
+  static Variable vx;
 
-      static Float[] x = new Float[20];/**@todo initialize*/
+  static Float[] x = new Float[20]; /**@todo initialize*/
 
-      static float[] y = new float[20];/**@todo initialize*/
+  static float[] y = new float[20]; /**@todo initialize*/
 
-      protected double evaluate(final IGPProgram a_subject) {
-        return computeRawFitness(a_subject);
+  public SampleFitnessFunction() {
+    Random random = new Random();
+    // Randomly initialize function data (X-Y table) for x^4+x^3+x^2-x
+    // ---------------------------------------------------------------
+    for (int i = 0; i < 20; i++) {
+      float f = 8.0f * (random.nextFloat() - 0.3f);
+      x[i] = new Float(f);
+      y[i] = f * f * f * f + f * f * f + f * f - f;
+      System.out.println(i + ") " + x[i] + "   " + y[i]);
+    }
+  }
+
+  protected double evaluate(final IGPProgram a_subject) {
+    return computeRawFitness(a_subject);
+  }
+
+  public double computeRawFitness(final IGPProgram ind) {
+    double error = 0.0f;
+    Object[] noargs = new Object[0];
+    Variable vx = ind.getGPConfiguration().getVariable("X");
+    for (int i = 0; i < 20; i++) {
+      vx.set(x[i]);
+      try {
+        double result = ind.execute_float(0, noargs);
+        error += Math.abs(result - y[i]);
+      } catch (ArithmeticException ex) {
+        System.out.println("x = " + x[i].floatValue());
+        System.out.println(ind);
+        throw ex;
       }
-
-      public double computeRawFitness(final IGPProgram ind) {
-        double error = 0.0f;
-        Object[] noargs = new Object[0];
-        Variable vx = ind.getGPConfiguration().getVariable("X");
-        for (int i = 0; i < 20; i++) {
-          vx.set(x[i]);
-          try {
-            double result = ind.execute_float(0, noargs);
-            error += Math.abs(result - y[i]);
-          } catch (ArithmeticException ex) {
-            System.out.println("x = " + x[i].floatValue());
-            System.out.println(ind);
-            throw ex;
-          }
-        }
-        if (error < 0.000001) {
-          error = 0.0d;
-        }
-        return error;
-      }
-
+    }
+    if (error < 0.000001) {
+      error = 0.0d;
+    }
+    return error;
+  }
 }
