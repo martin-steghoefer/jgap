f5af41a16d30867fffb509b232e3787c9299d045
Klaus Meffert <kontakt@doktor-meffert.de>
2007-05-24 18:56:33 +0000

    new classes
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3209 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/impl/job/IPopulationSplitter.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.impl.job;
+
+/**
+ * For splitting a single population into smaller chunks. Each chunk being meant
+ * to be operated on a single node in a distributed calculation (e.g. a grid).
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public interface IPopulationSplitter {
+
+  /**
+   * Splits a single population into smaller sub-populations.
+   *
+   * @param a_pop input population
+   * @return resulting chunks
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  Population[] split(Population a_pop;
+}
---
+++ src/org/jgap/impl/job/JobData.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.impl.job;
+
+import org.jgap.*;
+
+/**
+ * Holds all data needed to execute a JGAP job.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public abstract class JobData {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private Configuration m_config;
+
+  public JobData(Configuration a_config) {
+    m_config = a_config;
+  }
+
+  public Configuration getConfiguration() {
+    return m_config;
+  }
+}
