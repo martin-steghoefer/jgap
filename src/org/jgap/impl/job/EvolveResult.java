6f38b27ddefe2ab88f33372e9de5a443a9d7c206
Klaus Meffert <kontakt@doktor-meffert.de>
2007-06-02 16:30:28 +0000

    Javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3226 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/impl/job/EvolveResult.java
+++ src/org/jgap/impl/job/EvolveResult.java
@@ -1,12 +1,30 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package org.jgap.impl.job;
 
 import org.jgap.*;
 
-public class EvolveResult extends JobResult{
+/**
+ * Result of an evolution.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public class EvolveResult
+    extends JobResult {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
+
   private Population m_pop;
 
   public EvolveResult() {
-
   }
 
   public Population getPopulation() {
