f7d3321a0a31e244b09a9afae741a6da14bb5991
Klaus Meffert <kontakt@doktor-meffert.de>
2007-06-13 19:51:59 +0000

    Javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3276 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/impl/job/SimpleJobConsumer.java
+++ src/org/jgap/impl/job/SimpleJobConsumer.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package org.jgap.impl.job;
 
 import java.util.*;
@@ -5,7 +14,18 @@ import org.jgap.*;
 import org.jgap.impl.*;
 import org.jgap.distr.*;
 
+/**
+ * Sample implementation of a service that generates jobs, takes them, executes
+ * them, receives results from finished jobs and merges the results in order to
+ * start a new round of evolution.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
 public class SimpleJobConsumer {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   private List m_jobs = new Vector();
   private List m_results = new Vector();
