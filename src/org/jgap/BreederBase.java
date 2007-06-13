2e2b8773a8ba536a8e3a97eb56fd13b038676882
Klaus Meffert <kontakt@doktor-meffert.de>
2007-06-13 19:39:58 +0000

    base impl. of IBreeder
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3274 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/BreederBase.java
@@ -0,0 +1,128 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap;
+
+import java.util.*;
+
+/**
+ * Abstract base class for breeders.
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public abstract class BreederBase
+    implements IBreeder {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  private Configuration m_config;
+
+  public BreederBase(Configuration a_config) {
+    if (a_config == null) {
+      throw new IllegalStateException(
+          "The configuration object must be set prior to evolution.");
+    }
+    m_config = a_config;
+  }
+
+  /**
+   * Applies all NaturalSelectors registered with the Configuration.
+   *
+   * @param a_processBeforeGeneticOperators true apply NaturalSelectors
+   * applicable before GeneticOperators, false: apply the ones applicable
+   * after GeneticOperators
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  protected Population applyNaturalSelectors(Configuration a_config,
+      Population a_pop, boolean a_processBeforeGeneticOperators) {
+    /**@todo optionally use working pool*/
+    try {
+      // Process all natural selectors applicable before executing the
+      // genetic operators (reproduction, crossing over, mutation...).
+      // -------------------------------------------------------------
+      int selectorSize = a_config.getNaturalSelectorsSize(
+          a_processBeforeGeneticOperators);
+      if (selectorSize > 0) {
+        int m_population_size = a_config.getPopulationSize();
+        int m_single_selection_size;
+        Population new_population = new Population(a_config,
+            m_population_size);
+        NaturalSelector selector;
+        // Repopulate the population of chromosomes with those selected
+        // by the natural selector. Iterate over all natural selectors.
+        // ------------------------------------------------------------
+        for (int i = 0; i < selectorSize; i++) {
+          selector = a_config.getNaturalSelector(
+              a_processBeforeGeneticOperators, i);
+          if (i == selectorSize - 1 && i > 0) {
+            // Ensure the last NaturalSelector adds the remaining Chromosomes.
+            // ---------------------------------------------------------------
+            m_single_selection_size = m_population_size - a_pop.size();
+          }
+          else {
+            m_single_selection_size = m_population_size / selectorSize;
+          }
+          // Do selection of chromosomes.
+          // ----------------------------
+          /**@todo utilize jobs: integrate job into NaturalSelector!*/
+          selector.select(m_single_selection_size, a_pop, new_population);
+          // Clean up the natural selector.
+          // ------------------------------
+          selector.empty();
+        }
+//        Population result = new Population(a_config);
+//        result.addChromosomes(new_population);
+        return new_population;
+      }
+      else {
+        return a_pop;
+      }
+    } catch (InvalidConfigurationException iex) {
+      // This exception should never be reached
+      throw new IllegalStateException(iex.getMessage());
+    }
+  }
+
+  /**
+   * Applies all GeneticOperators registered with the Configuration.
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  protected void applyGeneticOperators(Configuration a_config, Population a_pop) {
+    List geneticOperators = a_config.getGeneticOperators();
+    Iterator operatorIterator = geneticOperators.iterator();
+    while (operatorIterator.hasNext()) {
+      GeneticOperator operator = (GeneticOperator) operatorIterator.next();
+      /**@todo utilize jobs: integrate job into GeneticOperator*/
+      operator.operate(a_pop, a_pop.getChromosomes());
+    }
+  }
+
+  /**
+   * @return the configuration set
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public Configuration getConfiguration() {
+    return m_config;
+  }
+
+  /**
+   * @return deep clone of this instance
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public abstract Object clone();
+}
