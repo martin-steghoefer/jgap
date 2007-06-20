1b959e8b2e776a1e5449c3cf53ee7076b7baf7a6
Klaus Meffert <kontakt@doktor-meffert.de>
2007-06-20 19:50:36 +0000

    new class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3307 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/IBusinessKey.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap;
+
+/**
+ * Interface for objects that offer a business key.
+ * @see http://www.hibernate.org/109.html for a discussion on business keys
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
+public interface IBusinessKey {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * @return business key
+   *
+   * @author Klaus Meffert
+   * @since 3.2
+   */
+  public String getBusinessKey();
+
+}
