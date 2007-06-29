c08de01afd4271a589a1bc9e44a574c728cbf93a
Klaus Meffert <kontakt@doktor-meffert.de>
2007-06-29 19:15:11 +0000

    *** empty log message ***
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3366 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/IGPInitStrategy.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licencing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+import java.io.*;
+
+/**
+ * Optional initializer for creating GP programs.
+ *
+ * @author Klaus Meffert
+ * @since 4.0
+ */
+public interface IGPInitStrategy extends Serializable {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Initializes a chromosome within a GP program before a random creation of
+   * the (rest of the) program is executed.
+   *
+   * @param a_chrom the chromosome within the GP program to create randomly
+   * @param a_chromNum index of the chromosome with the GP program
+   * @return the CommandGene to use as first node, or null if random selection
+   *
+  * @author Klaus Meffert
+  * @since 4.0
+  */
+  CommandGene init(IGPChromosome a_chrom, int a_chromNum);
+}
