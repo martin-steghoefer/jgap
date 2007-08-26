3e949dfb0b1c2538e82397af25e396b1736fc3d1
Klaus Meffert <kontakt@doktor-meffert.de>
2007-08-26 19:54:42 +0000

    new class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3432 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/IPopulationCreator.java
@@ -0,0 +1,47 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+import org.jgap.gp.impl.*;
+
+/**
+ *
+ * Interface for initializing the population within
+ * GPGenotype.randomInitializeGenotype.
+ *
+ * @author Klaus Meffert
+ * @since 3.2.2
+ */
+public interface IPopulationCreator {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Initializes the given population.
+   *
+   * @param a_pop GPPopulation
+   * @param a_types Class[]
+   * @param a_argTypes Class[][]
+   * @param a_nodeSets CommandGene[][]
+   * @param a_minDepths int[]
+   * @param a_maxDepths int[]
+   * @param a_maxNodes int
+   * @param a_fullModeAllowed boolean[]
+   * @throws Exception in case of any error
+   *
+   * @author Klaus Meffert
+   * @since 3.2.2
+   */
+  void initialize(GPPopulation a_pop, Class[] a_types,
+                  Class[][] a_argTypes, CommandGene[][] a_nodeSets,
+                  int[] a_minDepths,
+                  int[] a_maxDepths, int a_maxNodes,
+                  boolean[] a_fullModeAllowed) throws Exception ;
+}
