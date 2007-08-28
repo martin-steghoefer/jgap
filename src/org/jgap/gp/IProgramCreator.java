0f7203b1bbcfb715b3f04b87ca2f95b2e89c01cf
Klaus Meffert <kontakt@doktor-meffert.de>
2007-08-28 18:13:07 +0000

    new GP initializer class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3443 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/IProgramCreator.java
@@ -0,0 +1,51 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+import org.jgap.gp.impl.*;
+import org.jgap.*;
+
+/**
+ *
+ * Interface for creating a single program (IGPProgram).
+ *
+ * @author Klaus Meffert
+ * @since 3.2.2
+ */
+public interface IProgramCreator {
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * Initializes the given population.
+   *
+   * @param a_conf
+   * @param a_types Class[]
+   * @param a_argTypes Class[][]
+   * @param a_nodeSets CommandGene[][]
+   * @param a_minDepths int[]
+   * @param a_maxDepths int[]
+   * @param a_maxNodes int
+   * @param a_fullModeAllowed boolean[]
+   *
+   * @return the newly created program
+   *
+   * @throws InvalidConfigurationException in case of any error
+   *
+   * @author Klaus Meffert
+   * @since 3.2.2
+   */
+  IGPProgram create(GPConfiguration a_conf, Class[] a_types, Class[][] a_argTypes,
+                  CommandGene[][] a_nodeSets, int[] a_minDepths,
+                  int[] a_maxDepths, int a_maxNodes, int a_depth,
+                  boolean a_grow, int a_tries, boolean[] a_fullModeAllowed)
+      throws InvalidConfigurationException;
+}
+
---
+++ src/org/jgap/gp/impl/DefaultProgramCreator.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp.impl;
+
+import org.jgap.*;
+import org.jgap.gp.*;
+
+public class DefaultProgramCreator
+    implements IProgramCreator {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public IGPProgram create(GPConfiguration a_conf, Class[] a_types,
+                           Class[][] a_argTypes,
+                           CommandGene[][] a_nodeSets, int[] a_minDepths,
+                           int[] a_maxDepths, int a_maxNodes, int a_depth,
+                           boolean a_grow, int a_tries,
+                           boolean[] a_fullModeAllowed)
+      throws InvalidConfigurationException {
+    GPProgram program = new GPProgram(a_conf, a_types,
+                                      a_argTypes, a_nodeSets, a_minDepths,
+                                      a_maxDepths,
+                                      a_maxNodes);
+    program.growOrFull(a_depth, a_grow, a_maxNodes, a_fullModeAllowed,
+                       a_tries);
+    return program;
+  }
+}
