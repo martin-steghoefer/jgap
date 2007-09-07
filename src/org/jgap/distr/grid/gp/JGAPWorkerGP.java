774677961b03e84ae6574998e3edd8313781892a
Klaus Meffert <kontakt@doktor-meffert.de>
2007-09-07 15:28:39 +0000

    Javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3468 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/grid/gp/JGAPWorkerGP.java
+++ src/org/jgap/distr/grid/gp/JGAPWorkerGP.java
@@ -3,7 +3,7 @@
  *
  * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
- * For licencing information please see the file license.txt included with JGAP
+ * For licensing information please see the file license.txt included with JGAP
  * or have a look at the top of class org.jgap.Chromosome which representatively
  * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
@@ -25,7 +25,7 @@ import org.jgap.distr.grid.*;
 public class JGAPWorkerGP
     implements Worker {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   /**
    * Executes the evolution and returns the result.
--- src/org/jgap/distr/grid/util/GridKit.java
+++ src/org/jgap/distr/grid/util/GridKit.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package org.jgap.distr.grid.util;
 
 import org.jgap.util.*;
@@ -5,7 +14,17 @@ import java.io.*;
 import java.net.*;
 import org.jgap.distr.grid.request.*;
 
+/**
+ *
+ *
+ * @author Klaus Meffert
+ * @since 3.2
+ */
 public class GridKit {
+
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
+
   public static String ensureDirectory(String a_currentDir, String a_subDir,
                                        String a_descr)
       throws IOException {
