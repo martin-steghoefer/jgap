e4d5820797d0a10912f76579a35d74f844b88c60
Klaus Meffert <kontakt@doktor-meffert.de>
2007-10-19 16:51:58 +0000

    Javadoc, formatting
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3511 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/gp/impl/DefaultProgramCreator.java
+++ src/org/jgap/gp/impl/DefaultProgramCreator.java
@@ -12,10 +12,16 @@ package org.jgap.gp.impl;
 import org.jgap.*;
 import org.jgap.gp.*;
 
+/**
+ * Used by DefaultPopulationCreator to create GP programs.
+ *
+ * @author Klaus Meffert
+ * @since 3.2.x
+ */
 public class DefaultProgramCreator
     implements IProgramCreator {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   public IGPProgram create(GPConfiguration a_conf, int a_programIndex,
                            Class[] a_types, Class[][] a_argTypes,
@@ -24,10 +30,8 @@ public class DefaultProgramCreator
                            boolean a_grow, int a_tries,
                            boolean[] a_fullModeAllowed)
       throws InvalidConfigurationException {
-    GPProgram program = new GPProgram(a_conf, a_types,
-                                      a_argTypes, a_nodeSets, a_minDepths,
-                                      a_maxDepths,
-                                      a_maxNodes);
+    GPProgram program = new GPProgram(a_conf, a_types, a_argTypes, a_nodeSets,
+                                      a_minDepths, a_maxDepths, a_maxNodes);
     program.growOrFull(a_depth, a_grow, a_maxNodes, a_fullModeAllowed,
                        a_tries);
     return program;
