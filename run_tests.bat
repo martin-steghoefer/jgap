d4d0129f5b5b878c5619aba5e133394f195d3e5a
Klaus Meffert <kontakt@doktor-meffert.de>
2007-12-03 20:32:40 +0000

    added new libs
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3588 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- run_tests.bat
+++ run_tests.bat
@@ -1,2 +1,2 @@
-java -cp ".;lib\junit3.8.1.jar;lib\junit-addons-1.4.jar;lib\log4j.jar;lib\commons-lang-2.1.jar;lib\commons-codec-1.3.jar;jgap-tests.jar;jgap.jar" junit.textui.TestRunner org.jgap.AllTests
+java -cp ".;lib\junit3.8.1.jar;lib\junit-addons-1.4.jar;lib\log4j.jar;lib\commons-lang-2.1.jar;lib\commons-codec-1.3.jar;lib\trove-2.0.2.jar;lib\xstream-1.2.2.jar;lib\xpp3_min-1.1.3.4.O.jar;jgap-tests.jar;jgap.jar" junit.textui.TestRunner org.jgap.AllTests
 pause
