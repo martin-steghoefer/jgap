00ed5090f6c9a6ef250d79c2673c83a35a39b933
Klaus Meffert <kontakt@doktor-meffert.de>
2008-04-07 19:50:57 +0000

    Javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3674 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/xml/XMLDocumentBuilder.java
+++ src/org/jgap/xml/XMLDocumentBuilder.java
@@ -3,7 +3,7 @@
  *
  * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
- * For licencing information please see the file license.txt included with JGAP
+ * For licensing information please see the file license.txt included with JGAP
  * or have a look at the top of class org.jgap.Chromosome which representatively
  * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
@@ -23,7 +23,7 @@ import javax.xml.parsers.*;
 public class XMLDocumentBuilder
     extends DocumentBuilderBase {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.8 $";
+  private final static String CVS_REVISION = "$Revision: 1.9 $";
 
   protected void setAttribute(final Object a_xmlElement, final String a_key,
                               final String a_value) {
