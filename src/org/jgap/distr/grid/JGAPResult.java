e4e4ee4d56473a16b18d0cac512b083db788f7a7
Klaus Meffert <kontakt@doktor-meffert.de>
2008-04-28 09:36:12 +0000

    Javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3724 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/grid/JGAPResult.java
+++ src/org/jgap/distr/grid/JGAPResult.java
@@ -3,7 +3,7 @@
  *
  * JGAP offers a dual license model containing the LGPL as well as the MPL.
  *
- * For licencing information please see the file license.txt included with JGAP
+ * For licensing information please see the file license.txt included with JGAP
  * or have a look at the top of class org.jgap.Chromosome which representatively
  * includes the JGAP license policy applicable for any file delivered with JGAP.
  */
@@ -21,7 +21,7 @@ import org.jgap.*;
 public class JGAPResult
     extends WorkResult {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   private IChromosome m_fittest;
   private Population m_pop;
