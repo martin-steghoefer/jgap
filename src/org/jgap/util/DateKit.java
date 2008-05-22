f289647ed71aa41c70dcfa8ff5d7dee9eedf2510
Klaus Meffert <kontakt@doktor-meffert.de>
2008-05-22 19:00:52 +0000

    added dateToString
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3816 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/util/DateKit.java
+++ src/org/jgap/util/DateKit.java
@@ -20,12 +20,16 @@ import java.util.*;
  */
 public class DateKit {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.3 $";
+  private final static String CVS_REVISION = "$Revision: 1.4 $";
 
   private static Calendar m_cal = Calendar.getInstance();
 
-  private static SimpleDateFormat m_sdfNow = new SimpleDateFormat(
-      "yyyyMMddHHmmssSSS");
+  public final static String DATEFORMAT_FULL_0 = "yyyyMMddHHmmssSSS";
+  public final static String DATEFORMAT_FULL_1 = "yyyy/MM/dd HH:mm:ss:SSS";
+  public final static String DATEFORMAT_NORMAL = "yyyy/MM/dd";
+
+  private static SimpleDateFormat m_sdfNow = new SimpleDateFormat(DATEFORMAT_FULL_0);
+
 
   private static SimpleDateFormat m_sdfToday = new SimpleDateFormat("yyyyMMdd");
 
@@ -47,6 +51,20 @@ public class DateKit {
    */
   public static String getTodayAsString() {
     return m_sdfToday.format(m_cal.getTime());
+  }
+
+  /**
+   * @param a_date the date to be returned in a specific format
+   * @param a_dateFormat the desired format of the date
+   *
+   * @return date in given format
+   *
+   * @author Klaus Meffert
+   * @since 3.3.4
+   */
+  public static String dateToString(Date a_date, String a_dateFormat) {
+    SimpleDateFormat sdf = new SimpleDateFormat(a_dateFormat);
+    return sdf.format(a_date);
   }
 
   /**
