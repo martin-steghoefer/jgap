50e45e72bbb5607ac197c361b0df5f007c6666d3
Klaus Meffert <kontakt@doktor-meffert.de>
2008-05-26 19:57:05 +0000

    new class
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3832 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ src/org/jgap/gp/GPPopulationConverter.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package org.jgap.gp;
+
+import org.jgap.gp.impl.*;
+import org.jgap.util.*;
+
+import com.thoughtworks.xstream.*;
+import com.thoughtworks.xstream.converters.*;
+
+/**
+ * Converts a GPPopulation object.
+ *
+ * @author Klaus Meffert
+ * @since 4.0
+ */
+public class GPPopulationConverter
+    extends XStreamPassThruConverter implements Converter {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  public GPPopulationConverter(XStream a_xstream, GPConfiguration a_conf) {
+    super(a_xstream, GPPopulation.class);
+    setConfiguration(a_conf);
+  }
+
+  public Object doUnmarshal(UnmarshallingContext context)
+      throws Exception {
+    GPPopulation pop = new GPPopulation();
+    pop = (GPPopulation)unmarshalDefault(context, pop);
+    setConfiguration(pop, "m_config");
+    pop.sortByFitness();
+    return pop;
+  }
+}
