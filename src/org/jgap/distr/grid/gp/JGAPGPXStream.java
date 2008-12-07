1d01516981c6c8753e4b33cf3ad3a7e095c2e790
Klaus Meffert <kontakt@doktor-meffert.de>
2008-12-07 15:45:27 +0000

    Javadoc
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3896 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/grid/gp/JGAPGPXStream.java
+++ src/org/jgap/distr/grid/gp/JGAPGPXStream.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package org.jgap.distr.grid.gp;
 
 import org.homedns.dade.jcgrid.message.*;
@@ -11,19 +20,15 @@ import org.jgap.impl.*;
 import com.thoughtworks.xstream.*;
 
 /**
- * <p> </p>
- *
- * <p> </p>
- *
- * <p> </p>
- *
- * <p> </p>
+ * Specialized version of XStream for JGAP.
  *
  * @author Klaus Meffert
- * @since 4.0
+ * @since 3.3.4
  */
 public class JGAPGPXStream
     extends XStream {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
   public JGAPGPXStream() {
     super();
     init(this);
