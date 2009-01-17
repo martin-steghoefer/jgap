76d52193e775418d6ae517811a932cede4b3df8b
Klaus Meffert <kontakt@doktor-meffert.de>
2009-01-17 15:32:25 +0000

    Javadoc, code opt.
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3955 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/src/examples/gp/monalisa/core/DrawingGPConfiguration.java
+++ examples/src/examples/gp/monalisa/core/DrawingGPConfiguration.java
@@ -17,33 +17,34 @@ import org.jgap.gp.impl.*;
  * Encapsulates the settings of the genetic algorithm.
  *
  * @author Yann N. Dauphin
+ * @author Klaus Meffert
  * @since 3.4
  */
 public class DrawingGPConfiguration
     extends GPConfiguration {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   protected BufferedImage m_target = null;
 
   public DrawingGPConfiguration(BufferedImage a_target)
       throws InvalidConfigurationException {
-    this.m_target = a_target;
-    this.setSelectionMethod(new TournamentSelector(2));
-    this.setGPFitnessEvaluator(new DeltaGPFitnessEvaluator());
-    this.setFitnessFunction(new LMSFitnessFunction(this));
+    m_target = a_target;
+    setSelectionMethod(new TournamentSelector(2));
+    setGPFitnessEvaluator(new DeltaGPFitnessEvaluator());
+    setFitnessFunction(new LMSFitnessFunction(this));
     setNodeValidator(new NodeValidator());
-    this.setPopulationSize(5);
-    this.setStrictProgramCreation(false);
+    setPopulationSize(5);
+    setStrictProgramCreation(false);
     setProgramCreationMaxTries( -1);
     setMinInitDepth(3);
     setMaxInitDepth(15);
     setInitStrategy(new InitStrategy());
-    this.setCrossoverProb(0.3f);
-    this.setReproductionProb(0.7f);
-    this.setNewChromsPercent(0.1f);
-    this.setMutationProb(0.8f);
-    this.setMaxCrossoverDepth(100);
+    setCrossoverProb(0.3f);
+    setReproductionProb(0.7f);
+    setNewChromsPercent(0.1f);
+    setMutationProb(0.8f);
+    setMaxCrossoverDepth(100);
   }
 
   /**
