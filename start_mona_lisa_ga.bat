924cce8e214dbcf608969d854399f15017af4304
Klaus Meffert <kontakt@doktor-meffert.de>
2009-01-21 19:04:36 +0000

    starts the GA mona lisa example
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3992 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ start_mona_lisa_ga.bat
@@ -0,0 +1,2 @@
+java -cp ".;jgap.jar;jgap-examples.jar;lib/jcgrid.jar;lib/log4j.jar;lib/commons-cli-1.0.jar;lib/commons-lang-2.1.jar;lib/appframework-1.0.3.jar;lib/jfreechart-1.0.11.jar;lib/jcommon-1.0.14.jar;lib/swing-layout-1.0.3.jar;lib/swing-worker-1.1.jar" examples.monalisa.gui.GeneticDrawingApp
+pause
