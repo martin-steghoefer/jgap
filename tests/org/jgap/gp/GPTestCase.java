c29199a7a2f840c2f50bb34768363077f5eb10f7
Klaus Meffert <kontakt@doktor-meffert.de>
2009-01-21 17:59:44 +0000

    added constant GP commands
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@3984 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- tests/org/jgap/gp/GPTestCase.java
+++ tests/org/jgap/gp/GPTestCase.java
@@ -29,13 +29,15 @@ public abstract class GPTestCase
   protected RandomGeneratorForTesting rn;
 
   protected Constant CMD_CONST0, CMD_CONST1, CMD_CONST2, CMD_CONST3, CMD_CONST4;
+  protected Terminal CMD_TERM0,CMD_TERM1,CMD_TERM2;
 
   protected Add CMD_ADD;
 
   protected ForLoop CMD_FOR;
   protected ForXLoop CMD_FORX;
 
-  protected SubProgram CMD_SUB_V_I, CMD_SUB_V_V_V, CMD_SUB_V_V;
+  protected SubProgram CMD_SUB_I_I, CMD_SUB_I_I2, CMD_SUB_I_IM, CMD_SUB_I_I2M,
+          CMD_SUB_I_I_I, CMD_SUB_V_I, CMD_SUB_V_V_V, CMD_SUB_V_V;
 
   protected NOP CMD_NOP;
 
@@ -62,9 +64,27 @@ public abstract class GPTestCase
                                 new Integer(3));
       CMD_CONST4 = new Constant(m_gpconf, CommandGene.IntegerClass,
                                 new Integer(4));
+      CMD_TERM0 = new Terminal(m_gpconf, CommandGene.IntegerClass);
+      CMD_TERM1 = new Terminal(m_gpconf, CommandGene.IntegerClass);
+      CMD_TERM2 = new Terminal(m_gpconf, CommandGene.IntegerClass);
       CMD_NOP = new NOP(m_gpconf);
       CMD_FOR = new ForLoop(m_gpconf, CommandGene.IntegerClass, 15);
       CMD_FORX = new ForXLoop(m_gpconf, CommandGene.IntegerClass);
+      CMD_SUB_I_I = new SubProgram(m_gpconf,
+                                   new Class[] {CommandGene.IntegerClass,
+                                   CommandGene.IntegerClass});
+      CMD_SUB_I_IM = new SubProgram(m_gpconf,
+                                   new Class[] {CommandGene.IntegerClass,
+                                   CommandGene.IntegerClass}, true);
+      CMD_SUB_I_I2 = new SubProgram(m_gpconf,
+                                   new Class[] {CommandGene.IntegerClass,
+                                   CommandGene.IntegerClass});
+      CMD_SUB_I_I2M = new SubProgram(m_gpconf,
+                                   new Class[] {CommandGene.IntegerClass,
+                                   CommandGene.IntegerClass}, true);
+      CMD_SUB_I_I_I = new SubProgram(m_gpconf,
+                                   new Class[] {CommandGene.IntegerClass,
+                                   CommandGene.IntegerClass,CommandGene.IntegerClass});
       CMD_SUB_V_I = new SubProgram(m_gpconf,
                                    new Class[] {CommandGene.VoidClass,
                                    CommandGene.IntegerClass});
@@ -84,5 +104,5 @@ public abstract class GPTestCase
 
 
   /** String containing the CVS revision. Read out via reflection!*/
-  private static final String CVS_REVISION = "$Revision: 1.6 $";
+  private static final String CVS_REVISION = "$Revision: 1.7 $";
 }
