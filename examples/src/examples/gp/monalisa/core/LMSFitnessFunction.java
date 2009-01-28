47dbba29699875793c92a666b9242e453d69bbea
Klaus Meffert <kontakt@doktor-meffert.de>
2009-01-28 17:21:05 +0000

    enhanced evaluation of fitness
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@4033 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/src/examples/gp/monalisa/core/LMSFitnessFunction.java
+++ examples/src/examples/gp/monalisa/core/LMSFitnessFunction.java
@@ -10,22 +10,22 @@
 package examples.gp.monalisa.core;
 
 import java.util.logging.*;
-
 import java.awt.image.*;
-
 import org.jgap.gp.*;
+import org.jgap.gp.impl.*;
 
 /**
  * Computes the fitness of a program as the Least-Mean-Sqare distance between
  * the image it generates and the target image.
  *
  * @author Yann N. Dauphin
+ * @author Klaus Meffert (enhancement)
  * @since 3.4
  */
 public class LMSFitnessFunction
     extends GPFitnessFunction {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.3 $";
+  private final static String CVS_REVISION = "$Revision: 1.4 $";
 
   private final DrawingGPConfiguration m_conf;
 
@@ -53,6 +53,8 @@ public class LMSFitnessFunction
   @Override
   protected double evaluate(final IGPProgram a_subject) {
     BufferedImage generated = programRunner.run(a_subject);
+    int numPoints = programRunner.numPoints;
+    int numPolygons = programRunner.numPolygons;
     final int[] generatedPixels = new int[generated.getWidth() *
         generated.getHeight()];
     PixelGrabber pg = new PixelGrabber(generated, 0, 0, generated.getWidth(),
@@ -68,12 +70,45 @@ public class LMSFitnessFunction
     for (int i = 0; i < generatedPixels.length && i < targetPixels.length; i++) {
       int c1 = targetPixels[i];
       int c2 = generatedPixels[i];
-      int r = ( (c1 >> 16) & 0xff) - ( (c2 >> 16) & 0xff);
-      int g = ( (c1 >> 8) & 0xff) - ( (c2 >> 8) & 0xff);
-      int b = (c1 & 0xff) - (c2 & 0xff);
-      sum += r * r + g * g + b * b;
+      int r, g, b;
+      if (c2 == -1) {
+//        r = 300;
+//        g = 300;
+//        b = 300;
+//        sum += r * r + g * g + b * b;
+        sum += 270000;
+      }
+      else {
+        r = ( (c1 >> 16) & 0xff) - ( (c2 >> 16) & 0xff);
+        g = ( (c1 >> 8) & 0xff) - ( (c2 >> 8) & 0xff);
+        b = (c1 & 0xff) - (c2 & 0xff);
+        sum += r * r + g * g + b * b;
+      }
     }
-    /**@todo consider number of polygons necessary*/
-    return Math.sqrt(sum);
+    double error = Math.sqrt(sum);
+    // Consider malus for number of used points and polygons.
+    // ------------------------------------------------------
+    GPProgram program = (GPProgram) a_subject;
+    ApplicationData data = (ApplicationData)program.getApplicationData();
+    data.numPoints = numPoints;
+    data.numPolygons = numPolygons;
+    double complexityMalus;
+    if (error > 250000) {
+      complexityMalus = 0;
+    }
+    else {
+      /**
+       * 5 Bytes per Polygon
+       *   + Delimiter: 1 Byte (-1 for first Polygon)
+       *   + Color (R, G, B, Alpha): 4 Bytes (simplified)
+       * 2 Bytes per Point:
+       *   + X: 1 Byte
+       *   + Y: 1 Byte
+       */
+      complexityMalus = numPoints * 2 + numPolygons * 5 - 1;
+    }
+    // Consider number of polygons necessary.
+    // --------------------------------------
+    return error + complexityMalus;
   }
 }
