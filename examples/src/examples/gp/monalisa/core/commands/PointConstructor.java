9c6d32369f8c79d1bb91e87a3b6ab664baaa844f
Klaus Meffert <kontakt@doktor-meffert.de>
2009-01-28 17:23:09 +0000

    introduced clone(); formatting
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@4036 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- examples/src/examples/gp/monalisa/core/commands/PointConstructor.java
+++ examples/src/examples/gp/monalisa/core/commands/PointConstructor.java
@@ -14,26 +14,25 @@ import java.awt.*;
 import org.jgap.*;
 import org.jgap.gp.*;
 import org.jgap.gp.impl.*;
+import org.jgap.util.*;
 
 import examples.gp.monalisa.core.DrawingProblem.*;
 
 /**
- * A point, consisting of X and Y coordinates.
+ * A point, consisting of an X and a Y coordinate.
  *
  * @author Yann N. Dauphin
  * @since 3.4
  */
 public class PointConstructor
-    extends CommandGene {
+    extends CommandGene implements ICloneable {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.2 $";
+  private final static String CVS_REVISION = "$Revision: 1.3 $";
 
   public PointConstructor(GPConfiguration a_conf)
       throws InvalidConfigurationException {
-    super(a_conf, 2, Point.class, 0,
-          new int[] {
-      TerminalType.WIDTH.intValue(),
-      TerminalType.HEIGHT.intValue()
+    super(a_conf, 2, Point.class, 0, new int[] {
+      TerminalType.WIDTH.intValue(), TerminalType.HEIGHT.intValue()
     });
   }
 
@@ -53,5 +52,22 @@ public class PointConstructor
   @Override
   public String toString() {
     return "new Point(&1, &2)";
+  }
+
+  /**
+   * Clones the object. Simple and straight forward implementation here.
+   *
+   * @return cloned instance of this object
+   *
+   * @author Klaus Meffert
+   * @since 3.4.1
+   */
+  public Object clone() {
+    try {
+      PointConstructor result = new PointConstructor(getGPConfiguration());
+      return result;
+    } catch (Throwable t) {
+      throw new CloneException(t);
+    }
   }
 }
