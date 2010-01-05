c585b9c9e2e1f590e432fa468254f39f60e9d6be
Klaus Meffert <kontakt@doktor-meffert.de>
2010-01-05 15:01:20 +0000

    added sample implementation of method event
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@4213 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/audit/FitnessImprovementMonitor.java
+++ src/org/jgap/audit/FitnessImprovementMonitor.java
@@ -15,6 +15,7 @@ import org.jgap.*;
 /**
  * Monitors the evolution and stops it if evolution does not make a progress
  * as desired.
+ * For usage of this monitor class, see class examples.audit.EvolutionMonitorExample.
  *
  * @author Klaus Meffert
  * @since 3.4.4
@@ -22,7 +23,7 @@ import org.jgap.*;
 public class FitnessImprovementMonitor
     implements IEvolutionMonitor {
   /** String containing the CVS revision. Read out via reflection!*/
-  private final static String CVS_REVISION = "$Revision: 1.1 $";
+  private final static String CVS_REVISION = "$Revision: 1.2 $";
 
   private int m_initialWaitSeconds;
 
@@ -63,8 +64,8 @@ public class FitnessImprovementMonitor
    * Called after another evolution cycle has been executed.
    *
    * @param a_pop the currently evolved population
-   * @param a_messages the monitor can append messages here to indicate why
-   * it asks evolution to stop
+   * @param a_messages the monitor can append messages to indicate why it asks
+   * evolution to stop
    * @return true: continue with the evolution; false: stop evolution
    *
    * @author Klaus Meffert
@@ -103,7 +104,7 @@ public class FitnessImprovementMonitor
               m_improvedFitnessExpected) {
             // Bad luck, not enough progress.
             // ------------------------------
-            a_messages.add("Not enough progress was made");
+            a_messages.add("Not enough progress was made after initial delay");
             return false;
           }
           else {
@@ -138,5 +139,105 @@ public class FitnessImprovementMonitor
    */
   public void start(Configuration a_config) {
     m_startMillis = System.currentTimeMillis();
+  }
+
+  /**
+   * Called whenever it's worth monitoring.
+   *
+   * @param a_monitorEvent see constants at top of class IEvolutionMonitor
+   * @param a_evolutionNo the index of the evolution round (1, 2, ...)
+   * @param a_information event-specific information
+   *
+   * @author Klaus Meffert
+   * @since 3.5
+   */
+  public void event(String a_monitorEvent, int a_evolutionNo,
+                    Object[] a_information) {
+    // Just a sample implemetation here to show how to react to the specific
+    // monitor events that are supported.
+    // ---------------------------------------------------------------------
+    if (a_monitorEvent == null) {
+      // Should never happen.
+      // --------------------
+      return;
+    }
+    if(a_information == null) {
+      return;
+    }
+    // The events are queried in the chronological order they do appear.
+    // -----------------------------------------------------------------
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_REMOVE_CHROMOSOME)) {
+      Population pop = (Population)a_information[0];
+      Integer chromosomeIndex = (Integer)a_information[1];
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_BEFORE_UPDATE_CHROMOSOMES1)) {
+      Population pop = (Population)a_information[0];
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_AFTER_UPDATE_CHROMOSOMES1)) {
+      Population pop = (Population)a_information[0];
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_BEFORE_SELECT)) {
+      NaturalSelector selector = (NaturalSelector)a_information[0];
+      Population pop = (Population)a_information[1];
+      int selectionSize = (Integer)a_information[2];
+      boolean a_processBeforeGeneticOperators = (Boolean) a_information[3];
+      /*
+       * a_processBeforeGeneticOperators = true:
+       *    called after MONITOR_EVENT_AFTER_UPDATE_CHROMOSOMES1
+       * a_processBeforeGeneticOperators = false:
+       *    called after MONITOR_EVENT_AFTER_UPDATE_CHROMOSOMES2
+       */
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_AFTER_SELECT)) {
+      NaturalSelector selector = (NaturalSelector)a_information[0];
+      Population pop = (Population)a_information[1];
+      Population newPop = (Population)a_information[2];
+      int selectionSize = (Integer)a_information[3];
+      boolean a_processBeforeGeneticOperators = (Boolean)a_information[4];
+      /*
+       * a_processBeforeGeneticOperators = true:
+       *    called after MONITOR_EVENT_AFTER_UPDATE_CHROMOSOMES1
+       * a_processBeforeGeneticOperators = false:
+       *    called after MONITOR_EVENT_AFTER_UPDATE_CHROMOSOMES2
+       */
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_BEFORE_OPERATE)) {
+      GeneticOperator operator = (GeneticOperator)a_information[0];
+      Population pop = (Population)a_information[1];
+      List<IChromosome> chromosomes = (List<IChromosome>)a_information[2];
+    }
+
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_AFTER_OPERATE)) {
+      GeneticOperator operator = (GeneticOperator)a_information[0];
+      Population pop = (Population)a_information[1];
+      List<IChromosome> chromosomes = (List<IChromosome>)a_information[2];
+    }
+
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_BEFORE_BULK_EVAL)) {
+      BulkFitnessFunction bulkFitnessFunction = (BulkFitnessFunction)a_information[0];
+      Population pop = (Population)a_information[1];
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_AFTER_BULK_EVAL)) {
+      BulkFitnessFunction bulkFitnessFunction = (BulkFitnessFunction)a_information[0];
+      Population pop = (Population)a_information[1];
+    }
+
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_BEFORE_UPDATE_CHROMOSOMES2)) {
+      Population pop = (Population)a_information[0];
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_AFTER_UPDATE_CHROMOSOMES2)) {
+      Population pop = (Population)a_information[0];
+    }
+
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_BEFORE_ADD_CHROMOSOME)) {
+      Population pop = (Population)a_information[0];
+      IChromosome newChromosome = (IChromosome)a_information[1];
+    }
+    if (a_monitorEvent.equals(IEvolutionMonitor.MONITOR_EVENT_READD_FITTEST)) {
+      Population pop = (Population)a_information[0];
+      IChromosome fittest = (IChromosome)a_information[1];
+    }
+
+
   }
 }
