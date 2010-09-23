950b527b247533b87a296ec9b6045ff81c8459a9
Klaus Meffert <kontakt@doktor-meffert.de>
2010-09-23 14:40:24 +0000

    adapted to apache cli version 1.2
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@4301 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- start_grid_server.bat
+++ start_grid_server.bat
@@ -1,2 +1,2 @@
-java -cp ".;jgap.jar;jgap-examples.jar;lib/jcgrid.jar;lib/log4j.jar;lib/commons-cli-1.0.jar;lib/commons-lang-2.1.jar" org.jgap.distr.grid.JGAPServer -server org.jgap.distr.grid.JGAPServer
+java -cp ".;jgap.jar;jgap-examples.jar;lib/jcgrid.jar;lib/log4j.jar;lib/commons-cli-1.2.jar;lib/commons-lang-2.1.jar" org.jgap.distr.grid.JGAPServer -server org.jgap.distr.grid.JGAPServer
 pause
