e947a238a2de7e69afb301a4c8f6dc73885b98ee
Klaus Meffert <kontakt@doktor-meffert.de>
2010-09-23 14:40:10 +0000

    new batch file
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@4300 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ start_grid_worker_gp.bat
@@ -0,0 +1,2 @@
+java -cp ".;jgap.jar;jgap-examples.jar;lib/jcgrid.jar;lib/log4j.jar;lib/commons-cli-1.2.jar;lib/commons-lang-2.1.jar" org.jgap.distr.grid.gp.JGAPWorkerGP -n RemoteSession -c 1
+pause
