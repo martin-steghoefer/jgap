023e862f167a2067c76fb45d4f01fd191982c582
Klaus Meffert <kontakt@doktor-meffert.de>
2011-01-09 10:34:35 +0000

    Javadoc; formatting
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@4380 3924feb9-9058-4ba4-b128-58a6af0bd8c6

--- src/org/jgap/distr/grid/gp/IGPPopulationInitializer.java
+++ src/org/jgap/distr/grid/gp/IGPPopulationInitializer.java
@@ -1,3 +1,12 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
 package org.jgap.distr.grid.gp;
 
 import org.jgap.gp.impl.GPGenotype;
@@ -9,15 +18,20 @@ import org.jgap.gp.CommandGene;
  * Allows to initialize a GP population right when creating a GPGenotype.
  * Is called within constructor of GPGenotype.
  *
- * See examples.grid.mathProblemDistrubuted for an example
- *
+ * See examples.grid.mathProblemDistrubuted for an example.
  *
  * @author Klaus Meffert
  * @since 3.6
  */
 public interface IGPPopulationInitializer {
-  void setUp(GPConfiguration a_conf, Class[] a_types,Class[][] argTypes,
-      CommandGene[][] a_nodeSets, int a_maxNodes, boolean a_verboseOutput) throws Exception;
-  GPGenotype execute() throws InvalidConfigurationException;
+  /** String containing the CVS revision. Read out via reflection!*/
+  final static String CVS_REVISION = "$Revision: 1.2 $";
 
+  void setUp(GPConfiguration a_conf, Class[] a_types, Class[][] argTypes,
+             CommandGene[][] a_nodeSets, int a_maxNodes,
+             boolean a_verboseOutput)
+      throws Exception;
+
+  GPGenotype execute()
+      throws InvalidConfigurationException;
 }
