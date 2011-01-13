79cff4bcab0df8f893eaaf87c5ad8bde94cdd181
Klaus Meffert <kontakt@doktor-meffert.de>
2011-01-13 09:47:12 +0000

    new example for MapGene usage
    git-svn-id: file:///home/mast/dev/jgap/svn/trunk@4393 3924feb9-9058-4ba4-b128-58a6af0bd8c6

---
+++ examples/src/examples/MapGeneExample.java
@@ -0,0 +1,222 @@
+/*
+ * This file is part of JGAP.
+ *
+ * JGAP offers a dual license model containing the LGPL as well as the MPL.
+ *
+ * For licensing information please see the file license.txt included with JGAP
+ * or have a look at the top of class org.jgap.Chromosome which representatively
+ * includes the JGAP license policy applicable for any file delivered with JGAP.
+ */
+package examples;
+
+import java.util.*;
+
+import org.jgap.*;
+import org.jgap.impl.*;
+
+/**
+ * This class provides an implementation of the classic "Make change" problem
+ * using a genetic algorithm. It is equivalent to the example in class
+ * MinimizingMakeChange in the same package, except that it uses a MapGene.
+ *
+ * A MapGene allows to specify discrete values for possible solutions. Instead,
+ * an IntegerGene (used in the original example) uses a range of values.
+ *
+ * @author Klaus Meffert
+ * @since 3.6
+ */
+public class MapGeneExample {
+  /** String containing the CVS revision. Read out via reflection!*/
+  private final static String CVS_REVISION = "$Revision: 1.1 $";
+
+  /**
+   * The total number of times we'll let the population evolve.
+   */
+  private static final int MAX_ALLOWED_EVOLUTIONS = 200;
+
+  /**
+   * Executes the genetic algorithm to determine the minimum number of
+   * coins necessary to make up the given target amount of change. The
+   * solution will then be written to System.out.
+   *
+   * @param a_targetChangeAmount the target amount of change for which this
+   * method is attempting to produce the minimum number of coins
+   *
+   * @throws Exception
+   *
+   * @author Neil Rotstan
+   * @author Klaus Meffert
+   * @since 1.0
+   */
+  public static void makeChangeForAmount(int a_targetChangeAmount)
+      throws Exception {
+    // Start with a DefaultConfiguration, which comes setup with the
+    // most common settings.
+    // -------------------------------------------------------------
+    Configuration conf = new DefaultConfiguration();
+    // Care that the fittest individual of the current population is
+    // always taken to the next generation.
+    // Consider: With that, the pop. size may exceed its original
+    // size by one sometimes!
+    // -------------------------------------------------------------
+    conf.setPreservFittestIndividual(true);
+    // Set the fitness function we want to use, which is our
+    // MinimizingMakeChangeFitnessFunction. We construct it with
+    // the target amount of change passed in to this method.
+    // ---------------------------------------------------------
+    FitnessFunction myFunc =
+        new MinimizingMakeChangeFitnessFunction(a_targetChangeAmount);
+    conf.setFitnessFunction(myFunc);
+    // Now we need to tell the Configuration object how we want our
+    // Chromosomes to be setup. We do that by actually creating a
+    // sample Chromosome and then setting it on the Configuration
+    // object. As mentioned earlier, we want our Chromosomes to each
+    // have four genes, one for each of the coin types. We want the
+    // values (alleles) of those genes to be integers, which represent
+    // how many coins of that type we have. We therefore use the
+    // IntegerGene class to represent each of the genes. That class
+    // also lets us specify a lower and upper bound, which we set
+    // to sensible values for each coin type.
+    // --------------------------------------------------------------
+    Gene[] sampleGenes = new Gene[4];
+    Map alleles = new Hashtable();
+    alleles = new Hashtable();
+    alleles.put("v1", new Integer(1));
+    alleles.put("v2",new Integer(2));
+    alleles.put("v3",new Integer(3));
+    alleles.put("v4",new Integer(4));
+    sampleGenes[0] = new MapGene(conf, alleles); // Quarters
+    alleles = new Hashtable();
+    alleles.put("v3",new Integer(3));
+    alleles.put("v4", new Integer(4));
+    sampleGenes[1] = new MapGene(conf, alleles); // Dimes
+    alleles = new Hashtable();
+    alleles.put("v0", new Integer(0));
+    alleles.put("v1", new Integer(1));
+    sampleGenes[2] = new MapGene(conf, alleles); // Nickels
+    alleles = new Hashtable();
+    alleles.put("v3", new Integer(3));
+    alleles.put("v4", new Integer(4));
+    sampleGenes[3] = new MapGene(conf, alleles); // Pennies
+
+    IChromosome sampleChromosome = new Chromosome(conf, sampleGenes);
+    conf.setSampleChromosome(sampleChromosome);
+    // Finally, we need to tell the Configuration object how many
+    // Chromosomes we want in our population. The more Chromosomes,
+    // the larger number of potential solutions (which is good for
+    // finding the answer), but the longer it will take to evolve
+    // the population (which could be seen as bad).
+    // ------------------------------------------------------------
+    conf.setPopulationSize(80);
+
+    // Create random initial population of Chromosomes.
+    // ------------------------------------------------
+    Genotype population = Genotype.randomInitialGenotype(conf);
+    // Now we initialize the population randomly, anyway (as an example only)!
+    // If you want to load previous results from file, remove the next line!
+    // -----------------------------------------------------------------------
+    population = Genotype.randomInitialGenotype(conf);
+    // Evolve the population. Since we don't know what the best answer
+    // is going to be, we just evolve the max number of times.
+    // ---------------------------------------------------------------
+    long startTime = System.currentTimeMillis();
+    for (int i = 0; i < MAX_ALLOWED_EVOLUTIONS; i++) {
+      if (!uniqueChromosomes(population.getPopulation())) {
+        throw new RuntimeException("Invalid state in generation "+i);
+      }
+      population.evolve();
+    }
+    long endTime = System.currentTimeMillis();
+    System.out.println("Total evolution time: " + ( endTime - startTime)
+                       + " ms");
+    // Display the best solution we found.
+    // -----------------------------------
+    IChromosome bestSolutionSoFar = population.getFittestChromosome();
+    System.out.println("The best solution has a fitness value of " +
+                       bestSolutionSoFar.getFitnessValue());
+    System.out.println("It contains the following: ");
+    System.out.println("\t" +
+                       MinimizingMakeChangeFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 0) + " quarters.");
+    System.out.println("\t" +
+                       MinimizingMakeChangeFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 1) + " dimes.");
+    System.out.println("\t" +
+                       MinimizingMakeChangeFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 2) + " nickels.");
+    System.out.println("\t" +
+                       MinimizingMakeChangeFitnessFunction.
+                       getNumberOfCoinsAtGene(
+        bestSolutionSoFar, 3) + " pennies.");
+    System.out.println("For a total of " +
+                       MinimizingMakeChangeFitnessFunction.amountOfChange(
+        bestSolutionSoFar) + " cents in " +
+                       MinimizingMakeChangeFitnessFunction.
+                       getTotalNumberOfCoins(
+        bestSolutionSoFar) + " coins.");
+  }
+
+  /**
+   * Main method. A single command-line argument is expected, which is the
+   * amount of change to create (in other words, 75 would be equal to 75
+   * cents).
+   *
+   * @param args amount of change in cents to create
+   * @throws Exception
+   *
+   * @author Neil Rotstan
+   * @author Klaus Meffert
+   * @since 1.0
+   */
+  public static void main(String[] args)
+      throws Exception {
+    if (args.length < 1) {
+      System.out.println("Syntax: MinimizingMakeChange <amount>");
+    }
+    else {
+      int amount = 0;
+      try {
+        amount = Integer.parseInt(args[0]);
+      }
+      catch (NumberFormatException e) {
+        System.out.println(
+            "The <amount> argument must be a valid integer value");
+        System.exit(1);
+      }
+      if (amount < 1 ||
+          amount >= MinimizingMakeChangeFitnessFunction.MAX_BOUND) {
+        System.out.println("The <amount> argument must be between 1 and "
+                           +
+                           (MinimizingMakeChangeFitnessFunction.MAX_BOUND - 1)
+                           + ".");
+      }
+      else {
+        makeChangeForAmount(amount);
+      }
+    }
+  }
+
+  /**
+   * @param a_pop the population to verify
+   * @return true if all chromosomes in the populationa are unique
+   *
+   * @author Klaus Meffert
+   * @since 3.3.1
+   */
+  public static boolean uniqueChromosomes(Population a_pop) {
+    // Check that all chromosomes are unique
+    for(int i=0;i<a_pop.size()-1;i++) {
+      IChromosome c = a_pop.getChromosome(i);
+      for(int j=i+1;j<a_pop.size();j++) {
+        IChromosome c2 =a_pop.getChromosome(j);
+        if (c == c2) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
